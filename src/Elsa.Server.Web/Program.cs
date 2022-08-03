6ef8c02b6f5dff64c257f0a8420a671e0b2b7d01
Jerry Zajac <32240008+jarekzaj@users.noreply.github.com>
2022-08-03 09:33:26 +0100

    added elsa config

--- src/Elsa.Server.Web/Program.cs
+++ src/Elsa.Server.Web/Program.cs
@@ -1,6 +1,44 @@
+using Elsa.Persistence.EntityFramework.Core.Extensions;
+using Elsa.Persistence.EntityFramework.Sqlite;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Elsa services.
+builder.Services
+    .AddElsa(elsa => elsa
+        .UseEntityFrameworkPersistence(ef => ef.UseSqlite())
+        .AddConsoleActivities()
+    );
+
+// Elsa API endpoints.
+builder.Services.AddElsaApiEndpoints();
+
+// Allow arbitrary client browser apps to access the API.
+// In a production environment, make sure to allow only origins you trust.
+builder.Services.AddCors(cors => cors.AddDefaultPolicy(policy => policy
+    .AllowAnyHeader()
+    .AllowAnyMethod()
+    .AllowAnyOrigin()
+    .WithExposedHeaders("Content-Disposition"))
+);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app
+    .UseHttpsRedirection()
+    .UseStaticFiles() // For Dashboard.
+    .UseRouting()
+    .UseEndpoints(endpoints =>
+    {
+        // Elsa API Endpoints are implemented as regular ASP.NET Core API controllers.
+        endpoints.MapControllers();
+    });
 
 app.Run();
