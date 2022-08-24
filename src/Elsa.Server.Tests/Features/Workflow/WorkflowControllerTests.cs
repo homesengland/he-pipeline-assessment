bd77ee0728a7031dfa44e28b492f3451d5b30632
Jonny T <90678699+Jonnythan-t@users.noreply.github.com>
2022-08-24 08:27:47 +0100

    Added unit tests for multiple choice controller and got rid of warnings in unit tests

--- src/Elsa.Server.Tests/Features/Workflow/WorkflowControllerTests.cs
+++ src/Elsa.Server.Tests/Features/Workflow/WorkflowControllerTests.cs
@@ -1,5 +1,6 @@
 ﻿using AutoFixture.Xunit2;
 using Elsa.Server.Features.Workflow;
+using Elsa.Server.Features.Workflow.LoadWorkflowActivity;
 using Elsa.Server.Features.Workflow.StartWorkflow;
 using Elsa.Server.Models;
 using MediatR;
@@ -11,8 +12,6 @@ namespace Elsa.Server.Tests.Features.Workflow
 {
     public class WorkflowControllerTests
     {
-
-
 
         [Theory]
         [AutoData]
@@ -40,9 +39,9 @@ namespace Elsa.Server.Tests.Features.Workflow
             Assert.IsType<OkObjectResult>(result);
 
             var okResult = (OkObjectResult)result;
-            var okResultValueData = (OperationResult<StartWorkflowResponse>)okResult.Value;
+            var okResultValueData = (OperationResult<StartWorkflowResponse>)okResult.Value!;
 
-            Assert.Equal(response.NextActivityId, okResultValueData.Data.NextActivityId);
+            Assert.Equal(response.NextActivityId, okResultValueData.Data!.NextActivityId);
             Assert.Equal(response.WorkflowInstanceId, okResultValueData.Data.WorkflowInstanceId);
 
         }
@@ -68,7 +67,7 @@ namespace Elsa.Server.Tests.Features.Workflow
             Assert.IsType<BadRequestObjectResult>(result);
 
             var badResult = (BadRequestObjectResult)result;
-            var badResultValueData = (string)badResult.Value;
+            var badResultValueData = (string)badResult.Value!;
 
             Assert.Equal(string.Join(',', operationResult.ErrorMessages), badResultValueData);
 
@@ -99,7 +98,7 @@ namespace Elsa.Server.Tests.Features.Workflow
             Assert.Equal(500, objectResult.StatusCode);
             Assert.IsType<Exception>(objectResult.Value);
 
-            var exceptionResult = (Exception)objectResult.Value;
+            var exceptionResult = (Exception)objectResult.Value!;
 
             Assert.Equal(exception.Message, exceptionResult.Message);
         }
@@ -112,12 +111,128 @@ namespace Elsa.Server.Tests.Features.Workflow
         {
 
             //Arrange
-            mediatorMock.Setup(x => x.Send(command, CancellationToken.None)).ReturnsAsync((OperationResult<StartWorkflowResponse>)null);
+            mediatorMock.Setup(x => x.Send(command, CancellationToken.None)).ReturnsAsync((OperationResult<StartWorkflowResponse>)null!);
 
             WorkflowController controller = new WorkflowController(mediatorMock.Object);
 
             //Act
             var result = await controller.StartWorkflow(command);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.IsType<ObjectResult>(result);
+
+            var objectResult = (ObjectResult)result;
+
+            Assert.Equal(500, objectResult.StatusCode);
+            Assert.IsType<NullReferenceException>(objectResult.Value);
+        }
+
+
+        [Theory]
+        [AutoData]
+        public async Task WorkflowController_LoadWorkflowActivity_ShouldReturnOK_WhenCommandHandlerIsSuccessful(
+           LoadWorkflowActivityRequest request,
+           LoadWorkflowActivityResponse response,
+           Mock<IMediator> mediatorMock)
+        {
+            var startWorkflowOperationResult = new OperationResult<LoadWorkflowActivityResponse>
+            {
+                ErrorMessages = new List<string>(),
+                ValidationMessages = new List<string>(),
+                Data = response
+            };
+            //Arrange
+            mediatorMock.Setup(x => x.Send(It.IsAny<LoadWorkflowActivityRequest>(), CancellationToken.None)).ReturnsAsync(startWorkflowOperationResult);
+
+            WorkflowController controller = new WorkflowController(mediatorMock.Object);
+
+            //Act
+            var result = await controller.LoadWorkflowActivity(request.WorkflowInstanceId, request.ActivityId);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.IsType<OkObjectResult>(result);
+
+            var okResult = (OkObjectResult)result;
+            var okResultValueData = (OperationResult<LoadWorkflowActivityResponse>)okResult.Value!;
+
+            Assert.Equal(response.ActivityId, okResultValueData.Data!.ActivityId);
+            Assert.Equal(response.WorkflowInstanceId, okResultValueData.Data.WorkflowInstanceId);
+
+        }
+
+        [Theory]
+        [AutoData]
+        public async Task WorkflowController_LoadWorkflowActivity_ShouldReturnBadRequest_WhenCommandHandlerReturnsErrors(
+            LoadWorkflowActivityRequest request,
+            OperationResult<LoadWorkflowActivityResponse> operationResult,
+            Mock<IMediator> mediatorMock)
+        {
+
+            //Arrange
+            mediatorMock.Setup(x => x.Send(It.IsAny<LoadWorkflowActivityRequest>(), CancellationToken.None)).ReturnsAsync(operationResult);
+
+            WorkflowController controller = new WorkflowController(mediatorMock.Object);
+
+            //Act
+            var result = await controller.LoadWorkflowActivity(request.WorkflowInstanceId, request.ActivityId);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.IsType<BadRequestObjectResult>(result);
+
+            var badResult = (BadRequestObjectResult)result;
+            var badResultValueData = (string)badResult.Value!;
+
+            Assert.Equal(string.Join(',', operationResult.ErrorMessages), badResultValueData);
+
+        }
+
+        [Theory]
+        [AutoData]
+        public async Task WorkflowController_LoadWorkflowActivity_ShouldReturn500_WhenCommandHandlerThrowsException(
+            LoadWorkflowActivityRequest request,
+            Exception exception,
+            Mock<IMediator> mediatorMock)
+        {
+
+            //Arrange
+            mediatorMock.Setup(x => x.Send(It.IsAny<LoadWorkflowActivityRequest>(), CancellationToken.None)).ThrowsAsync(exception);
+
+            WorkflowController controller = new WorkflowController(mediatorMock.Object);
+
+            //Act
+            var result = await controller.LoadWorkflowActivity(request.WorkflowInstanceId, request.ActivityId);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.IsType<ObjectResult>(result);
+
+            var objectResult = (ObjectResult)result;
+
+            Assert.Equal(500, objectResult.StatusCode);
+            Assert.IsType<Exception>(objectResult.Value);
+
+            var exceptionResult = (Exception)objectResult.Value!;
+
+            Assert.Equal(exception.Message, exceptionResult.Message);
+        }
+
+        [Theory]
+        [AutoData]
+        public async Task WorkflowController_LoadWorkflowActivity_ShouldReturn500_WhenCommandHandlerReturnsNull(
+            LoadWorkflowActivityRequest request,
+            Mock<IMediator> mediatorMock)
+        {
+
+            //Arrange
+            mediatorMock.Setup(x => x.Send(It.IsAny<LoadWorkflowActivityRequest>(), CancellationToken.None)).ReturnsAsync((OperationResult<LoadWorkflowActivityResponse>)null!);
+
+            WorkflowController controller = new WorkflowController(mediatorMock.Object);
+
+            //Act
+            var result = await controller.LoadWorkflowActivity(request.WorkflowInstanceId, request.ActivityId);
 
             //Assert
             Assert.NotNull(result);
