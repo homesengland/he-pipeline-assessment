d6547f8bc5b9c748f629828af93818c8e49f16dd
jarekzaj <32240008+jarekzaj@users.noreply.github.com>
2022-10-07 16:39:34 +0100

    Updated tag to use build number (#26)
    * Enable logging (#31)
    
    * small cleanup, removing not required environments and tags
    
    * added minimal logging and app insights telemetry for debugging purposes on the server
    
    * fixed unit test failure
    
    * added DataProtection to Dashboard and UI
    
    * added migrations for data protection keys and anti forgery token
    
    * added Microsoft.Data.SqlClient to the UI project
    
    Co-authored-by: Jerry Zajac 32240008+jarekzaj@users.noreply.github.com
    Co-authored-by: Jonny T 90678699+Jonnythan-t@users.noreply.github.com
    Co-authored-by: Ben Hall 3179852+benbhall@users.noreply.github.com
    Co-authored-by: Lorna B 93874097+lornacbirchall@users.noreply.github.com
    Co-authored-by: Rob Ryan 112186767+rryan0088@users.noreply.github.com
    
    Co-authored-by: Jonnythan-t <90678699+Jonnythan-t@users.noreply.github.com>
    Co-authored-by: Lorna Birchall <93874097+lornacbirchall@users.noreply.github.com>

--- src/Elsa.Dashboard/Program.cs
+++ src/Elsa.Dashboard/Program.cs
@@ -1,10 +1,15 @@
+using Elsa.CustomInfrastructure.Data;
 using Elsa.Dashboard;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.DataProtection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // For Dashboard.
 builder.Services.AddRazorPages();
 builder.Services.AddScoped<NonceConfig>();
+builder.Services.AddScoped<DbContext>(provider => provider.GetRequiredService<ElsaCustomContext>());
+builder.Services.AddDataProtection().PersistKeysToDbContext<ElsaCustomContext>();
 
 var app = builder.Build();
 
