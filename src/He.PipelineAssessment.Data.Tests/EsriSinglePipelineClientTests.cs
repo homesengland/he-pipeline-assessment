3eb0d8da1ddf7c7f99f332ad6f2350be700f0819
Sathesh Manickavasakam <Sathesh.Manic@homesengland.gov.uk>
2022-10-18 13:09:53 +0100

    httpclienttests added.

--- src/He.PipelineAssessment.Data.Tests/EsriSinglePipelineClientTests.cs
+++ src/He.PipelineAssessment.Data.Tests/EsriSinglePipelineClientTests.cs
@@ -10,143 +10,52 @@ using Xunit;
 namespace He.PipelineAssessment.Data.Tests
 {
 
-    //public class EsriSinglePipelineClientTests
-    //    {
-    //        [Theory]
-    //        [AutoMoqData]
-    //        public async Task PostStartWorkflow_ReturnsNull_GivenHttpClientGivesBackNonSuccessResponse(
-    //            [Frozen] Mock<IHttpClientFactory> httpClientFactoryMock,
-    //            [Frozen] Mock<HttpMessageHandler> httpMessageHandlerMock,
-    //            EsriSinglePipelineClient sut)
-    //        {
-    //            //Arrange
-    //            HttpClientTestHelpers.SetupHttpClientWithExpectedStatusCode(workflowNextActivityDataDto,
-    //                HttpStatusCode.BadRequest,
-    //                httpClientFactoryMock,
-    //                httpMessageHandlerMock);
+    public class EsriSinglePipelineClientTests
+    {
+        [Theory]
+        [AutoMoqData]
+        public async Task PostStartWorkflow_ReturnsNull_GivenHttpClientGivesBackNonSuccessResponse(
+            [Frozen] Mock<IHttpClientFactory> httpClientFactoryMock,
+            [Frozen] Mock<HttpMessageHandler> httpMessageHandlerMock,
+            string spid,
+            string resp,
+            EsriSinglePipelineClient sut)
+        {
+            //Arrange
+            HttpClientTestHelpers.SetupHttpClientWithExpectedStatusCode(resp,
+                HttpStatusCode.BadRequest,
+                httpClientFactoryMock,
+                httpMessageHandlerMock);
 
-    //            //Act
-    //            var exception = await Assert.ThrowsAsync<ApplicationException>(() => sut.GetSinglePipelineData(startWorkflowCommandDto));
+            //Act
+            var isNull = await  sut.GetSinglePipelineData(spid);
 
-    //            //Assert
-    //            Assert.Equal("Failed to start workflow", exception.Message);
-    //        }
+            //Assert
+            Assert.Null(isNull);
+        }
 
-    //        [Theory]
-    //        [AutoMoqData]
-    //        public async Task PostStartWorkflow_ReturnsWorkflowNextActivityDataDto_GivenHttpClientGivesBackNonSuccessResponse(
-    //            [Frozen] Mock<IHttpClientFactory> httpClientFactoryMock,
-    //            [Frozen] Mock<HttpMessageHandler> httpMessageHandlerMock,
-    //            StartWorkflowCommandDto startWorkflowCommandDto,
-    //            WorkflowNextActivityDataDto workflowNextActivityDataDto,
-    //            EsriSinglePipelineClient sut)
-    //        {
-    //            //Arrange
-    //            HttpClientTestHelpers.SetupHttpClientWithExpectedStatusCode(workflowNextActivityDataDto,
-    //                HttpStatusCode.OK,
-    //                httpClientFactoryMock,
-    //                httpMessageHandlerMock);
+        [Theory]
+        [AutoMoqData]
+        public async Task PostStartWorkflow_ReturnsValue_GivenHttpClientGivesBackNonSuccessResponse(
+            [Frozen] Mock<IHttpClientFactory> httpClientFactoryMock,
+            [Frozen] Mock<HttpMessageHandler> httpMessageHandlerMock,
+            string spid,
+            string resp,
+            EsriSinglePipelineClient sut)
+        {
+            //Arrange
+            HttpClientTestHelpers.SetupHttpClientWithExpectedStatusCode(resp,
+                HttpStatusCode.OK,
+                httpClientFactoryMock,
+                httpMessageHandlerMock);
 
-    //            //Act
-    //            var result = await sut.PostStartWorkflow(startWorkflowCommandDto);
+            //Act
+            var result = await sut.GetSinglePipelineData(spid);
 
-    //            //Assert
-    //            Assert.NotNull(result);
-    //            Assert.IsType<WorkflowNextActivityDataDto>(result);
-    //        }
+            //Assert
+            Assert.NotNull(result);
+        }
+    }
 
-    //        [Theory]
-    //        [AutoMoqData]
-    //        public async Task SaveAndContinue_ReturnsNull_GivenHttpClientGivesBackNonSuccessResponse(
-    //            [Frozen] Mock<IHttpClientFactory> httpClientFactoryMock,
-    //            [Frozen] Mock<HttpMessageHandler> httpMessageHandlerMock,
-    //            SaveAndContinueCommandDto saveAndContinueCommandDto,
-    //            WorkflowNextActivityDataDto workflowNextActivityDataDto,
-    //            EsriSinglePipelineClient sut)
-    //        {
-    //            //Arrange
-    //            HttpClientTestHelpers.SetupHttpClientWithExpectedStatusCode(workflowNextActivityDataDto,
-    //                HttpStatusCode.BadRequest,
-    //                httpClientFactoryMock,
-    //                httpMessageHandlerMock);
-
-
-    //            //Act
-    //            var result = await sut.SaveAndContinue(saveAndContinueCommandDto);
-
-    //            //Assert
-    //            Assert.Null(result);
-    //        }
-
-    //        [Theory]
-    //        [AutoMoqData]
-    //        public async Task SaveAndContinue_ReturnsWorkflowNextActivityDataDto_GivenHttpClientGivesBackNonSuccessResponse(
-    //            [Frozen] Mock<IHttpClientFactory> httpClientFactoryMock,
-    //            [Frozen] Mock<HttpMessageHandler> httpMessageHandlerMock,
-    //            SaveAndContinueCommandDto saveAndContinueCommandDto,
-    //            WorkflowNextActivityDataDto workflowNextActivityDataDto,
-    //            EsriSinglePipelineClient sut)
-    //        {
-    //            //Arrange
-    //            HttpClientTestHelpers.SetupHttpClientWithExpectedStatusCode(workflowNextActivityDataDto,
-    //                HttpStatusCode.OK,
-    //                httpClientFactoryMock,
-    //                httpMessageHandlerMock);
-
-    //            //Act
-    //            var result = await sut.SaveAndContinue(saveAndContinueCommandDto);
-
-    //            //Assert
-    //            Assert.NotNull(result);
-    //            Assert.IsType<WorkflowNextActivityDataDto>(result);
-    //        }
-
-    //        [Theory]
-    //        [AutoMoqData]
-    //        public async Task LoadWorkflowActivity_ReturnsNull_GivenHttpClientGivesBackNonSuccessResponse(
-    //            [Frozen] Mock<IHttpClientFactory> httpClientFactoryMock,
-    //            [Frozen] Mock<HttpMessageHandler> httpMessageHandlerMock,
-    //            LoadWorkflowActivityDto loadWorkflowActivityDto,
-    //            WorkflowActivityDataDto workflowActivityDataDto,
-    //            EsriSinglePipelineClient sut)
-    //        {
-    //            //Arrange
-    //            HttpClientTestHelpers.SetupHttpClientWithExpectedStatusCode(workflowActivityDataDto,
-    //                HttpStatusCode.BadRequest,
-    //                httpClientFactoryMock,
-    //                httpMessageHandlerMock);
-
-
-    //            //Act
-    //            var result = await sut.LoadWorkflowActivity(loadWorkflowActivityDto);
-
-    //            //Assert
-    //            Assert.Null(result);
-    //        }
-
-    //        [Theory]
-    //        [AutoMoqData]
-    //        public async Task LoadWorkflowActivity_ReturnsWorkflowNextActivityDataDto_GivenHttpClientGivesBackNonSuccessResponse(
-    //            [Frozen] Mock<IHttpClientFactory> httpClientFactoryMock,
-    //            [Frozen] Mock<HttpMessageHandler> httpMessageHandlerMock,
-    //            LoadWorkflowActivityDto loadWorkflowActivityDto,
-    //            WorkflowActivityDataDto workflowActivityDataDto,
-    //            EsriSinglePipelineClient sut)
-    //        {
-    //            //Arrange
-    //            HttpClientTestHelpers.SetupHttpClientWithExpectedStatusCode(workflowActivityDataDto,
-    //                HttpStatusCode.OK,
-    //                httpClientFactoryMock,
-    //                httpMessageHandlerMock);
-
-    //            //Act
-    //            var result = await sut.LoadWorkflowActivity(loadWorkflowActivityDto);
-
-    //            //Assert
-    //            Assert.NotNull(result);
-    //            Assert.IsType<WorkflowActivityDataDto>(result);
-    //        }
-    //    }
- 
 }
 
