17d32de6ab3526002bd63a12941562d93ad3c7b5
Jerry Zajac <32240008+jarekzaj@users.noreply.github.com>
2022-10-20 10:25:32 +0100

    fixed build errors

--- src/Elsa.Server/Extensions/ElsaStartupExtensions.cs
+++ src/Elsa.Server/Extensions/ElsaStartupExtensions.cs
@@ -1,9 +1,4 @@
-﻿using Elsa.CustomActivities.Activities.Currency;
-using Elsa.CustomActivities.Activities.Date;
-using Elsa.CustomActivities.Activities.LoadAssessmentStage;
-using Elsa.CustomActivities.Activities.MultipleChoice;
-using Elsa.CustomActivities.Activities.SingleChoice;
-using Elsa.CustomActivities.Activities.Text;
+﻿using Elsa.CustomActivities.Activities.Shared;
 
 namespace Elsa.Server.Extensions
 {
@@ -11,12 +6,7 @@ namespace Elsa.Server.Extensions
     {
         public static void AddCustomElsaScriptHandlers(this IServiceCollection services)
         {
-            services.AddNotificationHandlers(typeof(LoadAssessmentStageScriptHandler));
-            services.AddNotificationHandlers(typeof(GetMultipleChoiceQuestionScriptHandler));
-            services.AddNotificationHandlers(typeof(GetSingleChoiceQuestionScriptHandler));
-            services.AddNotificationHandlers(typeof(GetCurrencyQuestionScriptHandler));
-            services.AddNotificationHandlers(typeof(GetTextQuestionScriptHandler));
-            services.AddNotificationHandlers(typeof(GetDateQuestionScriptHandler));
+            services.AddNotificationHandlers(typeof(GetScriptHandler));
         }
     }
 }
