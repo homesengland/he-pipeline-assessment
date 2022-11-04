dfdd028a63ccf271d0af3beee5b4a0015ad7fa42
Jonny T <90678699+Jonnythan-t@users.noreply.github.com>
2022-11-04 09:04:09 +0000

    initial work to sync data

--- src/He.PipelineAssessment.UI/Features/SinglePipeline/Sync/SyncCommandHandler.cs
+++ src/He.PipelineAssessment.UI/Features/SinglePipeline/Sync/SyncCommandHandler.cs
@@ -1,4 +1,6 @@
 ﻿using He.PipelineAssessment.Data.SinglePipeline;
+using He.PipelineAssessment.Infrastructure.Repository;
+using He.PipelineAssessment.Models;
 using MediatR;
 
 namespace He.PipelineAssessment.UI.Features.SinglePipeline.Sync
@@ -7,11 +9,13 @@ namespace He.PipelineAssessment.UI.Features.SinglePipeline.Sync
     {
         private readonly IEsriSinglePipelineClient _esriSinglePipelineClient;
         private readonly IEsriSinglePipelineDataJsonHelper _jsonHelper;
+        private readonly IAssessmentRepository _assessmentRepository;
 
-        public SyncCommandHandler(IEsriSinglePipelineClient esriSinglePipelineClient, IEsriSinglePipelineDataJsonHelper jsonHelper)
+        public SyncCommandHandler(IEsriSinglePipelineClient esriSinglePipelineClient, IEsriSinglePipelineDataJsonHelper jsonHelper, IAssessmentRepository assessmentRepository)
         {
             _esriSinglePipelineClient = esriSinglePipelineClient;
             _jsonHelper = jsonHelper;
+            _assessmentRepository = assessmentRepository;
         }
 
         public async Task<SyncResponse> Handle(SyncCommand request, CancellationToken cancellationToken)
@@ -20,10 +24,37 @@ namespace He.PipelineAssessment.UI.Features.SinglePipeline.Sync
             if (data != null)
             {
                 var dataResult = _jsonHelper.JsonToSinglePipelineDataList(data);
-                //this.Output = dataResult;
+                List<int> sourceAssessmentSpIds = dataResult!.Select(x => x.sp_id!.Value).ToList();
 
+                var destinationAssessments = await _assessmentRepository.GetAssessments();
+                var destinationAssessmentSpIds = destinationAssessments.Select(x => x.SpId).ToList();
+
+                //items in one list not in the other
+                var assessmentSpIdsToAdd = sourceAssessmentSpIds.Where(s => !destinationAssessmentSpIds.Any(d => d == s)).ToList();
+                var sourceAssessmentsToAdd = dataResult!.Where(x => assessmentSpIdsToAdd.Contains(x.sp_id!.Value));
+
+                var assessmentsToBeAdded = new List<Assessment>();
+                foreach (var item in sourceAssessmentsToAdd)
+                {
+                    //Add to database
+                    var assessment = new Assessment()
+                    {
+                        Counterparty = string.IsNullOrEmpty(item.applicant_1) ? "-" : item.applicant_1,
+                        Reference = string.IsNullOrEmpty(item.internal_reference) ? "-" : item.internal_reference,
+                        SiteName = string.IsNullOrEmpty(item.pipeline_opportunity_site_name) ? "-" : item.pipeline_opportunity_site_name,
+                        SpId = item.sp_id.HasValue ? item.sp_id.Value : 999,
+                        Status = "New"
+                    };
+                    assessmentsToBeAdded.Add(assessment);
+                }
+
+                await _assessmentRepository.CreateAssessments(assessmentsToBeAdded);
             }
-            throw new NotImplementedException();
+
+            return new SyncResponse()
+            {
+                ErrorMessages = new List<string>()
+            };
         }
     }
 }
