4a3838709a0d99b558aec2aed537d49bbc2d652e
HCA\rob.ryan <112186767+rryan0088@users.noreply.github.com>
2022-11-10 15:31:10 +0000

    Added dropdown capability

--- src/Elsa.Dashboard/src/components/elsa-multiquestion-records-property/elsa-multiquestion-records-property.tsx
+++ src/Elsa.Dashboard/src/components/elsa-multiquestion-records-property/elsa-multiquestion-records-property.tsx
@@ -1,7 +1,7 @@
 import { Component, h, Prop, State, Listen } from '@stencil/core';
 
 import TrashCanIcon from '../../icons/trash-can';
-import PlusIcon from '../../icons/plus_icon';
+//import PlusIcon from '../../icons/plus_icon';
 import {
   ActivityDefinitionProperty,
   ActivityModel,
@@ -81,11 +81,22 @@ export class ElsaMultiQuestionRecordsProperty {
     this.updatePropertyModel();
   }
 
-  onAddQuestionClick(questionType: string) {
+  handleAddQuestion(e: Event) {
+    let value = (e.currentTarget as HTMLInputElement).value.trim();
+    if (value != null && value != "") {
+      this.onAddQuestion(value);
+      let element = e.currentTarget as HTMLSelectElement;
+      element.selectedIndex = 0;
+    }
+  }
+
+  onAddQuestion(questionType: string) {
+    console.log('Adding Question');
     const questionName = `Question ${this.multiQuestionModel.questions.length + 1}`;
     let sampleId = `${this.multiQuestionModel.questions.length + 1}`
     const newQuestion = { id: sampleId, title: questionName, questionGuidance: "", questionText: "", displayComments: false, questionHint: "", questionType: questionType };
     this.multiQuestionModel = { ...this.multiQuestionModel, questions: [...this.multiQuestionModel.questions, newQuestion] };
+    console.log(newQuestion)
     this.updatePropertyModel();
   }
 
@@ -94,8 +105,6 @@ export class ElsaMultiQuestionRecordsProperty {
     this.multiQuestionModel = { ...this.multiQuestionModel, questions: this.multiQuestionModel.questions.filter(x => x != question) };
     this.updatePropertyModel();
   }
-
-
 
   onAccordionQuestionClick(e: Event) {
     let element = e.currentTarget as HTMLElement;
@@ -151,13 +160,13 @@ export class ElsaMultiQuestionRecordsProperty {
       const field = `question-${index}`;
       return (
         <div id={`${field}-id`} class="accordion elsa-mb-4 elsa-rounded" onClick={this.onAccordionQuestionClick}>
-          <button type="button">Question {index + 1}</button>
+          <button type="button">Question {index + 1} - {multiQuestion.questionType} </button>
           <button type="button" onClick={e => this.onDeleteQuestionClick(e, multiQuestion)}
             class="elsa-h-5 elsa-w-5 elsa-mx-auto elsa-outline-none focus:elsa-outline-none trashcan-icon" style={{ float: "right" }}>
             <TrashCanIcon options={this.iconProvider.getOptions()}></TrashCanIcon>
           </button>
 
-          <elsa-question class="panel" question={multiQuestion}></elsa-question>
+          <elsa-question onClick={(e) => e.stopPropagation() } class="panel elsa-rounded" question={multiQuestion}></elsa-question>
         </div>
       );
     };
@@ -173,11 +182,18 @@ export class ElsaMultiQuestionRecordsProperty {
           </div>
         </div>
         {this.multiQuestionModel.questions.map(renderChoiceEditor)}
-        <button type="button" onClick={() => this.onAddQuestionClick("TextQuestion")}
-          class="elsa-inline-flex elsa-items-center elsa-px-4 elsa-py-2 elsa-border elsa-border-transparent elsa-shadow-sm elsa-text-sm elsa-font-medium elsa-rounded-md elsa-text-white elsa-bg-blue-600 hover:elsa-bg-blue-700 focus:elsa-outline-none focus:elsa-ring-2 focus:elsa-ring-offset-2 focus:elsa-ring-blue-500 elsa-mt-2">
-          <PlusIcon options={this.iconProvider.getOptions()} />
-          Add Question
-        </button>
+
+        <select id="addQuestionDropdown"
+          onChange={(e) => this.handleAddQuestion.bind(this)(e)}
+          name="addQuestionDropdown"
+          class="elsa-mt-1 elsa-block focus:elsa-ring-blue-500 focus:elsa-border-blue-500 elsa-w-full elsa-shadow-sm sm:elsa-max-w-xs sm:elsa-text-sm elsa-border-gray-300 elsa-rounded-md">
+          <option value="">Add a Question...</option>
+          <option value="MultipleChoiceQuestion">Checkbox Question</option>
+          <option value="CurrencyQuestion">Currency Question</option>
+          <option value="DateQuestion">Date Question</option>
+          <option value="SingleChoiceQuestion">Radio Question</option>
+          <option value="TextQuestion">Text Question</option> 
+        </select>
       </div>
     );
   }
