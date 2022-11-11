d920edaadd507912566dcec903f5f03ef0d4be72
Jonny T <90678699+Jonnythan-t@users.noreply.github.com>
2022-11-11 12:01:56 +0000

    initial validation on question screen implemented

--- src/He.PipelineAssessment.UI/Features/Workflow/SaveAndContinue/SaveAndContinueCommandValidator.cs
+++ src/He.PipelineAssessment.UI/Features/Workflow/SaveAndContinue/SaveAndContinueCommandValidator.cs
@@ -1,5 +1,6 @@
 ﻿using Elsa.CustomWorkflow.Sdk;
 using FluentValidation;
+using He.PipelineAssessment.UI.Features.Workflow.SaveAndContinue.Validators;
 using System.Globalization;
 
 namespace He.PipelineAssessment.UI.Features.Workflow.SaveAndContinue
@@ -9,44 +10,30 @@ namespace He.PipelineAssessment.UI.Features.Workflow.SaveAndContinue
 
         public SaveAndContinueCommandValidator()
         {
-            RuleFor(x => x.Data.QuestionActivityData!.MultipleChoice).Must(multipleChoice =>
-            {
-                if (multipleChoice.SelectedChoices.Count() <= 1) return true;
-                foreach (var choice in multipleChoice.Choices)
+            RuleFor(x => x.Data.QuestionActivityData!.MultipleChoice)
+                .SetValidator(new MultiChoiceValidator())
+                .When(x => x.Data.QuestionActivityData!.ActivityType == ActivityTypeConstants.MultipleChoiceQuestion);
+
+            RuleFor(x => x.Data.QuestionActivityData!.Date)
+                .SetValidator(new DateValidator())
+                .When(x => x.Data.QuestionActivityData!.ActivityType == ActivityTypeConstants.DateQuestion);
+
+            RuleFor(x => x.Data.MultiQuestionActivityData)
+                .ForEach(x =>
                 {
-                    if (choice.IsSingle && multipleChoice.SelectedChoices.Contains(choice.Answer))
-                    {
-                        return false;
-                    }
-                }
-                return true;
-            }).When(x => x.Data.QuestionActivityData!.ActivityType == ActivityTypeConstants.MultipleChoiceQuestion)
-                .WithMessage(x =>
-            {
-                var selectedAnswers = x.Data.QuestionActivityData!.MultipleChoice.SelectedChoices;
-                var exclusiveAnswers = x.Data.QuestionActivityData.MultipleChoice.Choices
-                    .Where(c => c.IsSingle && selectedAnswers.Contains(c.Answer)).Select(c => c.Answer).ToList();
+                    x.SetValidator(new MultiQuestionActivityDataValidator());
+                });
+        }
 
-                if (exclusiveAnswers.Any())
-                {
-                    if (exclusiveAnswers.Count() > 1)
-                    {
-                        var finalInvalidAnswer = exclusiveAnswers.Last();
-
-                        var invalidAnswers = string.Join(", ", exclusiveAnswers.Take(exclusiveAnswers.Count() - 1));
-                        return $"{invalidAnswers} and {finalInvalidAnswer} cannot be selected with any other answer.";
-                    }
-                    return $"{exclusiveAnswers.First()} cannot be selected with any other answer.";
-                }
-                return string.Empty;
-            });
-
+        private void DateValidation()
+        {
             RuleFor(x => x.Data.QuestionActivityData!.Date.Day).Must(day =>
             {
                 if (day == null)
                 {
                     return true;
                 }
+
                 if (day != null && day > 31)
                 {
                     return false;
@@ -63,6 +50,7 @@ namespace He.PipelineAssessment.UI.Features.Workflow.SaveAndContinue
                 {
                     return true;
                 }
+
                 if (month != null && month > 12)
                 {
                     return false;
@@ -79,6 +67,7 @@ namespace He.PipelineAssessment.UI.Features.Workflow.SaveAndContinue
                 {
                     return true;
                 }
+
                 if (year != null && (year > 9999 || year < 999))
                 {
                     return false;
@@ -90,26 +79,28 @@ namespace He.PipelineAssessment.UI.Features.Workflow.SaveAndContinue
             }).WithMessage("Year must include 4 numbers");
 
             RuleFor(x => x.Data.QuestionActivityData!.Date).Must(date =>
-            {
-                if (date.Day == null && date.Month == null && date.Year == null) return true;
-                var dateString =
-                    $"{date.Year}-{date.Month}-{date.Day}";
-                bool isValidDate = DateTime.TryParseExact(dateString, Constants.DateFormat, CultureInfo.InvariantCulture, DateTimeStyles.AdjustToUniversal, out DateTime parsedDateTime);
-                if (isValidDate)
                 {
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
-            }).When(x => x.Data.QuestionActivityData!.ActivityType == ActivityTypeConstants.DateQuestion)
+                    if (date.Day == null && date.Month == null && date.Year == null) return true;
+                    var dateString =
+                        $"{date.Year}-{date.Month}-{date.Day}";
+                    bool isValidDate = DateTime.TryParseExact(dateString, Constants.DateFormat, CultureInfo.InvariantCulture,
+                        DateTimeStyles.AdjustToUniversal, out DateTime parsedDateTime);
+                    if (isValidDate)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }).When(x => x.Data.QuestionActivityData!.ActivityType == ActivityTypeConstants.DateQuestion)
                 .WithMessage(x =>
                 {
                     var selectedDate = x.Data!.QuestionActivityData!.Date;
                     var dateString =
                         $"{selectedDate.Year}-{selectedDate.Month}-{selectedDate.Day}";
-                    bool isValidDate = DateTime.TryParseExact(dateString, Constants.DateFormat, CultureInfo.InvariantCulture, DateTimeStyles.AdjustToUniversal, out DateTime parsedDateTime);
+                    bool isValidDate = DateTime.TryParseExact(dateString, Constants.DateFormat, CultureInfo.InvariantCulture,
+                        DateTimeStyles.AdjustToUniversal, out DateTime parsedDateTime);
                     if (isValidDate)
                     {
                         return string.Empty;
@@ -119,7 +110,44 @@ namespace He.PipelineAssessment.UI.Features.Workflow.SaveAndContinue
                         return "The date entered must be a real date";
                     }
                 });
+        }
 
+        private void MultiChoiceValidation()
+        {
+            RuleFor(x => x.Data.QuestionActivityData!.MultipleChoice).Must(multipleChoice =>
+                {
+                    if (multipleChoice.SelectedChoices.Count() <= 1) return true;
+                    foreach (var choice in multipleChoice.Choices)
+                    {
+                        if (choice.IsSingle && multipleChoice.SelectedChoices.Contains(choice.Answer))
+                        {
+                            return false;
+                        }
+                    }
+
+                    return true;
+                }).When(x => x.Data.QuestionActivityData!.ActivityType == ActivityTypeConstants.MultipleChoiceQuestion)
+                .WithMessage(x =>
+                {
+                    var selectedAnswers = x.Data.QuestionActivityData!.MultipleChoice.SelectedChoices;
+                    var exclusiveAnswers = x.Data.QuestionActivityData.MultipleChoice.Choices
+                        .Where(c => c.IsSingle && selectedAnswers.Contains(c.Answer)).Select(c => c.Answer).ToList();
+
+                    if (exclusiveAnswers.Any())
+                    {
+                        if (exclusiveAnswers.Count() > 1)
+                        {
+                            var finalInvalidAnswer = exclusiveAnswers.Last();
+
+                            var invalidAnswers = string.Join(", ", exclusiveAnswers.Take(exclusiveAnswers.Count() - 1));
+                            return $"{invalidAnswers} and {finalInvalidAnswer} cannot be selected with any other answer.";
+                        }
+
+                        return $"{exclusiveAnswers.First()} cannot be selected with any other answer.";
+                    }
+
+                    return string.Empty;
+                });
         }
     }
 }
---
+++ src/He.PipelineAssessment.UI/Features/Workflow/SaveAndContinue/Validators/MultiQuestionActivityDataValidator.cs
@@ -0,0 +1,20 @@
+﻿using Elsa.CustomWorkflow.Sdk;
+using Elsa.CustomWorkflow.Sdk.Models.Workflow;
+using FluentValidation;
+
+namespace He.PipelineAssessment.UI.Features.Workflow.SaveAndContinue.Validators
+{
+    public class MultiQuestionActivityDataValidator : AbstractValidator<QuestionActivityData>
+    {
+        public MultiQuestionActivityDataValidator()
+        {
+            RuleFor(x => x.MultipleChoice)
+                .SetValidator(new MultiChoiceValidator())
+                .When(x => x.QuestionType == QuestionTypeConstants.MultipleChoiceQuestion);
+
+            RuleFor(x => x.Date)
+                .SetValidator(new DateValidator())
+                .When(x => x.QuestionType == QuestionTypeConstants.DateQuestion);
+        }
+    }
+}
