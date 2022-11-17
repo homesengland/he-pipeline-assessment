9162f345ceaf29fa283f350583c10bf4442ae333
Jerry Zajac <32240008+jarekzaj@users.noreply.github.com>
2022-11-17 14:51:32 +0000

    removed unused method from json helper

--- src/Elsa.Server/Features/Workflow/LoadWorkflowActivity/LoadWorkflowActivityJsonHelper.cs
+++ src/Elsa.Server/Features/Workflow/LoadWorkflowActivity/LoadWorkflowActivityJsonHelper.cs
@@ -5,21 +5,12 @@ namespace Elsa.Server.Features.Workflow.LoadWorkflowActivity
 {
     public interface ILoadWorkflowActivityJsonHelper
     {
-        //AssessmentQuestion? ActivityOutputJsonToAssessmentQuestion(string activityJson);
-
         T? ActivityDataDictionaryToQuestionActivityData<T>(IDictionary<string, object?>? activityDataDictionary);
     }
 
     [ExcludeFromCodeCoverage]
     public class LoadWorkflowActivityJsonHelper : ILoadWorkflowActivityJsonHelper
     {
-        //public AssessmentQuestion? ActivityOutputJsonToAssessmentQuestion(string activityJson)
-        //{
-        //    var output = JsonSerializer.Deserialize<AssessmentQuestion>(activityJson,
-        //        new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
-        //    return output;
-        //}
-
         public T? ActivityDataDictionaryToQuestionActivityData<T>(IDictionary<string, object?>? activityDataDictionary)
         {
             var json = JsonSerializer.Serialize(activityDataDictionary);
