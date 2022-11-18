869edf769758b38278c1ac0ea2bef758de1dd78d
Jonny T <90678699+Jonnythan-t@users.noreply.github.com>
2022-11-18 13:20:58 +0000

    updated nullable types

--- src/He.PipelineAssessment.Data/IdentityClientConfig.cs
+++ src/He.PipelineAssessment.Data/IdentityClientConfig.cs
@@ -2,8 +2,8 @@
 {
     public class IdentityClientConfig
     {
-        public string ApplicationManagedIdentity { get; set; }
-        public string AzureTenantId { get; set; }
-        public string AzureResourceId { get; set; }
+        public string ApplicationManagedIdentity { get; set; } = null!;
+        public string AzureTenantId { get; set; } = null!;
+        public string AzureResourceId { get; set; } = null!;
     }
 }
