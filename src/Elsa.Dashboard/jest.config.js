33939089f0f2c645c47399099bb0c6b1bf9d7fbc
Jerry Zajac <32240008+jarekzaj@users.noreply.github.com>
2022-11-18 16:42:53 +0000

    fixed typescript issue with our build by upgrading stencil package, added extra jest configuration to exclude both js and ts files running tests

--- src/Elsa.Dashboard/jest.config.js
+++ src/Elsa.Dashboard/jest.config.js
@@ -1,6 +1,10 @@
 module.exports = {
+    roots: [
+      "<rootDir>/src"
+    ],
     preset: 'ts-jest',
     testEnvironment: 'jsdom',
+    testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
     transform: {
       '^.+\\.ts?$': 'ts-jest',
       "^.+\\.(js|jsx)$": "babel-jest"
@@ -8,5 +12,10 @@ module.exports = {
     transformIgnorePatterns: ['<rootDir>/node_modules/'],
     globals: {
       window: {}
-    }
+    },
+    moduleFileExtensions: [
+      "ts",
+      "tsx",
+      "js"
+    ],
   };
--- src/Elsa.Dashboard/stencil.config.ts
+++ src/Elsa.Dashboard/stencil.config.ts
@@ -9,5 +9,5 @@ export const config: Config = {
       type: 'www',
       serviceWorker: null, // disable service workers
     },
-  ],
+  ]
 };
