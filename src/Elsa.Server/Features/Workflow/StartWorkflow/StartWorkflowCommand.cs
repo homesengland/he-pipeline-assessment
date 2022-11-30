c19858275aa47d35377cf185efcfdf445955ab35
Jonny T <90678699+Jonnythan-t@users.noreply.github.com>
2022-11-30 15:04:35 +0000

    passing correlationId to start workflow request

--- src/Elsa.Server/Features/Workflow/StartWorkflow/StartWorkflowCommand.cs
+++ src/Elsa.Server/Features/Workflow/StartWorkflow/StartWorkflowCommand.cs
@@ -5,6 +5,7 @@ namespace Elsa.Server.Features.Workflow.StartWorkflow
 {
     public class StartWorkflowCommand : IRequest<OperationResult<StartWorkflowResponse>>
     {
+        public string CorrelationId { get; set; } = null!;
         public string WorkflowDefinitionId { get; set; } = null!;
     }
 }
