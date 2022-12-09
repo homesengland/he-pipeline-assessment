09efe8361e4a0f8a0354b5addc1011bbd9efa8ff
Jerry Zajac <32240008+jarekzaj@users.noreply.github.com>
2022-12-09 15:10:27 +0000

    Revert "added extra validation so that all answers are required by default"
    This reverts commit 81f856fef9d872f35f03f44a304c1bfa10a23208.

--- src/Elsa.CustomWorkflow.Sdk.Tests/Workflow/Validators/MultiChoiceValidatorTests.cs
+++ src/Elsa.CustomWorkflow.Sdk.Tests/Workflow/Validators/MultiChoiceValidatorTests.cs
@@ -1,6 +1,7 @@
 ﻿using Elsa.CustomWorkflow.Sdk.Models.Workflow;
 using Elsa.CustomWorkflow.Sdk.Models.Workflow.Validators;
 using FluentValidation.TestHelper;
+using He.PipelineAssessment.Common.Tests;
 using Xunit;
 
 namespace Elsa.CustomWorkflow.Sdk.Tests.Workflow.Validators
@@ -79,7 +80,6 @@ namespace Elsa.CustomWorkflow.Sdk.Tests.Workflow.Validators
 
             result.ShouldHaveValidationErrorFor(c => c)
                 .WithErrorMessage("Test 1 cannot be selected with any other answer.");
-            result.ShouldNotHaveValidationErrorFor(c => c.SelectedChoices);
         }
 
         [Fact]
@@ -113,7 +113,6 @@ namespace Elsa.CustomWorkflow.Sdk.Tests.Workflow.Validators
 
             //Assert
             result.ShouldNotHaveValidationErrorFor(c => c);
-            result.ShouldNotHaveValidationErrorFor(c => c.SelectedChoices);
         }
 
         [Fact]
@@ -147,44 +146,6 @@ namespace Elsa.CustomWorkflow.Sdk.Tests.Workflow.Validators
 
             //Assert
             result.ShouldNotHaveValidationErrorFor(c => c);
-            result.ShouldNotHaveValidationErrorFor(c => c.SelectedChoices);
-
-        }
-
-        [Fact]
-        public void Should_Have_Error_When_SelectedChoicesEmpty()
-        {
-            //Arrange
-            MultiChoiceValidator validator = new MultiChoiceValidator();
-            Checkbox multiChoice = new Checkbox
-            {
-                Choices = new List<Choice>
-                {
-                    new Choice
-                    {
-                        Answer = "Test 1",
-                        IsSingle = true
-                    },
-                    new Choice
-                    {
-                        Answer = "Test 2",
-                        IsSingle = false
-                    },
-                    new Choice
-                    {
-                        Answer = "Test 3",
-                        IsSingle = false
-                    },
-                },
-                SelectedChoices = new List<string>()
-            };
-
-            //Act
-            var result = validator.TestValidate(multiChoice);
-
-            //Assert
-            result.ShouldNotHaveValidationErrorFor(c => c);
-            result.ShouldHaveValidationErrorFor(c => c.SelectedChoices).WithErrorMessage("The question has not been answered");
         }
 
     }
--- src/Elsa.CustomWorkflow.Sdk/Models/Workflow/Validators/MultiQuestionActivityDataValidator.cs
+++ src/Elsa.CustomWorkflow.Sdk/Models/Workflow/Validators/MultiQuestionActivityDataValidator.cs
@@ -8,23 +8,11 @@ namespace Elsa.CustomWorkflow.Sdk.Models.Workflow.Validators
         {
             RuleFor(x => x.Checkbox)
                 .SetValidator(new MultiChoiceValidator())
-                .When(x => x.QuestionType == QuestionTypeConstants.CheckboxQuestion);            
-            
-            RuleFor(x => x.Radio)
-                .SetValidator(new RadioValidator())
-                .When(x => x.QuestionType == QuestionTypeConstants.RadioQuestion);
+                .When(x => x.QuestionType == QuestionTypeConstants.CheckboxQuestion);
 
             RuleFor(x => x.Date)
                 .SetValidator(new DateValidator())
                 .When(x => x.QuestionType == QuestionTypeConstants.DateQuestion);
-
-            RuleFor(x => x)
-                .SetValidator(new CurrencyValidator())
-                .When(x => x.QuestionType == QuestionTypeConstants.CurrencyQuestion);
-
-            RuleFor(x => x)
-                .SetValidator(new TextValidator())
-                .When(x => x.QuestionType == QuestionTypeConstants.TextQuestion);
         }
     }
 }
