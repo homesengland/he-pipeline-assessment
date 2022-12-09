611fbb779c0a716a452d71afba49e1676140884b
Jerry Zajac <32240008+jarekzaj@users.noreply.github.com>
2022-12-09 15:39:33 +0000

    Revert "Revert "added extra validation so that all answers are required by default""
    This reverts commit 09efe8361e4a0f8a0354b5addc1011bbd9efa8ff.

--- src/Elsa.CustomWorkflow.Sdk/Models/Workflow/Validators/DateValidator.cs
+++ src/Elsa.CustomWorkflow.Sdk/Models/Workflow/Validators/DateValidator.cs
@@ -1,80 +1,87 @@
-﻿using Elsa.CustomWorkflow.Sdk;
-using FluentValidation;
+﻿using FluentValidation;
 using System.Globalization;
 
 namespace Elsa.CustomWorkflow.Sdk.Models.Workflow.Validators
 {
-    public class DateValidator : AbstractValidator<Elsa.CustomWorkflow.Sdk.Models.Workflow.Date>
+    public class DateValidator : AbstractValidator<Date>
     {
         public DateValidator()
         {
-            RuleFor(x => x.Day).Must(day =>
-            {
-                if (day == null)
-                {
-                    return true;
-                }
-
-                if (day != null && day > 31)
-                {
-                    return false;
-                }
-                else
-                {
-                    return true;
-                }
-            }).WithMessage("The date entered must include a real day");
-
-            RuleFor(x => x.Month).Must(month =>
-            {
-                if (month == null)
-                {
-                    return true;
-                }
-
-                if (month != null && month > 12)
-                {
-                    return false;
-                }
-                else
-                {
-                    return true;
-                }
-            }).WithMessage("The date entered must include a real month");
-
-            RuleFor(x => x.Year).Must(year =>
-            {
-                if (year == null)
-                {
-                    return true;
-                }
-
-                if (year != null && (year > 9999 || year < 999))
-                {
-                    return false;
-                }
-                else
-                {
-                    return true;
-                }
-            }).WithMessage("Year must include 4 numbers");
-
             RuleFor(x => x).Must(date =>
             {
-                if (date.Day == null && date.Month == null && date.Year == null) return true;
-                var dateString =
-                    $"{date.Year}-{date.Month}-{date.Day}";
-                bool isValidDate = DateTime.TryParseExact(dateString, Constants.DateFormat, CultureInfo.InvariantCulture,
-                    DateTimeStyles.AdjustToUniversal, out DateTime parsedDateTime);
-                if (isValidDate)
+                if (date.Day == null && date.Month == null && date.Year == null) return false;
+                return true;
+            }).WithMessage("The question has not been answered").DependentRules(() =>
                 {
-                    return true;
+                    RuleFor(x => x.Day).Must(day =>
+                    {
+                        if (day == null)
+                        {
+                            return true;
+                        }
+
+                        if (day != null && day > 31)
+                        {
+                            return false;
+                        }
+                        else
+                        {
+                            return true;
+                        }
+                    }).WithMessage("The date entered must include a real day");
+
+                    RuleFor(x => x.Month).Must(month =>
+                    {
+                        if (month == null)
+                        {
+                            return true;
+                        }
+
+                        if (month != null && month > 12)
+                        {
+                            return false;
+                        }
+                        else
+                        {
+                            return true;
+                        }
+                    }).WithMessage("The date entered must include a real month");
+
+                    RuleFor(x => x.Year).Must(year =>
+                    {
+                        if (year == null)
+                        {
+                            return true;
+                        }
+
+                        if (year != null && (year > 9999 || year < 999))
+                        {
+                            return false;
+                        }
+                        else
+                        {
+                            return true;
+                        }
+                    }).WithMessage("Year must include 4 numbers");
+
+                    RuleFor(x => x).Must(date =>
+                    {
+                        var dateString =
+                            $"{date.Year}-{date.Month}-{date.Day}";
+                        bool isValidDate = DateTime.TryParseExact(dateString, Constants.DateFormat,
+                            CultureInfo.InvariantCulture,
+                            DateTimeStyles.AdjustToUniversal, out DateTime parsedDateTime);
+                        if (isValidDate)
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }).WithMessage("The date entered must be a real date");
                 }
-                else
-                {
-                    return false;
-                }
-            }).WithMessage("The date entered must be a real date");
+            );
         }
     }
 }
---
+++ src/Elsa.CustomWorkflow.Sdk/Models/Workflow/Validators/RadioValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace Elsa.CustomWorkflow.Sdk.Models.Workflow.Validators
+{
+    public class RadioValidator : AbstractValidator<Radio>
+    {
+        public RadioValidator()
+        {
+            RuleFor(x => x.SelectedAnswer).NotEmpty().WithMessage("The question has not been answered");
+        }
+    }
+}
