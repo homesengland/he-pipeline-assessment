bfe895d9fb7d69c2e540ead46cfd5ba2bec6c2f8
M S <122261875+MSarwarDev@users.noreply.github.com>
2023-01-12 13:43:11 +0000

    Adding AssessmentId to NextWorkFlow Table

---
+++ src/He.PipelineAssessment.Infrastructure/Migrations/20230112134142_AddingAssessmentIdinNextWorkFlowTable.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace He.PipelineAssessment.Infrastructure.Migrations
+{
+    public partial class AddingAssessmentIdinNextWorkFlowTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AssessmentId",
+                table: "AssessmentToolInstanceNextWorkflow",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AssessmentId",
+                table: "AssessmentToolInstanceNextWorkflow");
+        }
+    }
+}
--- src/He.PipelineAssessment.Models/AssessmentToolInstanceNextWorkflow.cs
+++ src/He.PipelineAssessment.Models/AssessmentToolInstanceNextWorkflow.cs
@@ -6,7 +6,9 @@
         public int AssessmentToolWorkflowInstanceId { get; set; }
         public string NextWorkflowDefinitionId { get; set; } = null!;
         public bool IsStarted { get; set; }
+        public int AssessmentId { get; set; }
 
+     
         public virtual AssessmentToolWorkflowInstance AssessmentToolWorkflowInstance { get; set; } = null!;
     }
 }
