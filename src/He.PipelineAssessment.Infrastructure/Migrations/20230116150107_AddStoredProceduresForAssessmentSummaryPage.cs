812ee7d7ccdd4319967cadbb9d54b51a62946d4a
Jerry Zajac <32240008+jarekzaj@users.noreply.github.com>
2023-01-16 17:20:59 +0000

    stored procedure deployment

---
+++ src/He.PipelineAssessment.Infrastructure/Migrations/20230116150107_AddStoredProceduresForAssessmentSummaryPage.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System.Reflection;
+
+#nullable disable
+
+namespace He.PipelineAssessment.Infrastructure.Migrations
+{
+    public partial class AddStoredProceduresForAssessmentSummaryPage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var sqlFiles = assembly.GetManifestResourceNames().Where(file =>
+                file.Contains(
+                    "20230116150107_AddStoredProceduresForAssessmentSummaryPage.GetAssessmentStagesByAssessmentId.sql") ||
+                file.Contains(
+                    "20230116150107_AddStoredProceduresForAssessmentSummaryPage.GetStartableToolsByAssessmentId.sql"));
+            foreach (var sqlFile in sqlFiles)
+            {
+                using (Stream stream = assembly.GetManifestResourceStream(sqlFile))
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    var sqlScript = reader.ReadToEnd();
+                    migrationBuilder.Sql($"EXEC(N'{sqlScript}')");
+                }
+            }
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            var dropGetAssessmentStagesByAssessmentId = "DROP PROC GetAssessmentStagesByAssessmentId";
+            migrationBuilder.Sql(dropGetAssessmentStagesByAssessmentId);
+
+            var dropGetStartableToolsByAssessmentId = "DROP PROC GetStartableToolsByAssessmentId";
+            migrationBuilder.Sql(dropGetStartableToolsByAssessmentId);
+        }
+    }
+}
