a9ff3556665926d37d87e37c9fbc61df783818e2
jarekzaj <32240008+jarekzaj@users.noreply.github.com>
2023-01-20 10:03:20 +0000

    Updated Elsa packages and separated db context in Pipeline assessment UI (#86)
    Co-authored-by: Jerry Zajac 32240008+jarekzaj@users.noreply.github.com
    Co-authored-by: Jonny T 90678699+Jonnythan-t@users.noreply.github.com
    Co-authored-by: Ben Hall 3179852+benbhall@users.noreply.github.com
    Co-authored-by: Lorna B 93874097+lornacbirchall@users.noreply.github.com
    Co-authored-by: Rob Ryan 112186767+rryan0088@users.noreply.github.com
    Co-authored-by: M S Dev 122261875+MSarwarDev@users.noreply.github.com

--- src/He.PipelineAssessment.Infrastructure/Data/PipelineAssessmentContext.cs
+++ src/He.PipelineAssessment.Infrastructure/Data/PipelineAssessmentContext.cs
@@ -1,6 +1,5 @@
 ﻿using He.PipelineAssessment.Infrastructure.Config;
 using He.PipelineAssessment.Models;
-using He.PipelineAssessment.Models.ViewModels;
 using Microsoft.AspNetCore.DataProtection.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,7 +7,7 @@ namespace He.PipelineAssessment.Infrastructure.Data
 {
     public class PipelineAssessmentContext : DbContext, IDataProtectionKeyContext
     {
-        public PipelineAssessmentContext(DbContextOptions options) : base(options)
+        public PipelineAssessmentContext(DbContextOptions<PipelineAssessmentContext> options) : base(options)
         {
 
         }
@@ -18,14 +17,10 @@ namespace He.PipelineAssessment.Infrastructure.Data
         public DbSet<AssessmentTool> AssessmentTool { get; set; } = default!;
         public DbSet<AssessmentToolInstanceNextWorkflow> AssessmentToolInstanceNextWorkflow { get; set; } = default!;
         public DbSet<AssessmentToolWorkflow> AssessmentToolWorkflow { get; set; } = default!;
-        public DbSet<AssessmentStageViewModel> AssessmentStageViewModel { get; set; } = default!;
-        public DbSet<StartableToolViewModel> StartableToolViewModel { get; set; } = default!;
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-            modelBuilder.Entity<AssessmentStageViewModel>().HasNoKey().ToView(null);
-            modelBuilder.Entity<StartableToolViewModel>().HasNoKey().ToView(null);
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(AssessmentConfiguration).Assembly);
         }
     }
