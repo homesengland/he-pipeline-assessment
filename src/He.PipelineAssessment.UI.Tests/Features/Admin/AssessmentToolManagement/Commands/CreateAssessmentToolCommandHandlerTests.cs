2b564bc24c0800be5a43f96b8e4e654ded6c252f
MSarwar <122261875+MSarwarDev@users.noreply.github.com>
2023-02-01 16:10:34 +0000

    testing exception handling

---
+++ src/He.PipelineAssessment.UI.Tests/Features/Admin/AssessmentToolManagement/Commands/CreateAssessmentToolCommandHandlerTests.cs
@@ -0,0 +1,34 @@
+﻿using AutoFixture.Xunit2;
+using He.PipelineAssessment.Common.Tests;
+using He.PipelineAssessment.UI.Features.Admin.AssessmentToolManagement.Commands.CreateAssessmentTool;
+using He.PipelineAssessment.UI.Features.Admin.AssessmentToolManagement.Mappers;
+using Moq;
+using Xunit;
+
+namespace He.PipelineAssessment.UI.Tests.Features.Admin.AssessmentToolManagement.Commands
+{
+    public class CreateAssessmentToolCommandHandlerTests
+    {
+        [Theory]
+        [AutoMoqData]
+        public async Task Handle_ThrowExceptoin_GivenDependencyThrows
+        (
+             [Frozen] Mock<IAssessmentToolMapper> assessmentToolMapper,
+             CreateAssessmentToolCommand createAssessmentToolCommand,
+             Exception exception,
+             CreateAssessmentToolCommandHandler sut 
+         )
+        {
+            //Arrange
+            assessmentToolMapper.Setup(x => x.CreateAssessmentToolCommandToAssessmentTool(createAssessmentToolCommand))
+                .Throws(exception);           
+
+            //Act
+            var result = await Assert.ThrowsAsync<Exception>(() => sut.Handle(createAssessmentToolCommand, CancellationToken.None));
+
+            //Assert          
+            Assert.Equal(exception.Message, result.Message);
+
+        }
+    }
+}
