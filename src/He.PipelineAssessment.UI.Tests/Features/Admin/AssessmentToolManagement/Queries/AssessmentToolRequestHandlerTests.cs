107e8a8318c0741cba2c6a0c9b9c7a0644db53ae
MSarwar <122261875+MSarwarDev@users.noreply.github.com>
2023-02-02 15:05:32 +0000

    unit test WIP

---
+++ src/He.PipelineAssessment.UI.Tests/Features/Admin/AssessmentToolManagement/Queries/AssessmentToolRequestHandlerTests.cs
@@ -0,0 +1,41 @@
+﻿using AutoFixture.Xunit2;
+using He.PipelineAssessment.Common.Tests;
+using He.PipelineAssessment.Infrastructure.Repository;
+using He.PipelineAssessment.Models;
+using He.PipelineAssessment.UI.Features.Admin.AssessmentToolManagement.Mappers;
+using He.PipelineAssessment.UI.Features.Admin.AssessmentToolManagement.Queries.GetAssessmentTools;
+using He.PipelineAssessment.UI.Features.Assessment.AssessmentList;
+using He.PipelineAssessment.UI.Features.Assessment.AssessmentSummary;
+using Moq;
+using Xunit;
+
+namespace He.PipelineAssessment.UI.Tests.Features.Admin.AssessmentToolManagement.Queries
+{
+    public class AssessmentToolRequestHandlerTests
+    {
+        [Theory]
+        [AutoMoqData]
+        public async Task Handle_ReturnsNull_GivenRepoReturnNull(
+            [Frozen] Mock<IAdminAssessmentToolRepository> adminAssessmentToolRepository,
+            [Frozen] Mock<IAssessmentToolMapper> assessmentToolMapper,
+            AssessmentToolRequest request,
+            AssessmentToolListData assessmentToolListData,
+            AssessmentToolRequestHandler sut
+        )
+        {
+            //Arrange  
+            var emptyAssessmentTool = Enumerable.Empty<AssessmentTool>();
+            adminAssessmentToolRepository.Setup(x => x.GetAssessmentTools()).ReturnsAsync(emptyAssessmentTool);
+            assessmentToolListData = new AssessmentToolListData();
+            // assessmentToolMapper.Setup(x => x.AssessmentToolsToAssessmentToolData(emptyAssessmentTool.ToList())).Returns(assessmentToolListData);
+
+            //Act
+            var result = await sut.Handle(request, CancellationToken.None);
+
+            //Assert
+            //Assert.IsType<AssessmentToolListData>(result);
+            Assert.Equal(assessmentToolListData.AssessmentTools, result.AssessmentTools);           
+           
+        }
+    }
+}
