3e941de71836e7fc0be5c730b6c1c324ff25a99f
MSarwar <122261875+MSarwarDev@users.noreply.github.com>
2023-02-03 09:39:17 +0000

    unit test for mapper

---
+++ src/He.PipelineAssessment.UI.Tests/Features/Admin/AssessmentToolManagement/Mappers/AssessmentToolWorkflowMapperTests.cs
@@ -0,0 +1,33 @@
+﻿using He.PipelineAssessment.Common.Tests;
+using He.PipelineAssessment.UI.Features.Admin.AssessmentToolManagement.Commands.CreateAssessmentToolWorkflow;
+using He.PipelineAssessment.UI.Features.Admin.AssessmentToolManagement.Mappers;
+using He.PipelineAssessment.UI.Features.Workflow.QuestionScreenSaveAndContinue;
+using Xunit;
+
+namespace He.PipelineAssessment.UI.Tests.Features.Admin.AssessmentToolManagement.Mappers
+{
+    public class AssessmentToolWorkflowMapperTests
+    {
+        [Theory]
+        [AutoMoqData]
+        public void CreateAssessmentToolWorkflowCommandToAssessmentToolWorkflow_Returns(
+           CreateAssessmentToolWorkflowCommand assessmentToolWorkflowCommand,
+           AssessmentToolWorkflowMapper sut
+       )
+        {
+            //Arrange
+
+            //Act
+            var result = sut.CreateAssessmentToolWorkflowCommandToAssessmentToolWorkflow(assessmentToolWorkflowCommand);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.Equal(assessmentToolWorkflowCommand.AssessmentToolId, result.AssessmentToolId);
+            Assert.Equal(assessmentToolWorkflowCommand.IsFirstWorkflow, result.IsFirstWorkflow);
+            Assert.Equal(assessmentToolWorkflowCommand.Version, result.Version);
+            Assert.Equal(assessmentToolWorkflowCommand.IsLatest, result.IsLatest);
+            Assert.Equal(assessmentToolWorkflowCommand.WorkflowDefinitionId, result.WorkflowDefinitionId);
+            Assert.Equal(assessmentToolWorkflowCommand.Name, result.Name);
+        }
+    }
+}
