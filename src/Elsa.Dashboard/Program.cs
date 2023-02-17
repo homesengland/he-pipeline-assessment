b75284c90252a40b9274c601df25c5a40455dff3
Khalid Usman <123958491+kusman83@users.noreply.github.com>
2023-02-17 14:29:24 +0000

    Authentication | #52739 - Add authentication configuration to Dashboard

--- src/Elsa.Dashboard/Program.cs
+++ src/Elsa.Dashboard/Program.cs
@@ -1,56 +1,52 @@
 using Elsa.CustomInfrastructure.Data;
 using Elsa.CustomInfrastructure.Extensions;
 using Elsa.Dashboard;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.DataProtection;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// For Authentication
-builder.AddCustomAuth0Configuration();
-//builder.Services.AddCustomAuthentication();
-
-builder.Services.AddAuthorization(options =>
-options.FallbackPolicy = options.DefaultPolicy
-); ;
+var elsaCustomConnectionString = builder.Configuration.GetConnectionString("ElsaCustom");
 
 // For Dashboard.
 builder.Services.AddRazorPages();
+
 builder.Services.AddScoped<NonceConfig>();
+
+builder.Services.AddDbContext<ElsaCustomContext>(config =>
+    config.UseSqlServer(
+        elsaCustomConnectionString,
+        x => x.MigrationsAssembly("Elsa.CustomInfrastructure")));
+
 builder.Services.AddScoped<DbContext>(provider => provider.GetRequiredService<ElsaCustomContext>());
 builder.Services.AddDataProtection().PersistKeysToDbContext<ElsaCustomContext>();
 
-
+// For Authentication
+builder.AddCustomAuth0Configuration();
+builder.Services.AddCustomAuthentication();
 
 var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseExceptionHandler("/Error"); // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseMiddleware<SecurityHeaderMiddleware>();
 
-app
-    .UseStaticFiles()
-        .UseStaticFiles(new StaticFileOptions
-        {
-          FileProvider = new Microsoft.Extensions.FileProviders.PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), @"www")),
-          ServeUnknownFileTypes = true,
-          RequestPath = "/static"
-        })// For Dashboard.
-
+app.UseStaticFiles()
+    .UseStaticFiles(new StaticFileOptions
+    {
+      FileProvider = new Microsoft.Extensions.FileProviders.PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), @"www")),
+      ServeUnknownFileTypes = true,
+      RequestPath = "/static"
+    }) // For Dashboard.
     .UseRouting()
+    .UseDirectoryBrowser()
     .UseAuthentication()
     .UseAuthorization()
-    .UseDirectoryBrowser()
 
-  //app.MapControllerRoute(
-  //    name: "default",
-  //    pattern: "{controller=Home}/{action=Index}/{id?}");
   .UseEndpoints(endpoints =>
   {
     // Elsa API Endpoints are implemented as regular ASP.NET Core API controllers.
@@ -61,21 +57,4 @@ app
     endpoints.MapFallbackToPage("/_Host");
   });
 
-
-
-
-
 app.Run();
-
-//public static class AppRole
-//{
-
-//  public const string PipelineAdmin = "Pipeline.Admin";
-//  public const string PipelineAssessor = "Pipeline.Assessor";
-//}
-
-//public static class AuthorizationPolicies
-//{
-//  public const string AssignmentToPipelineAssessorRoleRequired = "AssignmentToPipelineAssessorRoleRequired";
-//  public const string AssignmentToPipelineAdminRoleRequired = "AssignmentToPipelineAdminRoleRequired";
-//}
