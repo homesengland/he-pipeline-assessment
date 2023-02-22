160091e7e2a1ee9d139991d1435a1f979910698e
Jerry Zajac <32240008+jarekzaj@users.noreply.github.com>
2023-02-22 11:07:25 +0000

    disabling auth for elsa dashboard temporarily

--- src/Elsa.Dashboard/Program.cs
+++ src/Elsa.Dashboard/Program.cs
@@ -1,5 +1,4 @@
 using Elsa.CustomInfrastructure.Data;
-using Elsa.CustomInfrastructure.Extensions;
 using Elsa.Dashboard;
 using Microsoft.AspNetCore.DataProtection;
 using Microsoft.EntityFrameworkCore;
@@ -22,8 +21,8 @@ builder.Services.AddScoped<DbContext>(provider => provider.GetRequiredService<El
 builder.Services.AddDataProtection().PersistKeysToDbContext<ElsaCustomContext>();
 
 // For Authentication
-builder.AddCustomAuth0Configuration();
-builder.Services.AddCustomAuthentication();
+//builder.AddCustomAuth0Configuration();
+//builder.Services.AddCustomAuthentication();
 
 var app = builder.Build();
 
