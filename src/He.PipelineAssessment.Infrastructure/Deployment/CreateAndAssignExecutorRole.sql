4664a5ecd46635e60997f588102082d48a17b705
jarekzaj <32240008+jarekzaj@users.noreply.github.com>
2023-03-01 17:10:56 +0000

    Feature/question screen/refactor (#98)
    Fix for db_executor role 
    
    Co-authored-by: Jerry Zajac 32240008+jarekzaj@users.noreply.github.com
    Co-authored-by: Jonny T 90678699+Jonnythan-t@users.noreply.github.com
    Co-authored-by: Ben Hall 3179852+benbhall@users.noreply.github.com
    Co-authored-by: Lorna B 93874097+lornacbirchall@users.noreply.github.com
    Co-authored-by: Rob Ryan 112186767+rryan0088@users.noreply.github.com
    Co-authored-by: Muhammad Sarwar Dev 122261875+MSarwarDev@users.noreply.github.com
    Co-authored-by: Khalid Usman 123958491+kusman83@users.noreply.github.com

---
+++ src/He.PipelineAssessment.Infrastructure/Deployment/CreateAndAssignExecutorRole.sql
@@ -0,0 +1,12 @@
+﻿IF NOT EXISTS(SELECT top 1 * FROM sys.database_principals  WHERE [type] = N'R' AND [name] = 'db_executor')
+BEGIN
+	CREATE ROLE db_executor
+	GRANT EXECUTE TO db_executor
+END
+
+IF EXISTS(SELECT TOP 1 * FROM sys.database_principals  WHERE [type] = N'E' AND [name] like 'mid-pipasmt-%-01')
+BEGIN
+	DECLARE @UserName as varchar(100)
+	SET @UserName = (SELECT TOP 1 [name] FROM sys.database_principals  WHERE [type] = N'E' AND [name] like 'mid-pipasmt-%-01')
+	EXEC sp_addrolemember 'db_executor', @UserName
+END
