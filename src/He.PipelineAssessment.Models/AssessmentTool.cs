efdfc2f39eaf7944a17881d280ac097ec24497da
Lorna Birchall <200~93874097+lornacbirchall@users.noreply.github.com>
2023-03-08 14:00:32 +0000

    Add AuditableEntity base class.

--- src/He.PipelineAssessment.Models/AssessmentTool.cs
+++ src/He.PipelineAssessment.Models/AssessmentTool.cs
@@ -1,12 +1,10 @@
 ﻿namespace He.PipelineAssessment.Models
 {
-    public class AssessmentTool
+    public class AssessmentTool : AuditableEntity
     {
         public int Id { get; set; }
         public string Name { get; set; } = null!;
         public bool IsVisible { get; set; }
-        public DateTime CreatedDate { get; set; }
-        public DateTime LastModified { get; set; }
         public int Order { get; set; }
 
         public virtual List<AssessmentToolWorkflow>? AssessmentToolWorkflows { get; set; }
---
+++ src/He.PipelineAssessment.Models/IAuditable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace He.PipelineAssessment.Models
+{
+    public abstract class AuditableEntity
+    {
+        public DateTime CreatedDate { get; set; }
+
+        public string CreatedBy { get; set; }
+
+        public DateTime? LastModifiedDate { get; set; }
+
+        public string? LastModifiedBy { get; set; }
+    }
+}
