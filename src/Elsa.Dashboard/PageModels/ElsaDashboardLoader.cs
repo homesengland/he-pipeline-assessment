ee2d4e6e7726a88206a6b469769f3372968c3a74
Jonnythan-t <90678699+Jonnythan-t@users.noreply.github.com>
2023-03-09 11:18:56 +0000

    Feature/43011 setup access policies (#103)
    * Added Authorization  to Elsa Dashboard
    
    * Added Initial Authorization  to Pipeline Assessment
    
    ---------
    
    Co-authored-by: Khalid Usman <123958491+kusman83@users.noreply.github.com>
    Co-authored-by: Jerry Zajac <32240008+jarekzaj@users.noreply.github.com>
    Co-authored-by: Lorna Birchall <200~93874097+lornacbirchall@users.noreply.github.com>

--- src/Elsa.Dashboard/PageModels/ElsaDashboardLoader.cs
+++ src/Elsa.Dashboard/PageModels/ElsaDashboardLoader.cs
@@ -1,20 +1,18 @@
 using Elsa.CustomWorkflow.Sdk.HttpClients;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Net;
-using System.Net.Sockets;
-using System.Net.WebSockets;
-using System.Reflection.Metadata.Ecma335;
 
 namespace Elsa.Dashboard.PageModels
 {
+  [Authorize]
   public class ElsaDashboardLoader : PageModel
   {
     private string _serverUrl { get; set; }
     private IElsaServerHttpClient _client { get; set; }
 
-    private ILogger<ElsaDashboardLoader> _logger {get;set;}
-    
+    private ILogger<ElsaDashboardLoader> _logger { get; set; }
+
     public string? JsonResponse { get; set; }
 
     public ElsaDashboardLoader(IElsaServerHttpClient client, IConfiguration configuration, ILogger<ElsaDashboardLoader> logger)
---
+++ src/He.PipelineAssessment.UI/Authorization/Constants.cs
@@ -0,0 +1,23 @@
+﻿namespace He.PipelineAssessment.UI.Authorization
+{
+    public class Constants
+    {
+        public static class AppRole
+        {
+            public const string PipelineObserver = "PipelineAssessment.Observer";
+            public const string PipelineProjectManager = "PipelineAssessment.ProjectManager";
+            public const string PipelineAssessorMPP = "PipelineAssessment.AssessorMPP";
+            public const string PipelineAssessorInvestment = "PipelineAssessment.AssessorInvestment";
+            public const string PipelineAssessorDevelopment = "PipelineAssessment.AssessorDevelopment";
+            public const string PipelineAdminOperations = "PipelineAssessment.AdminOperations";
+            public const string PipelineAdminBuild = "PipelineAssessment.AdminBuild";
+            public const string PipelineEconomist = "PipelineAssessment.Economist";
+        }
+
+        public static class AuthorizationPolicies
+        {
+            public const string AssignmentToPipelineAdminRoleRequired = "AssignmentToPipelineAdminRoleRequired";
+            public const string AssignmentToPipelineViewAssessmentRoleRequired = "AssignmentToPipelineViewAssessmentRoleRequired";
+        }
+    }
+}
