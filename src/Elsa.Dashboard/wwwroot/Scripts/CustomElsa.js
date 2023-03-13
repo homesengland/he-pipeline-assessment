12cb9e0ab384404bd0d5a72bd53972fa7b341b1d
Rob Ryan <112186767+rryan0088@users.noreply.github.com>
2023-03-13 17:02:18 +0000

    Remove commented out code.

--- src/Elsa.Dashboard/wwwroot/Scripts/CustomElsa.js
+++ src/Elsa.Dashboard/wwwroot/Scripts/CustomElsa.js
@@ -22,8 +22,6 @@ export function InitCustomElsa(elsaStudioRoot, customProperties) {
     elsaStudio.propertyDisplayManager.addDriver(CustomPropertyUIHints.TextActivityBuilder,
       () => new TextActivityDriver(elsaStudio, CustomComponentTags.TextActivity));
 
-    //elsaStudio.propertyDisplayManager.addDriver(CustomPropertyUIHints.ConditionalTextListBuilder,
-    //  () => new ConditionalTextListDriver(elsaStudio, CustomComponentTags.ConditionalTextList));
 
     elsaStudio.propertyDisplayManager.addDriver(CustomPropertyUIHints.CustomTextBuilder,
       () => new CustomTextDriver(elsaStudio, CustomComponentTags.CustomTextArea));
