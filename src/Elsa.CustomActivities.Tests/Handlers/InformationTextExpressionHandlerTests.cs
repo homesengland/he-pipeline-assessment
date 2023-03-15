196d0fb7ab976d1b16cb8db1508db4bb7e90f078
Lorna Birchall <200~93874097+lornacbirchall@users.noreply.github.com>
2023-03-15 15:11:53 +0000

    Information text expression handler tests started.

--- src/Elsa.CustomActivities.Tests/Handlers/InformationTextExpressionHandlerTests.cs
+++ src/Elsa.CustomActivities.Tests/Handlers/InformationTextExpressionHandlerTests.cs
@@ -1,11 +1,14 @@
 ﻿using Elsa.CustomActivities.Activities.Common;
 using Elsa.CustomActivities.Constants;
 using Elsa.CustomActivities.Handlers.Models;
+using Elsa.CustomActivities.Handlers.ParseModels;
 using Elsa.CustomActivities.Handlers.Syntax;
 using Elsa.Expressions;
 using Elsa.Services.Models;
+using Fluid.Values;
 using He.PipelineAssessment.Tests.Common;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Primitives;
 using Moq;
 using Newtonsoft.Json;
 using System;
@@ -29,6 +32,13 @@ namespace Elsa.CustomActivities.Tests.Handlers
             var context = new ActivityExecutionContext(provider.Object, default!, default!, default!, default, default);
             List<ElsaProperty> informationTextProperties = new List<ElsaProperty>();
 
+            var sampleElseProperty1 = SampleElsaProperty(GetDictionary(SyntaxNames.Literal, "A piece of text 1"), SyntaxNames.Literal, "Text A");
+
+            var sampleElseProperty2 = SampleElsaProperty(GetDictionary(SyntaxNames.JavaScript, "A piece of text 2"), SyntaxNames.JavaScript, "Text B");
+            informationTextProperties.Add(sampleElseProperty1);
+            informationTextProperties.Add(sampleElseProperty2);
+
+            string expressionString = JsonConvert.SerializeObject(informationTextProperties);
 
             //Act
 
@@ -37,6 +47,31 @@ namespace Elsa.CustomActivities.Tests.Handlers
             //Assert
 
 
+        }
+
+        private Dictionary<string, string> GetDictionary(string defaultSyntax, 
+            string defaultValue, 
+            string hyperlinkValue = "false", 
+            string urlValue = "",
+            string paragraphValue = "true", 
+            string guidanceValue = "false",
+            string conditionValue = "true")
+        {
+
+            return new Dictionary<string, string>()
+            {
+                {defaultSyntax, defaultValue},
+                {TextActivitySyntaxNames.Paragraph, paragraphValue },
+                {TextActivitySyntaxNames.Url, urlValue},
+                {TextActivitySyntaxNames.Hyperlink, hyperlinkValue},
+                {TextActivitySyntaxNames.Guidance, guidanceValue},
+                {CustomSyntaxNames.Condition, conditionValue},
+            };
+        }
+
+        private ElsaProperty SampleElsaProperty(Dictionary<string, string> expressions, string syntax, string name)
+        {
+            return new ElsaProperty(expressions, syntax, "", name);
         }
     }
 }
