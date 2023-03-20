2b1580ab1c97627db2b6da303d1c1ffac48d4ce8
Rob Ryan <112186767+rryan0088@users.noreply.github.com>
2023-03-20 14:34:20 +0000

    Fix for explicit properties on IOptions

--- src/Elsa.CustomActivities/OptionsProviders/Options.cs
+++ src/Elsa.CustomActivities/OptionsProviders/Options.cs
@@ -3,13 +3,13 @@
     public interface IOptions
     {
         public string Data { get; set; }
-        public string Metadata { get; set; }
+        public string? Metadata { get; set; }
     }
 
     public class JsonOptions : IOptions
     {
         public string Data { get; set; } = null!;
-        public string Metadata { get; set; } = null!;
+        public string? Metadata { get; set; }
     }
 
     public class PotScoreOptions : IOptions
