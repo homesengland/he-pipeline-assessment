b76ea5b021139543bada315cfc220cc1a7f948f0
Rob Ryan <112186767+rryan0088@users.noreply.github.com>
2023-04-03 13:39:08 +0100

    Work on setting up the helper method.

--- src/Elsa.CustomActivities/Activities/QuestionScreen/Helpers/CheckboxQuestionHelper.cs
+++ src/Elsa.CustomActivities/Activities/QuestionScreen/Helpers/CheckboxQuestionHelper.cs
@@ -141,6 +141,36 @@ namespace Elsa.CustomActivities.Activities.QuestionScreen.Helpers
             return result;
         }
 
+        public async Task<int> Count(string workflowInstanceId, string workflowName, string activityName, string questionId)
+        {
+            int result = -1;
+            var workflowBlueprint = await _workflowRegistry.FindByNameAsync(workflowName, Models.VersionOptions.Published);
+
+            if (workflowBlueprint != null)
+            {
+                var activity = workflowBlueprint.Activities.FirstOrDefault(x => x.Name == activityName);
+                if (activity != null)
+                {
+
+                    var questionScreenAnswer = await _elsaCustomRepository.GetQuestionScreenAnswer(activity.Id, workflowInstanceId, questionId, CancellationToken.None);
+                    if (questionScreenAnswer != null &&
+                        questionScreenAnswer.QuestionType == QuestionTypeConstants.CheckboxQuestion)
+                    {
+                        var choices = questionScreenAnswer.Choices;
+                        if (choices != null)
+                        {
+                            var answerList = JsonSerializer.Deserialize<string[]>(questionScreenAnswer.Answer!);
+                            if (answerList != null)
+                            {
+                                result = answerList.Count();
+                            }
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+
         public Task Handle(EvaluatingJavaScriptExpression notification, CancellationToken cancellationToken)
         {
             var activityExecutionContext = notification.ActivityExecutionContext;
@@ -148,6 +178,7 @@ namespace Elsa.CustomActivities.Activities.QuestionScreen.Helpers
             engine.SetValue("checkboxQuestionGetAnswer", (Func<string, string, string, string?>)((workflowName, activityName, questionId) => GetAnswer(activityExecutionContext.WorkflowInstance.Id, workflowName, activityName, questionId).Result));
             engine.SetValue("checkboxQuestionAnswerEquals", (Func<string, string, string, string[], bool>)((workflowName, activityName, questionId, choiceIdsToCheck) => AnswerEquals(activityExecutionContext.WorkflowInstance.Id, workflowName, activityName, questionId, choiceIdsToCheck).Result));
             engine.SetValue("checkboxQuestionAnswerContains", (Func<string, string, string, string[], bool>)((workflowName, activityName, questionId, choiceIdsToCheck) => AnswerContains(activityExecutionContext.WorkflowInstance.Id, workflowName, activityName, questionId, choiceIdsToCheck).Result));
+            engine.SetValue("checkboxQuestionCount", (Func<string, string, string, int>)((workflowName, activityName, questionId) => Count(activityExecutionContext.WorkflowInstance.Id, workflowName, activityName, questionId).Result));
             return Task.CompletedTask;
         }
 
@@ -157,6 +188,7 @@ namespace Elsa.CustomActivities.Activities.QuestionScreen.Helpers
             output.AppendLine("declare function checkboxQuestionGetAnswer(workflowName: string, activityName:string, questionId:string ): string;");
             output.AppendLine("declare function checkboxQuestionAnswerEquals(workflowName: string, activityName:string, questionId:string, choiceIdsToCheck:string[] ): boolean;");
             output.AppendLine("declare function checkboxQuestionAnswerContains(workflowName: string, activityName:string, questionId:string, choiceIdsToCheck:string[]  ): boolean;");
+            output.AppendLine("declare function checkboxQuestionCount(workflowName: string, activityName:string, questionId:string ): int;");
             return Task.CompletedTask;
         }
     }
