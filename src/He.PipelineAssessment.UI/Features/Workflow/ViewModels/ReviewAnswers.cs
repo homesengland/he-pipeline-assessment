821f30ed220c1bc9f2a3d51f3277235c0f250dde
Jerry Zajac <32240008+jarekzaj@users.noreply.github.com>
2023-04-04 13:37:50 +0100

    fixed displaying answers on confirmation and check your answers screens - crerated shared partial view removed dot from confirmation screen footer

---
+++ src/He.PipelineAssessment.UI/Features/Workflow/ViewModels/ReviewAnswers.cs
@@ -0,0 +1,11 @@
+﻿using Elsa.CustomModels;
+
+namespace He.PipelineAssessment.UI.Features.Workflow.ViewModels
+{
+    public class ReviewAnswers
+    {
+        public List<Question>? Questions { get; set; }
+        public bool RenderChangeLinks { get; set; }
+        public string? WorkflowInstanceId { get; set; }
+    }
+}
