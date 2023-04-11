4bad8fb7e725303015439e93af2ae6ee2f3f2b71
Jerry Zajac <32240008+jarekzaj@users.noreply.github.com>
2023-04-11 11:22:58 +0100

    added a couple of SetResultRequestHandlers tests

---
+++ src/He.PipelineAssessment.UI.Tests/Features/Workflow/SetResult/SetResultRequestHandlerTests.cs
@@ -0,0 +1,58 @@
+﻿using AutoFixture.Xunit2;
+using Elsa.CustomWorkflow.Sdk.HttpClients;
+using Elsa.CustomWorkflow.Sdk.Models.Workflow;
+using He.PipelineAssessment.Tests.Common;
+using He.PipelineAssessment.UI.Features.Workflow.QuestionScreenSaveAndContinue;
+using He.PipelineAssessment.UI.Features.Workflow.SetResult;
+using Moq;
+using Xunit;
+
+namespace He.PipelineAssessment.UI.Tests.Features.Workflow.SetResult
+{
+    public class SetResultRequestHandlerTests
+    {
+        [Theory]
+        [AutoMoqData]
+        public async Task Handle_ReturnsNull_GivenHttpClientResponseIsNull(
+            [Frozen] Mock<IElsaServerHttpClient> elsaServerHttpClient,
+            SetResultRequest request,
+            SetResultRequestHandler sut)
+        {
+            //Arrange
+
+            elsaServerHttpClient.Setup(x => x.SetResult(It.IsAny<LoadWorkflowActivityDto>()))
+                .ReturnsAsync((WorkflowActivityDataDto?)null);
+
+            //Act
+            var result = await sut.Handle(request, CancellationToken.None);
+
+            //Assert
+            Assert.Null(result);
+            elsaServerHttpClient.Verify(x => x.SetResult(It.IsAny<LoadWorkflowActivityDto>()), Times.Once);
+        }
+
+        [Theory]
+        [AutoMoqData]
+        public async Task Handle_ReturnsQuestionScreenSaveAndContinueCommandResponse_GivenNoErrorsEncountered(
+            [Frozen] Mock<IElsaServerHttpClient> elsaServerHttpClient,
+            SetResultRequest request,
+            WorkflowActivityDataDto workflowActivityDataDto,
+            SetResultRequestHandler sut)
+        {
+            //Arrange
+            elsaServerHttpClient.Setup(x => x.SetResult(It.IsAny<LoadWorkflowActivityDto>()))
+                .ReturnsAsync(workflowActivityDataDto);
+
+            //Act
+            var result = await sut.Handle(request, CancellationToken.None);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.IsType<QuestionScreenSaveAndContinueCommandResponse>(result);
+            Assert.Equal(workflowActivityDataDto.Data.ActivityId, result!.ActivityId);
+            Assert.Equal(workflowActivityDataDto.Data.ActivityType, result.ActivityType);
+            Assert.Equal(workflowActivityDataDto.Data.WorkflowInstanceId, result.WorkflowInstanceId);
+            elsaServerHttpClient.Verify(x => x.SetResult(It.IsAny<LoadWorkflowActivityDto>()), Times.Once);
+        }
+    }
+}
--- src/He.PipelineAssessment.UI/Features/Workflow/SetResult/SetResultRequestHandler.cs
+++ src/He.PipelineAssessment.UI/Features/Workflow/SetResult/SetResultRequestHandler.cs
@@ -1,27 +1,22 @@
-﻿using Elsa.CustomWorkflow.Sdk;
+﻿using System.Text.Json;
 using Elsa.CustomWorkflow.Sdk.HttpClients;
 using Elsa.CustomWorkflow.Sdk.Models.Workflow;
-using He.PipelineAssessment.Infrastructure.Repository;
-using He.PipelineAssessment.Models;
 using He.PipelineAssessment.UI.Features.Workflow.QuestionScreenSaveAndContinue;
-using He.PipelineAssessment.UI.Features.Workflow.SetResult;
 using MediatR;
-using System.Text.Json;
 
-namespace He.PipelineAssessment.UI.Features.Workflow.LoadConfirmationScreen
+namespace He.PipelineAssessment.UI.Features.Workflow.SetResult
 {
     public class SetResultRequestHandler : IRequestHandler<SetResultRequest, QuestionScreenSaveAndContinueCommandResponse?>
     {
         private readonly IElsaServerHttpClient _elsaServerHttpClient;
-        private readonly IAssessmentRepository _assessmentRepository;
 
-        public SetResultRequestHandler(IElsaServerHttpClient elsaServerHttpClient, IAssessmentRepository assessmentRepository)
+        public SetResultRequestHandler(IElsaServerHttpClient elsaServerHttpClient)
         {
             _elsaServerHttpClient = elsaServerHttpClient;
-            _assessmentRepository = assessmentRepository;
         }
 
-        public async Task<QuestionScreenSaveAndContinueCommandResponse?> Handle(SetResultRequest request, CancellationToken cancellationToken)
+        public async Task<QuestionScreenSaveAndContinueCommandResponse?> Handle(SetResultRequest request,
+            CancellationToken cancellationToken)
         {
             var response = await _elsaServerHttpClient.SetResult(new LoadWorkflowActivityDto
             {
@@ -33,13 +28,20 @@ namespace He.PipelineAssessment.UI.Features.Workflow.LoadConfirmationScreen
             {
                 string jsonResponse = JsonSerializer.Serialize(response);
                 SetResultResponse? result = JsonSerializer.Deserialize<SetResultResponse>(jsonResponse);
-                QuestionScreenSaveAndContinueCommandResponse nextActivityResponse = new QuestionScreenSaveAndContinueCommandResponse();
-                nextActivityResponse.ActivityId = result.Data.ActivityId;
-                nextActivityResponse.ActivityType = result.Data.ActivityType;
-                nextActivityResponse.WorkflowInstanceId = result.Data.WorkflowInstanceId;
+                if (result != null)
+                {
+                    QuestionScreenSaveAndContinueCommandResponse nextActivityResponse =
+                        new QuestionScreenSaveAndContinueCommandResponse
+                        {
+                            ActivityId = result.Data.ActivityId,
+                            ActivityType = result.Data.ActivityType,
+                            WorkflowInstanceId = result.Data.WorkflowInstanceId
+                        };
                     return await Task.FromResult(nextActivityResponse);
                 }
-            return null;
             }
+
+            return null;
         }
     }
+}
