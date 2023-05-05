19a69e2595bc27b1363a13cf98861adb48df7111
Jerry Zajac <32240008+jarekzaj@users.noreply.github.com>
2023-05-05 11:41:37 +0100

    fix for switch case and remove weighted score activity

--- src/Elsa.CustomActivities/Activities/Scoring/WeightedScore.cs
+++ src/Elsa.CustomActivities/Activities/Scoring/WeightedScore.cs
@@ -1,80 +1,82 @@
-﻿using Elsa.Activities.ControlFlow;
-using Elsa.ActivityResults;
-using Elsa.Attributes;
-using Elsa.CustomActivities.Constants;
-using Elsa.CustomInfrastructure.Data.Repository;
-using Elsa.CustomModels;
-using Elsa.Design;
-using Elsa.Expressions;
-using Elsa.Services;
-using Elsa.Services.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using Elsa.Activities.ControlFlow;
+//using Elsa.ActivityResults;
+//using Elsa.Attributes;
+//using Elsa.CustomActivities.Constants;
+//using Elsa.CustomInfrastructure.Data.Repository;
+//using Elsa.CustomModels;
+//using Elsa.Design;
+//using Elsa.Expressions;
+//using Elsa.Services;
+//using Elsa.Services.Models;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace Elsa.CustomActivities.Activities.Scoring
-{
-    [Trigger(
-        Category = "Pipeline Assessment Scoring",
-        Description = "Schedule an activity to calculate the Weighted Score Outcomes",
-        Outcomes = new[] { OutcomeNames.Done },
-        DisplayName = "Weighted Score"
-        )]
-    public class WeightedScore : Activity
-    {
+//commented it for now, but will keep - unsure whether the intention was to use a separate activity like this one for weighted scores? JZ
 
-        private readonly IElsaCustomRepository _elsaCustomRepository;
-        public WeightedScore(IElsaCustomRepository elsaCustomRepository)
-        {
-            _elsaCustomRepository = elsaCustomRepository;
-        }
+//namespace Elsa.CustomActivities.Activities.Scoring
+//{
+//    [Trigger(
+//        Category = "Pipeline Assessment Scoring",
+//        Description = "Schedule an activity to calculate the Weighted Score Outcomes",
+//        Outcomes = new[] { OutcomeNames.Done },
+//        DisplayName = "Weighted Score"
+//        )]
+//    public class WeightedScore : Activity
+//    {
 
-        public string Calculation { get; set; } = null!;
+//        private readonly IElsaCustomRepository _elsaCustomRepository;
+//        public WeightedScore(IElsaCustomRepository elsaCustomRepository)
+//        {
+//            _elsaCustomRepository = elsaCustomRepository;
+//        }
 
-        [ActivityInput(Label = "Scoring outcome conditions", Hint = "The conditions to evaluate.", UIHint = CustomActivityUIHints.CustomSwitch, DefaultSyntax = "Switch", IsDesignerCritical = true)]
-        public ICollection<SwitchCase> Cases { get; set; } = new List<SwitchCase>();
+//        public string Calculation { get; set; } = null!;
 
-        [ActivityInput(
-            Hint = "The switch mode determines whether the first match should be scheduled, or all matches.",
-            DefaultValue = SwitchMode.MatchFirst,
-            SupportedSyntaxes = new[] { SyntaxNames.Literal, SyntaxNames.JavaScript, SyntaxNames.Liquid }
-        )]
-        public SwitchMode Mode { get; set; } = SwitchMode.MatchFirst;
+//        [ActivityInput(Label = "Scoring outcome conditions", Hint = "The conditions to evaluate.", UIHint = CustomActivityUIHints.CustomSwitch, DefaultSyntax = "Switch", IsDesignerCritical = true)]
+//        public ICollection<SwitchCase> Cases { get; set; } = new List<SwitchCase>();
 
-        [ActivityOutput] public string? Output { get; set; }
+//        [ActivityInput(
+//            Hint = "The switch mode determines whether the first match should be scheduled, or all matches.",
+//            DefaultValue = SwitchMode.MatchFirst,
+//            SupportedSyntaxes = new[] { SyntaxNames.Literal, SyntaxNames.JavaScript, SyntaxNames.Liquid }
+//        )]
+//        public SwitchMode Mode { get; set; } = SwitchMode.MatchFirst;
 
-        protected override async ValueTask<IActivityExecutionResult> OnExecuteAsync(ActivityExecutionContext context)
-        {
-            try
-            {
-                await SetWorkflowCalculationScore();
-                if (Calculation == null || Calculation == string.Empty)
-                {
-                    context.JournalData.Add("Error", "Unable to parse Calculation");
-                    return new SuspendResult();
-                }
-                else
-                {
-                    Output = Calculation;
-                    await _elsaCustomRepository.SetWorkflowInstanceScore(context.WorkflowInstance.Id, Calculation);
-                }
-            }
-            catch (Exception)
-            {
-                context.JournalData.Add("Error", string.Format("Error occured whilst updating workflow with {0} score.", Calculation));
-                return new SuspendResult();
-            }
+//        [ActivityOutput] public string? Output { get; set; }
 
-            return Done();
-        }
+//        protected override async ValueTask<IActivityExecutionResult> OnExecuteAsync(ActivityExecutionContext context)
+//        {
+//            try
+//            {
+//                await SetWorkflowCalculationScore();
+//                if (Calculation == null || Calculation == string.Empty)
+//                {
+//                    context.JournalData.Add("Error", "Unable to parse Calculation");
+//                    return new SuspendResult();
+//                }
+//                else
+//                {
+//                    Output = Calculation;
+//                    await _elsaCustomRepository.SetWorkflowInstanceScore(context.WorkflowInstance.Id, Calculation);
+//                }
+//            }
+//            catch (Exception)
+//            {
+//                context.JournalData.Add("Error", string.Format("Error occured whilst updating workflow with {0} score.", Calculation));
+//                return new SuspendResult();
+//            }
 
-        private async Task SetWorkflowCalculationScore()
-        {
-            await Task.CompletedTask;
-            //TODO
-        }
+//            return Done();
+//        }
 
-    }
-}
+//        private async Task SetWorkflowCalculationScore()
+//        {
+//            await Task.CompletedTask;
+//            //TODO
+//        }
+
+//    }
+//}
