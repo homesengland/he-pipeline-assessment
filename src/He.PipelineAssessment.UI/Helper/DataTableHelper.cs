c2fe3bab074fbbfe21ae94e21e70b9671630f8ce
Lorna Birchall <200~93874097+lornacbirchall@users.noreply.github.com>
2023-05-22 15:49:35 +0100

    Update review screen to display data table information.

---
+++ src/He.PipelineAssessment.UI/Helper/DataTableHelper.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json;
+
+namespace He.PipelineAssessment.UI.Helper;
+
+public static class DataTableHelper
+{
+    public static DataTableModel ToTableModel(this string jsonModel)
+    {
+        var dataTableModel = new DataTableModel();
+        var dataTableRows = JsonSerializer.Deserialize<List<DataTableRow>>(jsonModel);
+        if(dataTableRows != null)
+        {
+            dataTableModel.DataTableRows.AddRange(dataTableRows);
+        }
+        return dataTableModel;
+    }
+
+    public class DataTableModel
+    {
+        public List<DataTableRow> DataTableRows { get; set; } = new List<DataTableRow>();
+    }
+    public class DataTableRow
+    {
+        public string? Title { get; set; }
+        public string? Input { get; set; }
+    }
+}
