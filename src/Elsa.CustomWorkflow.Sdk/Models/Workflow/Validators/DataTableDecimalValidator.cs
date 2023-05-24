41ba62d381a40c798af44aa29fe50f9ae24bc534
Lorna Birchall <200~93874097+lornacbirchall@users.noreply.github.com>
2023-05-24 13:49:32 +0100

    Initial changes for the validator

---
+++ src/Elsa.CustomWorkflow.Sdk/Models/Workflow/Validators/DataTableDecimalValidator.cs
@@ -0,0 +1,33 @@
+﻿using Elsa.CustomModels;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elsa.CustomWorkflow.Sdk.Models.Workflow.Validators
+{
+    public class DataTableDecimalValidator : AbstractValidator<List<TableInput>>
+    {
+        public DataTableDecimalValidator()
+        {
+            RuleFor(x => x).NotEmpty().WithMessage("TO DO")
+                            .DependentRules(
+                () =>
+                {
+                    RuleForEach(x => x).Must(
+                        decimalInput =>
+                        {
+                            if (decimalInput.Input != null)
+                            {
+                                var isNumeric = decimal.TryParse(decimalInput.Input, out _);
+                                return isNumeric;
+                            }
+                            return true;
+                        }).WithMessage("The answer must be a number");
+                }
+                );
+        }
+    }
+}
---
+++ src/Elsa.CustomWorkflow.Sdk/Models/Workflow/Validators/DataTableValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using System.Text.Json;
+
+
+namespace Elsa.CustomWorkflow.Sdk.Models.Workflow.Validators
+{
+    public class DataTableValidator : AbstractValidator<DataTableInput>
+    {
+        public DataTableValidator()
+        {
+            RuleFor(x => x).NotEmpty().WithMessage("TO DO");
+            RuleFor(x => x.Inputs).NotEmpty().WithMessage("TO DO");
+            RuleFor(x => x.Inputs).SetValidator(new DataTableDecimalValidator())
+                .When(x => x.InputType == DataTableInputTypeConstants.CurrencyDataTableInput);
+            RuleFor(x => x.Inputs).SetValidator(new DataTableDecimalValidator())
+            .When(x => x.InputType == DataTableInputTypeConstants.DecimalDataTableInput);
+            RuleFor(x => x.Inputs).SetValidator(new DataTableIntegerValidator())
+            .When(x => x.InputType == DataTableInputTypeConstants.IntegerDataTableInput);
+        }
+    }
+}
