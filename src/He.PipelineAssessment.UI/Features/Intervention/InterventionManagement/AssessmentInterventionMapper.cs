db38551422e69dd3b49627f5dce69cb969c160f5
Jerry Zajac <32240008+jarekzaj@users.noreply.github.com>
2023-06-05 10:44:19 +0100

    removed unused method in assessment intervention mapper

--- src/He.PipelineAssessment.UI/Features/Intervention/InterventionManagement/AssessmentInterventionMapper.cs
+++ src/He.PipelineAssessment.UI/Features/Intervention/InterventionManagement/AssessmentInterventionMapper.cs
@@ -6,8 +6,6 @@ namespace He.PipelineAssessment.UI.Features.Intervention.InterventionManagement
 {
     public interface IAssessmentInterventionMapper
     {
-        AssessmentIntervention AssessmentInterventionFromAssessmentInterventionCommand(AssessmentInterventionCommand command);
-
         AssessmentInterventionCommand AssessmentInterventionCommandFromAssessmentIntervention(AssessmentIntervention intervention);
 
         List<TargetWorkflowDefinition> TargetWorkflowDefinitionsFromAssessmentToolWorkflows(
@@ -18,29 +16,6 @@ namespace He.PipelineAssessment.UI.Features.Intervention.InterventionManagement
 
     public class AssessmentInterventionMapper : IAssessmentInterventionMapper
     {
-
-        public AssessmentIntervention AssessmentInterventionFromAssessmentInterventionCommand(AssessmentInterventionCommand command)
-        {
-            AssessmentIntervention intervention = new AssessmentIntervention()
-            {
-                Id = command.AssessmentInterventionId,
-                AssessmentToolWorkflowInstanceId = command.AssessmentToolWorkflowInstanceId,
-                TargetAssessmentToolWorkflowId = command.TargetWorkflowId,
-                RequestedBy = command.RequestedBy,
-                RequestedByEmail = command.RequestedByEmail,
-                Administrator = command.Administrator,
-                AdministratorEmail = command.AdministratorEmail,
-                SignOffDocument = command.SignOffDocument,
-                DecisionType = command.DecisionType,
-                AssessorRationale = command.AssessorRationale,
-                AdministratorRationale = command.AdministratorRationale,
-                DateSubmitted = command.DateSubmitted,
-                Status = command.Status
-            };
-
-            return intervention;
-        }
-
         public AssessmentInterventionCommand AssessmentInterventionCommandFromAssessmentIntervention(AssessmentIntervention intervention)
         {
             AssessmentInterventionCommand command = new AssessmentInterventionCommand()
@@ -86,7 +61,7 @@ namespace He.PipelineAssessment.UI.Features.Intervention.InterventionManagement
 
             AssessmentInterventionDto dto = new AssessmentInterventionDto()
             {
-                AssessmentInterventionCommand = new CreateOverrideCommand()
+                AssessmentInterventionCommand = new AssessmentInterventionCommand
                 {
                     AssessmentToolWorkflowInstanceId = instance.Id,
                     WorkflowInstanceId = instance.WorkflowInstanceId,
