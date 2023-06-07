c135060fe3152205f108192bbdfdc85826b1137a
jarekzaj <32240008+jarekzaj@users.noreply.github.com>
2023-06-07 16:05:57 +0100

    Bugfix/unable to delete tools (#152)
    * fixed a bug where an assessment tool could not be deleted due to foreign key constraint on assessment intervention
    added deleted status to assessment tool and assessment tool workflow
    added logic to update LastUpdated on AuditableEntity creation
    
    Co-authored-by: Jerry Zajac 32240008+jarekzaj@users.noreply.github.com
    Co-authored-by: Jonny T 90678699+Jonnythan-t@users.noreply.github.com
    Co-authored-by: Ben Hall 3179852+benbhall@users.noreply.github.com
    Co-authored-by: Lorna B 93874097+lornacbirchall@users.noreply.github.com
    Co-authored-by: Rob Ryan 112186767+rryan0088@users.noreply.github.com
    Co-authored-by: Khalid Usman 123958491+kusman83@users.noreply.github.com

---
+++ src/He.PipelineAssessment.Infrastructure/Migrations/20230607125835_AddesdStatusToAssessmentToolsAndAssessmentToolWorkflows.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace He.PipelineAssessment.Infrastructure.Migrations
+{
+    public partial class AddesdStatusToAssessmentToolsAndAssessmentToolWorkflows : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "AssessmentToolWorkflow",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "AssessmentTool",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "AssessmentToolWorkflow")
+                .Annotation("SqlServer:IsTemporal", true)
+                .Annotation("SqlServer:TemporalHistoryTableName", "AssessmentToolWorkflowHistory")
+                .Annotation("SqlServer:TemporalHistoryTableSchema", null);
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "AssessmentTool")
+                .Annotation("SqlServer:IsTemporal", true)
+                .Annotation("SqlServer:TemporalHistoryTableName", "AssessmentToolHistory")
+                .Annotation("SqlServer:TemporalHistoryTableSchema", null);
+        }
+    }
+}
