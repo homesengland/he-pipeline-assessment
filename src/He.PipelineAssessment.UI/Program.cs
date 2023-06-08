7be0244431efaecf544213a059b73fc1200cf14b
Jerry Zajac <32240008+jarekzaj@users.noreply.github.com>
2023-06-08 16:41:40 +0100

    removed registration of error middleware from program cs

--- src/He.PipelineAssessment.UI/Program.cs
+++ src/He.PipelineAssessment.UI/Program.cs
@@ -128,8 +128,6 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 }
 
-app.UseMiddleware<ErrorHandlerMiddleware>();
-
 app.Use((context, next) =>
 {
     context.Request.Scheme = "https";
