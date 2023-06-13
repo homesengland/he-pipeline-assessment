4c5a72560b832e9f038dfd72d10f7bcefec60988
Jonny T <90678699+Jonnythan-t@users.noreply.github.com>
2023-06-13 10:25:02 +0100

    updated status filter to include null

--- src/He.PipelineAssessment.Infrastructure/Migrations/20230613090638_GetAssessmentsUpdateDeleted/GetAssessments.sql
+++ src/He.PipelineAssessment.Infrastructure/Migrations/20230613090638_GetAssessmentsUpdateDeleted/GetAssessments.sql
@@ -22,8 +22,8 @@ BEGIN
 	ISNULL((
 		select top 1 at.Name 
 		from [AssessmentToolWorkflowInstance] atwi 
-		inner join AssessmentToolWorkflow atw on atw.WorkflowDefinitionId = atwi.WorkflowDefinitionId and atw.Status !=''Deleted''
-		inner join AssessmentTool at on at.Id = atw.AssessmentToolId and at.Status !=''Deleted''
+		inner join AssessmentToolWorkflow atw on atw.WorkflowDefinitionId = atwi.WorkflowDefinitionId and (atw.Status !=''Deleted'' OR atw.Status is null)
+		inner join AssessmentTool at on at.Id = atw.AssessmentToolId and (at.Status !=''Deleted'' OR at.Status is null)
 		where a.Id = atwi.AssessmentId 
 		order by atwi.[LastModifiedDateTime] desc
 	),''New'') as [Status]
