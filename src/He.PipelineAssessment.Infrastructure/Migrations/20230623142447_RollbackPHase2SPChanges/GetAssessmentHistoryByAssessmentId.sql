b2220e9c4f7934fcf62c6edc816f5bf4655b0f4c
Jonny T <90678699+Jonnythan-t@users.noreply.github.com>
2023-06-23 15:30:09 +0100

    added migrations for stored proc changes

---
+++ src/He.PipelineAssessment.Infrastructure/Migrations/20230623142447_RollbackPHase2SPChanges/GetAssessmentHistoryByAssessmentId.sql
@@ -0,0 +1,29 @@
+﻿CREATE OR ALTER   PROCEDURE [dbo].[GetAssessmentHistoryByAssessmentId]
+    @assessmentId int
+AS
+BEGIN
+     SELECT 
+		AT.IsVisible, 
+		AT.[Order], 
+		AT.Name +'' - ''+ ATWFI.WorkflowName as [Name],
+		ATWFI.WorkflowName, 
+		ATWFI.WorkflowDefinitionId, 
+		ATWFI.WorkflowInstanceId,
+		ATWFI.CurrentActivityId, 
+		ATWFI.CurrentActivityType, 
+		ATWFI.[Status], 
+		ATWFI.CreatedDateTime,
+		ATWFI.SubmittedDateTime, 
+		ATWFI.Id as AssessmentToolWorkflowInstanceId, 
+		ATWF.AssessmentToolID, 
+		ATWF.IsFirstWorkflow, 
+		ATWFI.LastModifiedDateTime,
+		ATWFI.Result,
+		ATWFI.SubmittedBy
+        FROM AssessmentToolWorkFlowInstance ATWFI 
+        INNER JOIN AssessmentToolWorkflow ATWF ON ATWFI.AssessmentToolWorkflowId = AtWF.Id 	AND (ATWF.Status IS NULL OR ATWF.Status != ''Deleted'')
+		INNER JOIN AssessmentTool AT ON AT.Id = ATWF.AssessmentToolId 
+        WHERE 
+		ATWFI.AssessmentId = @assessmentId  AND
+		(ATWFI.Status != ''Draft'' AND ATWFI.Status != ''Submitted'')
+END
