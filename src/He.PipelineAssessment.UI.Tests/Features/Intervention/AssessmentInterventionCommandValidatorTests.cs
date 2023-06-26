66ff8d43af63e8dd42306c695a46bd41e17c6982
Jonny T <90678699+Jonnythan-t@users.noreply.github.com>
2023-06-26 11:37:03 +0100

    updated unit tests

--- src/He.PipelineAssessment.UI.Tests/Features/Intervention/AssessmentInterventionCommandValidatorTests.cs
+++ src/He.PipelineAssessment.UI.Tests/Features/Intervention/AssessmentInterventionCommandValidatorTests.cs
@@ -2,29 +2,33 @@
 using Xunit;
 using He.PipelineAssessment.Tests.Common;
 using He.PipelineAssessment.UI.Features.Intervention;
+using Azure.Core;
 
 namespace He.PipelineAssessment.UI.Tests.Features.Intervention
 {
     public class AssessmentInterventionCommandValidatorTests
     {
         [Theory]
-        [InlineAutoMoqData("CorrectValue", "CorrectValue", "CorrectValue", null, false, "TargetWorkflowId", "The target workflow definition has to be selected")]
+        [InlineAutoMoqData("CorrectValue", "CorrectValue", "CorrectValue", null,1, false, "TargetWorkflowId", "The target workflow definition has to be selected")]
 
-        [InlineAutoMoqData("", "CorrectValue", "CorrectValue", 1, false, "SignOffDocument", "The Sign Off Document cannot be empty")]
-        [InlineAutoMoqData(null, "CorrectValue", "CorrectValue", 2, false, "SignOffDocument", "The Sign Off Document cannot be empty")]
+        [InlineAutoMoqData("", "CorrectValue", "CorrectValue", 1,2, false, "SignOffDocument", "The Sign Off Document cannot be empty")]
+        [InlineAutoMoqData(null, "CorrectValue", "CorrectValue", 2,3, false, "SignOffDocument", "The Sign Off Document cannot be empty")]
 
-        [InlineAutoMoqData("CorrectValue", "", "CorrectValue", 3, false, "AdministratorRationale", "The Administrator Rationale cannot be empty")]
-        [InlineAutoMoqData("CorrectValue", null, "CorrectValue", 4, false, "AdministratorRationale", "The Administrator Rationale cannot be empty")]
+        [InlineAutoMoqData("CorrectValue", "", "CorrectValue", 3,4, false, "AdministratorRationale", "The Administrator Rationale cannot be empty")]
+        [InlineAutoMoqData("CorrectValue", null, "CorrectValue", 4,5, false, "AdministratorRationale", "The Administrator Rationale cannot be empty")]
 
-        [InlineAutoMoqData("CorrectValue", "CorrectValue", "", 5, false, "AssessorRationale", "The Assessor Rationale cannot be empty")]
-        [InlineAutoMoqData("CorrectValue", "CorrectValue", null, 6, false, "AssessorRationale", "The Assessor Rationale cannot be empty")]
+        [InlineAutoMoqData("CorrectValue", "CorrectValue", "", 5,6, false, "AssessorRationale", "The Assessor Rationale cannot be empty")]
+        [InlineAutoMoqData("CorrectValue", "CorrectValue", null, 6,7, false, "AssessorRationale", "The Assessor Rationale cannot be empty")]
 
-        [InlineAutoMoqData("CorrectValue", "CorrectValue", "CorrectValue", 8, true, null, null)]
+        [InlineAutoMoqData("CorrectValue", "CorrectValue", "CorrectValue", 6, null, false, "InterventionReasonId", "The request reason cannot be empty")]
+
+        [InlineAutoMoqData("CorrectValue", "CorrectValue", "CorrectValue", 8,8, true, null, null)]
         public void AssessmentInterventionCommandValidator_ShouldValidateSingleField_GivenValidationScenarios(
             string? signOffDocument,
             string? administratorRationale,
             string? assessorRationale,
             int? targetWorkflowId,
+            int? interventionReasonId,
             bool validationResult,
             string? errorPropertyName,
             string? validationMessage
@@ -38,7 +42,8 @@ namespace He.PipelineAssessment.UI.Tests.Features.Intervention
                 SignOffDocument = signOffDocument,
                 AdministratorRationale = administratorRationale,
                 TargetWorkflowId = targetWorkflowId,
-                AssessorRationale = assessorRationale
+                AssessorRationale = assessorRationale,
+                InterventionReasonId = interventionReasonId,
             };
 
             //Act
@@ -66,17 +71,19 @@ namespace He.PipelineAssessment.UI.Tests.Features.Intervention
             {
                 SignOffDocument = null,
                 AdministratorRationale = null,
-                TargetWorkflowId = null
+                TargetWorkflowId = null,
+                InterventionReasonId = null
             };
 
             //Act
             TestValidationResult<AssessmentInterventionCommand> result = validator.TestValidate(command);
 
             //Assert
-            Assert.Equal(4, result.Errors.Count);
+            Assert.Equal(5, result.Errors.Count);
             result.ShouldHaveValidationErrorFor(x => x.SignOffDocument).WithErrorMessage("The Sign Off Document cannot be empty");
             result.ShouldHaveValidationErrorFor(x => x.AdministratorRationale).WithErrorMessage("The Administrator Rationale cannot be empty");
             result.ShouldHaveValidationErrorFor(x => x.TargetWorkflowId).WithErrorMessage("The target workflow definition has to be selected");
+            result.ShouldHaveValidationErrorFor(x => x.InterventionReasonId).WithErrorMessage("The request reason cannot be empty");
         }
     }
 }
