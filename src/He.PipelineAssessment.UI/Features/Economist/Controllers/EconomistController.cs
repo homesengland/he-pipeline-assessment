56c5a9d09d386484611f4abf23904e1f4e783ddc
Jonny T <90678699+Jonnythan-t@users.noreply.github.com>
2023-07-10 10:03:48 +0100

    economist bug fix

--- src/He.PipelineAssessment.UI/Features/Economist/Controllers/EconomistController.cs
+++ src/He.PipelineAssessment.UI/Features/Economist/Controllers/EconomistController.cs
@@ -8,20 +8,12 @@ namespace He.PipelineAssessment.UI.Features.Economist.Controllers;
 [Authorize(Policy = Constants.AuthorizationPolicies.AssignmentToWorkflowEconomistRoleRequired)]
 public class EconomistController : BaseController<EconomistController>
 {
-    public EconomistController(IMediator mediator, ILogger<EconomistController> logger) : base(mediator, logger){}
+    public EconomistController(IMediator mediator, ILogger<EconomistController> logger) : base(mediator, logger) { }
 
     [Authorize(Policy = Constants.AuthorizationPolicies.AssignmentToWorkflowEconomistRoleRequired)]
     public async Task<IActionResult> GetEconomistList()
     {
-        try
-        {
-            var listModel = await _mediator.Send(new EconomistAssessmentListCommand());
-            return View("~/Features/Assessment/Views/Index.cshtml", listModel);
-        }
-        catch (Exception e)
-        {
-            _logger.LogError(e.Message);
-            return RedirectToAction("Index", "Error", new { message = e.Message });
-        }
+        var listModel = await _mediator.Send(new EconomistAssessmentListCommand());
+        return View("~/Features/Assessment/Views/Index.cshtml", listModel);
     }
 }
