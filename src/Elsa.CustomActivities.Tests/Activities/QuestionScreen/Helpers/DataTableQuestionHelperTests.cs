716753ecb1e412f96dcd63208289db68f01e3956
Jonny T <90678699+Jonnythan-t@users.noreply.github.com>
2023-07-11 08:44:56 +0100

    added data table helper method to return answers as array

--- src/Elsa.CustomActivities.Tests/Activities/QuestionScreen/Helpers/DataTableQuestionHelperTests.cs
+++ src/Elsa.CustomActivities.Tests/Activities/QuestionScreen/Helpers/DataTableQuestionHelperTests.cs
@@ -566,5 +566,467 @@ namespace Elsa.CustomActivities.Tests.Activities.QuestionScreen.Helpers
             //Assert
             Assert.Equal((decimal)123.45,result);
         }
+
+        [Theory]
+        [AutoMoqData]
+        public async Task GetStringAnswerArray_ReturnsEmptyString_GivenWorkflowFindByNameAsyncReturnsNull(
+            [Frozen] Mock<IElsaCustomRepository> elsaCustomRepository,
+            [Frozen] Mock<IWorkflowRegistry> workflowRegistry,
+            string workflowName,
+            string activityName,
+            string questionId,
+            string correlationId,
+            DataTableQuestionHelper sut)
+        {
+            //Arrange
+            elsaCustomRepository.Setup(x => x.GetQuestionByCorrelationId(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(), CancellationToken.None)).ReturnsAsync((Question?)null);
+
+            workflowRegistry.Setup(x => x.FindByNameAsync(workflowName!, VersionOptions.Published, null, default)).ReturnsAsync((WorkflowBlueprint?)null);
+
+            //Act
+            var result = await sut.GetStringAnswerArray(correlationId, workflowName, activityName, questionId);
+
+            //Assert
+            Assert.Equal(new string[]{}, result);
+        }
+
+        [Theory]
+        [AutoMoqData]
+        public async Task GetStringAnswerArray_ReturnsEmptyString_WorkflowActivitesReturnsNull(
+            [Frozen] Mock<IElsaCustomRepository> elsaCustomRepository,
+            [Frozen] Mock<IWorkflowRegistry> workflowRegistry,
+            string workflowName,
+            string activityName,
+            string questionId,
+            string correlationId,
+            WorkflowBlueprint workflowBlueprint,
+            DataTableQuestionHelper sut)
+        {
+
+            //Arrange
+            elsaCustomRepository.Setup(x => x.GetQuestionByCorrelationId(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(), CancellationToken.None)).ReturnsAsync((Question?)null);
+
+            workflowRegistry.Setup(x => x.FindByNameAsync(workflowName!, VersionOptions.Published, null, default)).ReturnsAsync(workflowBlueprint);
+
+            //Act
+            var result = await sut.GetStringAnswerArray(correlationId, workflowName, activityName, questionId);
+
+            //Assert
+            Assert.Equal(new string[] { }, result);
+        }
+
+        [Theory]
+        [AutoMoqData]
+        public async Task GetStringAnswerArray_ReturnsEmptyString_GetQuestionRecordReturnsNull(
+            [Frozen] Mock<IElsaCustomRepository> elsaCustomRepository,
+            [Frozen] Mock<IWorkflowRegistry> workflowRegistry,
+            string workflowName,
+            string activityId,
+            string activityName,
+            string questionId,
+            string correlationId,
+            WorkflowBlueprint workflowBlueprint,
+            DataTableQuestionHelper sut)
+        {
+            //Arrange
+            workflowBlueprint.Activities.Add(new ActivityBlueprint()
+            {
+                Id = activityId,
+                Name = activityName
+            });
+
+            elsaCustomRepository.Setup(x => x.GetQuestionByCorrelationId(activityId, correlationId, It.IsAny<string>(), CancellationToken.None)).ReturnsAsync((Question?)null);
+
+            workflowRegistry.Setup(x => x.FindByNameAsync(workflowName!, VersionOptions.Published, null, default)).ReturnsAsync(workflowBlueprint);
+
+            //Act
+            var result = await sut.GetStringAnswerArray(correlationId, workflowName, activityName, questionId);
+
+            //Assert
+            Assert.Equal(new string[] { }, result);
+        }
+
+        [Theory]
+        [AutoMoqData]
+        public async Task GetStringAnswerArray_ReturnsEmptyString_GivenQuestionRecordAnswerIsNull(
+            [Frozen] Mock<IElsaCustomRepository> elsaCustomRepository,
+            [Frozen] Mock<IWorkflowRegistry> workflowRegistry,
+            string workflowName,
+            string activityId,
+            string activityName,
+            string questionId,
+            string correlationId,
+            WorkflowBlueprint workflowBlueprint,
+            Question question,
+            DataTableQuestionHelper sut)
+        {
+            //Arrange
+            workflowBlueprint.Activities.Add(new ActivityBlueprint()
+            {
+                Id = activityId,
+                Name = activityName
+            });
+
+            elsaCustomRepository.Setup(x => x.GetQuestionByCorrelationId(activityId, correlationId, It.IsAny<string>(), CancellationToken.None)).ReturnsAsync(question);
+
+            workflowRegistry.Setup(x => x.FindByNameAsync(workflowName!, VersionOptions.Published, null, default)).ReturnsAsync(workflowBlueprint);
+
+            question.QuestionType = QuestionTypeConstants.DataTable;
+            question.Answers = null;
+
+            //Act
+            var result = await sut.GetStringAnswerArray(correlationId, workflowName, activityName, questionId);
+
+            //Assert
+            Assert.Equal(new string[] { }, result);
+        }
+
+        [Theory]
+        [AutoMoqData]
+        public async Task GetStringAnswerArray_ReturnsEmptyString_GivenQuestionRecordFirstAnswerIsNull(
+            [Frozen] Mock<IElsaCustomRepository> elsaCustomRepository,
+            [Frozen] Mock<IWorkflowRegistry> workflowRegistry,
+            string workflowName,
+            string activityId,
+            string activityName,
+            string questionId,
+            string correlationId,
+            WorkflowBlueprint workflowBlueprint,
+            Question question,
+            DataTableQuestionHelper sut)
+        {
+            //Arrange
+            workflowBlueprint.Activities.Add(new ActivityBlueprint()
+            {
+                Id = activityId,
+                Name = activityName
+            });
+
+            elsaCustomRepository.Setup(x => x.GetQuestionByCorrelationId(activityId, correlationId, It.IsAny<string>(), CancellationToken.None)).ReturnsAsync(question);
+
+            workflowRegistry.Setup(x => x.FindByNameAsync(workflowName!, VersionOptions.Published, null, default)).ReturnsAsync(workflowBlueprint);
+
+            question.QuestionType = QuestionTypeConstants.DataTable;
+            question.Answers![0] = null!;
+
+            //Act
+            var result = await sut.GetStringAnswerArray(correlationId, workflowName, activityName, questionId);
+
+            //Assert
+            Assert.Equal(new string[] { }, result);
+        }
+
+        [Theory]
+        [AutoMoqData]
+        public async Task GetStringAnswerArray_ReturnsEmptyString_GivenQuestionRecordAnswerDoesNotDeserialise(
+            [Frozen] Mock<IElsaCustomRepository> elsaCustomRepository,
+            [Frozen] Mock<IWorkflowRegistry> workflowRegistry,
+            string workflowName,
+            string activityId,
+            string activityName,
+            string questionId,
+            string correlationId,
+            WorkflowBlueprint workflowBlueprint,
+            Question question,
+            DataTableQuestionHelper sut)
+        {
+            //Arrange
+            workflowBlueprint.Activities.Add(new ActivityBlueprint()
+            {
+                Id = activityId,
+                Name = activityName
+            });
+
+            elsaCustomRepository.Setup(x => x.GetQuestionByCorrelationId(activityId, correlationId, It.IsAny<string>(), CancellationToken.None)).ReturnsAsync(question);
+
+            workflowRegistry.Setup(x => x.FindByNameAsync(workflowName!, VersionOptions.Published, null, default)).ReturnsAsync(workflowBlueprint);
+
+            question.QuestionType = QuestionTypeConstants.DataTable;
+            question.Answers![0].AnswerText = JsonSerializer.Serialize(question.Answers[0].AnswerText);
+
+            //Act
+            var result = await sut.GetStringAnswerArray(correlationId, workflowName, activityName, questionId);
+
+            //Assert
+            Assert.Equal(new string[] { }, result);
+        }
+
+        [Theory]
+        [AutoMoqData]
+        public async Task GetStringAnswerArray_ReturnsCorrectString_GivenDataTableAnswerIsPopulated(
+            [Frozen] Mock<IElsaCustomRepository> elsaCustomRepository,
+            [Frozen] Mock<IWorkflowRegistry> workflowRegistry,
+            string workflowName,
+            string activityId,
+            string activityName,
+            string questionId,
+            string correlationId,
+            string[] answerText,
+            WorkflowBlueprint workflowBlueprint,
+            Question question,
+            DataTable datable,
+            DataTableQuestionHelper sut)
+        {
+            //Arrange
+            workflowBlueprint.Activities.Add(new ActivityBlueprint()
+            {
+                Id = activityId,
+                Name = activityName
+            });
+
+            elsaCustomRepository.Setup(x => x.GetQuestionByCorrelationId(activityId, correlationId, It.IsAny<string>(), CancellationToken.None)).ReturnsAsync(question);
+
+            workflowRegistry.Setup(x => x.FindByNameAsync(workflowName!, VersionOptions.Published, null, default)).ReturnsAsync(workflowBlueprint);
+
+            question.QuestionType = QuestionTypeConstants.DataTable;
+
+            for (int i = 0; i < answerText.Length; i++)
+            {
+                TableInput input = new TableInput(i.ToString(), null, true, true, answerText[i]);
+                datable.Inputs[i] = input;
+            }
+            
+            question.Answers![0].AnswerText = JsonSerializer.Serialize(datable);
+
+            //Act
+            var result = await sut.GetStringAnswerArray(correlationId, workflowName, activityName, questionId);
+
+            //Assert
+            Assert.Equal(answerText, result);
+        }
+
+        [Theory]
+        [AutoMoqData]
+        public async Task GetDecimalAnswerArray_ReturnsEmpty_GivenWorkflowFindByNameAsyncReturnsNull(
+    [Frozen] Mock<IElsaCustomRepository> elsaCustomRepository,
+    [Frozen] Mock<IWorkflowRegistry> workflowRegistry,
+    string workflowName,
+    string activityName,
+    string questionId,
+    string correlationId,
+    DataTableQuestionHelper sut)
+        {
+            //Arrange
+            elsaCustomRepository.Setup(x => x.GetQuestionByCorrelationId(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(), CancellationToken.None)).ReturnsAsync((Question?)null);
+
+            workflowRegistry.Setup(x => x.FindByNameAsync(workflowName!, VersionOptions.Published, null, default)).ReturnsAsync((WorkflowBlueprint?)null);
+
+            //Act
+            var result = await sut.GetDecimalAnswerArray(correlationId, workflowName, activityName, questionId);
+
+            //Assert
+            Assert.Equal(new List<decimal?>().ToArray(), result);
+        }
+
+        [Theory]
+        [AutoMoqData]
+        public async Task GetDecimalAnswerArray_ReturnsEmpty_WorkflowActivitesReturnsNull(
+            [Frozen] Mock<IElsaCustomRepository> elsaCustomRepository,
+            [Frozen] Mock<IWorkflowRegistry> workflowRegistry,
+            string workflowName,
+            string activityName,
+            string questionId,
+            string correlationId,
+            WorkflowBlueprint workflowBlueprint,
+            DataTableQuestionHelper sut)
+        {
+
+            //Arrange
+            elsaCustomRepository.Setup(x => x.GetQuestionByCorrelationId(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(), CancellationToken.None)).ReturnsAsync((Question?)null);
+
+            workflowRegistry.Setup(x => x.FindByNameAsync(workflowName!, VersionOptions.Published, null, default)).ReturnsAsync(workflowBlueprint);
+
+            //Act
+            var result = await sut.GetDecimalAnswerArray(correlationId, workflowName, activityName, questionId);
+
+            //Assert
+            Assert.Equal(new List<decimal?>().ToArray(), result);
+        }
+
+        [Theory]
+        [AutoMoqData]
+        public async Task GetDecimalAnswerArray_ReturnsEmpty_GetQuestionRecordReturnsNull(
+            [Frozen] Mock<IElsaCustomRepository> elsaCustomRepository,
+            [Frozen] Mock<IWorkflowRegistry> workflowRegistry,
+            string workflowName,
+            string activityId,
+            string activityName,
+            string questionId,
+            string correlationId,
+            WorkflowBlueprint workflowBlueprint,
+            DataTableQuestionHelper sut)
+        {
+            //Arrange
+            workflowBlueprint.Activities.Add(new ActivityBlueprint()
+            {
+                Id = activityId,
+                Name = activityName
+            });
+
+            elsaCustomRepository.Setup(x => x.GetQuestionByCorrelationId(activityId, correlationId, It.IsAny<string>(), CancellationToken.None)).ReturnsAsync((Question?)null);
+
+            workflowRegistry.Setup(x => x.FindByNameAsync(workflowName!, VersionOptions.Published, null, default)).ReturnsAsync(workflowBlueprint);
+
+            //Act
+            var result = await sut.GetDecimalAnswerArray(correlationId, workflowName, activityName, questionId);
+
+            //Assert
+            Assert.Equal(new List<decimal?>().ToArray(), result);
+        }
+
+        [Theory]
+        [AutoMoqData]
+        public async Task GetDecimalAnswerArray_ReturnsEmpty_GivenQuestionRecordAnswerIsNull(
+            [Frozen] Mock<IElsaCustomRepository> elsaCustomRepository,
+            [Frozen] Mock<IWorkflowRegistry> workflowRegistry,
+            string workflowName,
+            string activityId,
+            string activityName,
+            string questionId,
+            string correlationId,
+            WorkflowBlueprint workflowBlueprint,
+            Question question,
+            DataTableQuestionHelper sut)
+        {
+            //Arrange
+            workflowBlueprint.Activities.Add(new ActivityBlueprint()
+            {
+                Id = activityId,
+                Name = activityName
+            });
+
+            elsaCustomRepository.Setup(x => x.GetQuestionByCorrelationId(activityId, correlationId, It.IsAny<string>(), CancellationToken.None)).ReturnsAsync(question);
+
+            workflowRegistry.Setup(x => x.FindByNameAsync(workflowName!, VersionOptions.Published, null, default)).ReturnsAsync(workflowBlueprint);
+
+            question.QuestionType = QuestionTypeConstants.DataTable;
+            question.Answers = null;
+
+            //Act
+            var result = await sut.GetDecimalAnswerArray(correlationId, workflowName, activityName, questionId);
+
+            //Assert
+            Assert.Equal(new List<decimal?>().ToArray(), result);
+        }
+
+        [Theory]
+        [AutoMoqData]
+        public async Task GetDecimalAnswerArray_ReturnsEmpty_GivenQuestionRecordFirstAnswerIsNull(
+            [Frozen] Mock<IElsaCustomRepository> elsaCustomRepository,
+            [Frozen] Mock<IWorkflowRegistry> workflowRegistry,
+            string workflowName,
+            string activityId,
+            string activityName,
+            string questionId,
+            string correlationId,
+            WorkflowBlueprint workflowBlueprint,
+            Question question,
+            DataTableQuestionHelper sut)
+        {
+            //Arrange
+            workflowBlueprint.Activities.Add(new ActivityBlueprint()
+            {
+                Id = activityId,
+                Name = activityName
+            });
+
+            elsaCustomRepository.Setup(x => x.GetQuestionByCorrelationId(activityId, correlationId, It.IsAny<string>(), CancellationToken.None)).ReturnsAsync(question);
+
+            workflowRegistry.Setup(x => x.FindByNameAsync(workflowName!, VersionOptions.Published, null, default)).ReturnsAsync(workflowBlueprint);
+
+            question.QuestionType = QuestionTypeConstants.DataTable;
+            question.Answers![0] = null!;
+
+            //Act
+            var result = await sut.GetDecimalAnswerArray(correlationId, workflowName, activityName, questionId);
+
+            //Assert
+            Assert.Equal(new List<decimal?>().ToArray(), result);
+        }
+
+        [Theory]
+        [AutoMoqData]
+        public async Task GetDecimalAnswerArray_ReturnsEmpty_GivenQuestionRecordAnswerDoesNotDeserialise(
+            [Frozen] Mock<IElsaCustomRepository> elsaCustomRepository,
+            [Frozen] Mock<IWorkflowRegistry> workflowRegistry,
+            string workflowName,
+            string activityId,
+            string activityName,
+            string questionId,
+            string correlationId,
+            WorkflowBlueprint workflowBlueprint,
+            Question question,
+            DataTableQuestionHelper sut)
+        {
+            //Arrange
+            workflowBlueprint.Activities.Add(new ActivityBlueprint()
+            {
+                Id = activityId,
+                Name = activityName
+            });
+
+            elsaCustomRepository.Setup(x => x.GetQuestionByCorrelationId(activityId, correlationId, It.IsAny<string>(), CancellationToken.None)).ReturnsAsync(question);
+
+            workflowRegistry.Setup(x => x.FindByNameAsync(workflowName!, VersionOptions.Published, null, default)).ReturnsAsync(workflowBlueprint);
+
+            question.QuestionType = QuestionTypeConstants.DataTable;
+            question.Answers![0].AnswerText = JsonSerializer.Serialize(question.Answers[0].AnswerText);
+
+            //Act
+            var result = await sut.GetDecimalAnswerArray(correlationId, workflowName, activityName, questionId);
+
+            //Assert
+            Assert.Equal(new List<decimal?>().ToArray(), result);
+        }
+
+        [Theory]
+        [AutoMoqData]
+        public async Task GetDecimalAnswerArray_ReturnsCorrectArray_GivenDataTableAnswerIsPopulated(
+            [Frozen] Mock<IElsaCustomRepository> elsaCustomRepository,
+            [Frozen] Mock<IWorkflowRegistry> workflowRegistry,
+            string workflowName,
+            string activityId,
+            string activityName,
+            string questionId,
+            string correlationId,
+            WorkflowBlueprint workflowBlueprint,
+            Question question,
+            DataTable datable,
+            DataTableQuestionHelper sut)
+        {
+            //Arrange
+            datable.InputType = DataTableInputTypeConstants.CurrencyDataTableInput;
+
+            var answerText = new[] { (decimal?)null,Convert.ToDecimal(100), Convert.ToDecimal(200) };
+
+           workflowBlueprint.Activities.Add(new ActivityBlueprint()
+            {
+                Id = activityId,
+                Name = activityName
+            });
+
+            elsaCustomRepository.Setup(x => x.GetQuestionByCorrelationId(activityId, correlationId, It.IsAny<string>(), CancellationToken.None)).ReturnsAsync(question);
+
+            workflowRegistry.Setup(x => x.FindByNameAsync(workflowName!, VersionOptions.Published, null, default)).ReturnsAsync(workflowBlueprint);
+
+            question.QuestionType = QuestionTypeConstants.DataTable;
+
+            for (int i = 0; i < answerText.Length; i++)
+            {
+                var value = answerText[i]?.ToString();
+                TableInput input = new TableInput(i.ToString(), null, true, true, value);
+                datable.Inputs[i] = input;
+            }
+
+            question.Answers![0].AnswerText = JsonSerializer.Serialize(datable);
+
+            //Act
+            var result = await sut.GetDecimalAnswerArray(correlationId, workflowName, activityName, questionId);
+
+            //Assert
+            Assert.Equal(answerText[0], result[0]);
+            Assert.Equal(answerText[1], result[1]);
+            Assert.Equal(answerText[2], result[2]);
+        }
     }
 }
