28d15cc09bd580acb79955baf2356d966aad5df3
Lorna Birchall <200~93874097+lornacbirchall@users.noreply.github.com>
2023-07-12 15:45:41 +0100

    Various component key fixes

--- src/Elsa.Dashboard/src/nested-drivers/he-check-list-driver.tsx
+++ src/Elsa.Dashboard/src/nested-drivers/he-check-list-driver.tsx
@@ -1,6 +1,6 @@
 import { ActivityModel, ActivityPropertyDescriptor } from "../models/elsa-interfaces";
 import { h } from "@stencil/core";
-import { getOrCreateProperty } from "../utils/utils";
+import { getOrCreateProperty, getUniversalUniqueId } from "../utils/utils";
 import { HePropertyDisplayDriver } from "./display-managers/display-manager";
 import { NestedProperty } from "../models/custom-component-models";
 
@@ -8,10 +8,12 @@ export class HeCheckListDriver implements HePropertyDisplayDriver {
 
   display(activity: ActivityModel, property: ActivityPropertyDescriptor) {
     const prop = getOrCreateProperty(activity, property.name);
-    return <he-check-list-property activityModel={activity} propertyDescriptor={property} propertyModel={prop} />;
+    const keyId = getUniversalUniqueId();
+    return <he-check-list-property key={`${keyId}`} activityModel={activity} propertyDescriptor={property} propertyModel={prop} />;
   }
 
   displayNested(activity: ActivityModel, property: NestedProperty, onExpressionChanged: Function) {
-    return <he-check-list-property activityModel={activity} propertyDescriptor={property.descriptor} propertyModel={property.value} onExpressionChanged={e => onExpressionChanged(e, property    )} />;
+    const keyId = getUniversalUniqueId();
+    return <he-check-list-property key={`${keyId}`}  activityModel={activity} propertyDescriptor={property.descriptor} propertyModel={property.value} onExpressionChanged={e => onExpressionChanged(e, property    )} />;
   }
 }
--- src/Elsa.Dashboard/src/nested-drivers/he-question-data-dictionary-driver.tsx
+++ src/Elsa.Dashboard/src/nested-drivers/he-question-data-dictionary-driver.tsx
@@ -1,6 +1,6 @@
 import { ActivityModel, ActivityPropertyDescriptor } from "../models/elsa-interfaces";
 import { h } from "@stencil/core";
-import { getOrCreateProperty } from "../utils/utils";
+import { getOrCreateProperty, getUniversalUniqueId } from "../utils/utils";
 import { HePropertyDisplayDriver } from "./display-managers/display-manager";
 import { NestedProperty } from "../models/custom-component-models";
 
@@ -8,11 +8,12 @@ export class HeQuestionDataDictionaryDriver implements HePropertyDisplayDriver {
 
   display(activity: ActivityModel, property: ActivityPropertyDescriptor) {
     const prop = getOrCreateProperty(activity, property.name);
-    return <he-question-data-dictionary-property activityModel={activity} propertyDescriptor={property} propertyModel={prop} />;
+    const keyId = getUniversalUniqueId();
+    return <he-question-data-dictionary-property key={`${keyId}`} activityModel={activity} propertyDescriptor={property} propertyModel={prop} />;
   }
 
   displayNested(activity: ActivityModel, property: NestedProperty, onExpressionChanged: Function ) {
-  
-    return <he-question-data-dictionary-property activityModel={activity} propertyDescriptor={property.descriptor} propertyModel={property.value} onExpressionChanged={e => onExpressionChanged(e, property)} />;
+    const keyId = getUniversalUniqueId();
+    return <he-question-data-dictionary-property key={`${keyId}`} activityModel={activity} propertyDescriptor={property.descriptor} propertyModel={property.value} onExpressionChanged={e => onExpressionChanged(e, property)} />;
   }
 }
