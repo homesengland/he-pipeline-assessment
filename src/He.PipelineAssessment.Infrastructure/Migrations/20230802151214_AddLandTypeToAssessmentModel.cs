5713c527f96eabc66fc5d2e5f6dbc0a6745afd9c
Jerry Zajac <32240008+jarekzaj@users.noreply.github.com>
2023-08-02 16:15:01 +0100

    added land type to assessment table

---
+++ src/He.PipelineAssessment.Infrastructure/Migrations/20230802151214_AddLandTypeToAssessmentModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace He.PipelineAssessment.Infrastructure.Migrations
+{
+    public partial class AddLandTypeToAssessmentModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "LandType",
+                table: "Assessment",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LandType",
+                table: "Assessment")
+                .Annotation("SqlServer:IsTemporal", true)
+                .Annotation("SqlServer:TemporalHistoryTableName", "AssessmentHistory")
+                .Annotation("SqlServer:TemporalHistoryTableSchema", null);
+        }
+    }
+}
