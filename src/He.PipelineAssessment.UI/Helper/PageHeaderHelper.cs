652ab7c0a7315c703134e569d5f51d9de62d5ceb
Jerry Zajac <32240008+jarekzaj@users.noreply.github.com>
2023-08-17 12:08:51 +0100

    updated links to be opening in a new window added exit to summary page button on all pages added project reference and name to every page

---
+++ src/He.PipelineAssessment.UI/Helper/PageHeaderHelper.cs
@@ -0,0 +1,29 @@
+﻿using He.PipelineAssessment.Models;
+using He.PipelineAssessment.UI.Features.Workflow.LoadConfirmationScreen;
+using He.PipelineAssessment.UI.Features.Workflow.QuestionScreenSaveAndContinue;
+
+namespace He.PipelineAssessment.UI.Helper
+{
+    public static class PageHeaderHelper
+    {
+        public static void PopulatePageHeaderInformation(QuestionScreenSaveAndContinueCommand result, AssessmentToolWorkflowInstance assessmentWorkflowInstance)
+        {
+            result.CorrelationId = assessmentWorkflowInstance.Assessment.SpId;
+            result.SiteName = assessmentWorkflowInstance.Assessment.SiteName;
+            result.CounterParty = assessmentWorkflowInstance.Assessment.Counterparty;
+            result.Reference = assessmentWorkflowInstance.Assessment.Reference;
+            result.LocalAuthority = assessmentWorkflowInstance.Assessment.LocalAuthority;
+            result.ProjectManager = assessmentWorkflowInstance.Assessment.ProjectManager;
+        }
+
+        public static void PopulatePageHeaderInformation(LoadConfirmationScreenResponse result, AssessmentToolWorkflowInstance assessmentWorkflowInstance)
+        {
+            result.CorrelationId = assessmentWorkflowInstance.Assessment.SpId;
+            result.SiteName = assessmentWorkflowInstance.Assessment.SiteName;
+            result.CounterParty = assessmentWorkflowInstance.Assessment.Counterparty;
+            result.Reference = assessmentWorkflowInstance.Assessment.Reference;
+            result.LocalAuthority = assessmentWorkflowInstance.Assessment.LocalAuthority;
+            result.ProjectManager = assessmentWorkflowInstance.Assessment.ProjectManager;
+        }
+    }
+}
