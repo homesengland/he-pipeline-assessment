ff70655bef98a8e1ff44d8d53bf18bf6fde60ee4
Jerry Zajac <32240008+jarekzaj@users.noreply.github.com>
2023-08-21 11:51:03 +0100

    disabling certificate validation altogether

--- src/Elsa.Server/Extensions/ElsaStartupExtensions.cs
+++ src/Elsa.Server/Extensions/ElsaStartupExtensions.cs
@@ -61,47 +61,7 @@ namespace Elsa.Server.Extensions
             X509Chain? chain,
             SslPolicyErrors sslPolicyErrors)
         {
-            // If the certificate is a valid, signed certificate, return true.
-            if (sslPolicyErrors == SslPolicyErrors.None)
-            {
-                return true;
-            }
-
-            // If there are errors in the certificate chain, look at each error to determine the cause.
-            if ((sslPolicyErrors & SslPolicyErrors.RemoteCertificateChainErrors) != 0)
-            {
-                if (chain != null)
-                {
-                    foreach (X509ChainStatus status in chain.ChainStatus)
-                    {
-                        if ((certificate.Subject == certificate.Issuer) &&
-                            (status.Status == X509ChainStatusFlags.UntrustedRoot))
-                        {
-                            // Self-signed certificates with an untrusted root are valid. 
-                            continue;
-                        }
-                        else
-                        {
-                            if (status.Status != X509ChainStatusFlags.NoError)
-                            {
-                                // If there are any other errors in the certificate chain, the certificate is invalid,
-                                // so the method returns false.
-                                return false;
-                            }
-                        }
-                    }
-                }
-
-                // When processing reaches this line, the only errors in the certificate chain are 
-                // untrusted root errors for self-signed certificates. These certificates are valid
-                // for default Exchange server installations, so return true.
-                return true;
-            }
-            else
-            {
-                // In all other cases, return false.
-                return false;
-            }
+            return true;
         }
     }
 }
