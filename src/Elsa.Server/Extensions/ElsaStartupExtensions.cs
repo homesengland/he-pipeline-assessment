ea7d34e2e3ec9bd4d499c666fb78c63cd7fafd72
Jerry Zajac <32240008+jarekzaj@users.noreply.github.com>
2023-08-21 11:12:31 +0100

    updated code to do ssl validation but ignore untrusted roots

--- src/Elsa.Server/Extensions/ElsaStartupExtensions.cs
+++ src/Elsa.Server/Extensions/ElsaStartupExtensions.cs
@@ -1,4 +1,6 @@
-﻿using Elsa.CustomActivities.Activities.QuestionScreen.Helpers;
+﻿using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using Elsa.CustomActivities.Activities.QuestionScreen.Helpers;
 using Elsa.Extensions;
 using Elsa.Options;
 using Medallion.Threading.Redis;
@@ -48,9 +50,58 @@ namespace Elsa.Server.Extensions
             string connectionString)
         {
             var configurationOptions = ConfigurationOptions.Parse(connectionString);
-            configurationOptions.Ssl = false;
+            configurationOptions.CertificateValidation += CertificateValidationCallBack!;
             var connectionMultiplexer = (IConnectionMultiplexer)ConnectionMultiplexer.Connect(configurationOptions);
             return services.AddSingleton(connectionMultiplexer);
+        }
+
+        private static bool CertificateValidationCallBack(
+            object sender,
+            X509Certificate certificate,
+            X509Chain? chain,
+            SslPolicyErrors sslPolicyErrors)
+        {
+            // If the certificate is a valid, signed certificate, return true.
+            if (sslPolicyErrors == SslPolicyErrors.None)
+            {
+                return true;
+            }
+
+            // If there are errors in the certificate chain, look at each error to determine the cause.
+            if ((sslPolicyErrors & SslPolicyErrors.RemoteCertificateChainErrors) != 0)
+            {
+                if (chain != null)
+                {
+                    foreach (X509ChainStatus status in chain.ChainStatus)
+                    {
+                        if ((certificate.Subject == certificate.Issuer) &&
+                            (status.Status == X509ChainStatusFlags.UntrustedRoot))
+                        {
+                            // Self-signed certificates with an untrusted root are valid. 
+                            continue;
+                        }
+                        else
+                        {
+                            if (status.Status != X509ChainStatusFlags.NoError)
+                            {
+                                // If there are any other errors in the certificate chain, the certificate is invalid,
+                                // so the method returns false.
+                                return false;
+                            }
+                        }
+                    }
+                }
+
+                // When processing reaches this line, the only errors in the certificate chain are 
+                // untrusted root errors for self-signed certificates. These certificates are valid
+                // for default Exchange server installations, so return true.
+                return true;
+            }
+            else
+            {
+                // In all other cases, return false.
+                return false;
+            }
         }
     }
 }
