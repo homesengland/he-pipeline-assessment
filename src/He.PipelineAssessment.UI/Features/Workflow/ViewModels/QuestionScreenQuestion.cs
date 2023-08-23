35e4384b4c9b0c63d6039c5c12e599dd59501cf3
Jonny T <90678699+Jonnythan-t@users.noreply.github.com>
2023-08-23 15:19:44 +0100

    updated questions page to add a partial view for rendering indiviual questions

---
+++ src/He.PipelineAssessment.UI/Features/Workflow/ViewModels/QuestionScreenQuestion.cs
@@ -0,0 +1,13 @@
+﻿using Elsa.CustomWorkflow.Sdk.Models.Workflow;
+using FluentValidation.Results;
+
+namespace He.PipelineAssessment.UI.Features.Workflow.ViewModels
+{
+    public class QuestionScreenQuestion
+    {
+        public QuestionActivityData QuestionActivityData { get; set; }
+        public int Index { get; set; }
+        public bool IsValid { get; set; }
+        public ValidationResult? ValidationMessages { get; set; }
+    }
+}
