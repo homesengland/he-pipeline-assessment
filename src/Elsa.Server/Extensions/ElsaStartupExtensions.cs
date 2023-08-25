fc9532ef2d399e238dca79f0a3b26f987154e251
jarekzaj <32240008+jarekzaj@users.noreply.github.com>
2023-08-25 10:16:50 +0100

    Feature/disable redis ssl (#219)
    * customised elsa startup extensions to allow creating a connection to redis with no ssl, as the redis certificate is self signed
    
    
    ---------
    
    Co-authored-by: Jonnythan-t <90678699+Jonnythan-t@users.noreply.github.com>

--- src/Elsa.Server/Extensions/ElsaStartupExtensions.cs
+++ src/Elsa.Server/Extensions/ElsaStartupExtensions.cs
@@ -1,4 +1,8 @@
-﻿using Elsa.CustomActivities.Activities.QuestionScreen.Helpers;
+﻿using System.Net.Security;
+using System.Runtime.InteropServices;
+using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
+using Elsa.CustomActivities.Activities.QuestionScreen.Helpers;
 using Elsa.Extensions;
 using Elsa.Options;
 using Medallion.Threading.Redis;
@@ -38,6 +42,51 @@ namespace Elsa.Server.Extensions
             }
 
             return options;
+        }
+    }
+
+    public static class RedisServiceCollectionExtensions
+    {
+        private static string _certificatePath = "";
+        private static string _certificateKeyPath = "";
+
+        public static IServiceCollection AddRedisWithSelfSignedSslCertificate(
+            this IServiceCollection services,
+            string connectionString, string certificatePath, string certificateKeyPath)
+        {
+            _certificatePath = certificatePath;
+            _certificateKeyPath = certificateKeyPath;
+            var configurationOptions = ConfigurationOptions.Parse(connectionString);
+            configurationOptions.CertificateValidation += CertificateValidationCallBack!;
+            configurationOptions.CertificateSelection += OptionsOnCertificateSelection;
+            configurationOptions.Ssl = true;
+            configurationOptions.SslProtocols = SslProtocols.Tls12;
+            configurationOptions.AbortOnConnectFail = false;
+            var connectionMultiplexer = (IConnectionMultiplexer)ConnectionMultiplexer.Connect(configurationOptions);
+            return services.AddSingleton(connectionMultiplexer);
+        }
+
+        private static X509Certificate OptionsOnCertificateSelection(object sender, string targethost, X509CertificateCollection localcertificates, X509Certificate? remotecertificate, string[] acceptableissuers)
+        {
+            return CreateCertFromPemFile(_certificatePath, _certificateKeyPath);
+        }
+
+        static X509Certificate2 CreateCertFromPemFile(string certPath, string keyPath)
+        {
+            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                return X509Certificate2.CreateFromPemFile(certPath, keyPath);
+
+            using var cert = X509Certificate2.CreateFromPemFile(certPath, keyPath);
+            return new X509Certificate2(cert.Export(X509ContentType.Pkcs12));
+        }
+
+        private static bool CertificateValidationCallBack(
+            object sender,
+            X509Certificate certificate,
+            X509Chain? chain,
+            SslPolicyErrors sslPolicyErrors)
+        {
+            return true;
         }
     }
 }
