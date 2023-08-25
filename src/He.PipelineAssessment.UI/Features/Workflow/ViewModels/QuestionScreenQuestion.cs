112a325f1839d20ef1e2b38ffdbcf26469b0518d
Jonny T <90678699+Jonnythan-t@users.noreply.github.com>
2023-08-25 13:07:55 +0100

    fixed build issue

--- src/He.PipelineAssessment.UI/Features/Workflow/ViewModels/QuestionScreenQuestion.cs
+++ src/He.PipelineAssessment.UI/Features/Workflow/ViewModels/QuestionScreenQuestion.cs
@@ -5,7 +5,7 @@ namespace He.PipelineAssessment.UI.Features.Workflow.ViewModels
 {
     public class QuestionScreenQuestion
     {
-        public QuestionActivityData QuestionActivityData { get; set; }
+        public QuestionActivityData QuestionActivityData { get; set; } = new QuestionActivityData();
         public int Index { get; set; }
         public bool IsValid { get; set; }
         public ValidationResult? ValidationMessages { get; set; }
