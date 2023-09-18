3b3349f56f2538c8c8bf31a9c85820f024944d71
rryan0088 <112186767+rryan0088@users.noreply.github.com>
2023-09-18 09:37:10 +0100

    Bugfix/workflow instances page (#240)
    Added custom serializer settings to our Workflows to prevent issues where Instants were not being serialized or deserialized correctly. This was causing issues where the Workflow Definition's CreatedAt property was being set to null in the Cache - and then on subsequent saves being set to Null in the database.
    
    Added Newtonsoft Serializer, and used the Noda extensions to configure correct parsing of Instant values.
    
    Upgraded Elsa to 2.12 in all projects
    
    Co-authored-by: Jerry Zajac 32240008+jarekzaj@users.noreply.github.com
    Co-authored-by: Jonny T 90678699+Jonnythan-t@users.noreply.github.com
    Co-authored-by: Lorna B 93874097+lornacbirchall@users.noreply.github.com
    Co-authored-by: Rob Ryan 112186767+rryan0088@users.noreply.github.com

---
+++ src/Elsa.Server/Extensions/ConfigureSerializer.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using NodaTime;
+using NodaTime.Serialization.JsonNet;
+
+namespace Elsa.Server.Extensions
+{
+    public static class ConfigureSerializer
+    {
+        public static JsonSerializerSettings ConfigureForInstants(this JsonSerializerSettings @this)
+        {
+            @this.DateParseHandling = DateParseHandling.None;
+            @this.NullValueHandling = NullValueHandling.Ignore;
+            @this.ConfigureForNodaTime(DateTimeZoneProviders.Tzdb);
+            return @this;
+        }
+
+    }
+}
