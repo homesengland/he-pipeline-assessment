d107d482864e879ad4fb9dbddc7b751fef98c705
Rob Ryan <Rob.Ryan@homesengland.gov.uk>
2023-09-25 15:11:09 +0100

    Adding options for DI injection

--- src/Elsa.CustomActivities/Activities/Common/TextModel.cs
+++ src/Elsa.CustomActivities/Activities/Common/TextModel.cs
@@ -20,4 +20,11 @@
     {
         public List<TextGroup> TextGroups { get; set; } = new List<TextGroup>();
     }
+
+    public class GroupedTextOptions
+    {
+        public bool Guidance { get; set; } = false;
+        public bool Collapsed { get; set; } = false;
+
+    }
 }
