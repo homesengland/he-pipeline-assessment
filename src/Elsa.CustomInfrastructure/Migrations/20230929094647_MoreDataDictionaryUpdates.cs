1e05b5152d0f5e53f74e09d410b9c10034f624e7
Jerry Zajac <32240008+jarekzaj@users.noreply.github.com>
2023-09-29 11:52:18 +0100

    added scripts for more data tags

---
+++ src/Elsa.CustomInfrastructure/Migrations/20230929094647_MoreDataDictionaryUpdates.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System.Reflection;
+
+#nullable disable
+
+namespace Elsa.CustomInfrastructure.Migrations
+{
+    public partial class MoreDataDictionaryUpdates : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var sqlFiles = assembly.GetManifestResourceNames().Where(file =>
+                file.Contains(
+                    "20230929094647_MoreDataDictionaryUpdates.MoreDataDictionaryUpdates.sql"));
+            foreach (var sqlFile in sqlFiles)
+            {
+                using (Stream stream = assembly.GetManifestResourceStream(sqlFile))
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    var sqlScript = reader.ReadToEnd();
+                    migrationBuilder.Sql($"EXEC(N'{sqlScript}')");
+                }
+            }
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ src/Elsa.CustomInfrastructure/Migrations/20230929094647_MoreDataDictionaryUpdates/MoreDataDictionaryUpdates.sql
@@ -0,0 +1,22 @@
+
+---Add new ones to 10	Strategic Fit S4
+Insert into [QuestionDataDictionary] ([Id],[QuestionDataDictionaryGroupId],[LegacyName],[Name],[CreatedDateTime]) values (315,10,NULL,''Strategic_Fit4_Site_Facilitate_Economic_Growth_With_High_Quality_Homes'',GetDate());
+Insert into [QuestionDataDictionary] ([Id],[QuestionDataDictionaryGroupId],[LegacyName],[Name],[CreatedDateTime]) values (316,10,NULL,''Strategic_Fit4_Site_Infrastructure_Spend'',GetDate());
+Insert into [QuestionDataDictionary] ([Id],[QuestionDataDictionaryGroupId],[LegacyName],[Name],[CreatedDateTime]) values (317,10,NULL,''Strategic_Fit4_Alignment_With_Economic_Investment'',GetDate());
+Insert into [QuestionDataDictionary] ([Id],[QuestionDataDictionaryGroupId],[LegacyName],[Name],[CreatedDateTime]) values (318,10,NULL,''Strategic_Fit4_Site_Alignment_With_Economic_Investment'',GetDate());
+Insert into [QuestionDataDictionary] ([Id],[QuestionDataDictionaryGroupId],[LegacyName],[Name],[CreatedDateTime]) values (319,10,NULL,''Strategic_Fit4_Partner_Alignment_With_Economic_Investment'',GetDate());
+Insert into [QuestionDataDictionary] ([Id],[QuestionDataDictionaryGroupId],[LegacyName],[Name],[CreatedDateTime]) values (320,10,NULL,''Strategic_Fit4_Partner_Standard_Of_Design_Considerations'',GetDate());
+Insert into [QuestionDataDictionary] ([Id],[QuestionDataDictionaryGroupId],[LegacyName],[Name],[CreatedDateTime]) values (321,10,NULL,''Strategic_Fit4_Site_Standard_Of_Design_Considerations'',GetDate());
+Insert into [QuestionDataDictionary] ([Id],[QuestionDataDictionaryGroupId],[LegacyName],[Name],[CreatedDateTime]) values (322,10,NULL,''Strategic_Fit4_Public_Sector_Investement'',GetDate());
+Insert into [QuestionDataDictionary] ([Id],[QuestionDataDictionaryGroupId],[LegacyName],[Name],[CreatedDateTime]) values (323,10,NULL,''Strategic_Fit4_KPI3_Employment_FTE_Construction_Homes'',GetDate());
+Insert into [QuestionDataDictionary] ([Id],[QuestionDataDictionaryGroupId],[LegacyName],[Name],[CreatedDateTime]) values (324,10,NULL,''Strategic_Fit4_KPI3_Employment_FTE_Construction_Infrastructure'',GetDate());
+Insert into [QuestionDataDictionary] ([Id],[QuestionDataDictionaryGroupId],[LegacyName],[Name],[CreatedDateTime]) values (325,10,NULL,''Strategic_Fit4_KPI3_Total_Direct_Jobs'',GetDate());
+Insert into [QuestionDataDictionary] ([Id],[QuestionDataDictionaryGroupId],[LegacyName],[Name],[CreatedDateTime]) values (326,10,NULL,''Strategic_Fit4_KPI3_Total_OccJobs'',GetDate());
+Insert into [QuestionDataDictionary] ([Id],[QuestionDataDictionaryGroupId],[LegacyName],[Name],[CreatedDateTime]) values (327,10,NULL,''Strategic_Fit4_KPI3_Total_Jobs'',GetDate());
+Insert into [QuestionDataDictionary] ([Id],[QuestionDataDictionaryGroupId],[LegacyName],[Name],[CreatedDateTime]) values (328,10,NULL,''Strategic_Fit4_KPI11_GDV_Private_Sector_Investment'',GetDate());
+
+---Add new ones to 12	Value For Money S4
+Insert into [QuestionDataDictionary] ([Id],[QuestionDataDictionaryGroupId],[LegacyName],[Name],[CreatedDateTime]) values (329,12,NULL,''VFM4_Affordable_Homes_Do_Something'',GetDate());
+Insert into [QuestionDataDictionary] ([Id],[QuestionDataDictionaryGroupId],[LegacyName],[Name],[CreatedDateTime]) values (330,12,NULL,''VFM4_Affordable_Homes_Do_Nothing'',GetDate());
+Insert into [QuestionDataDictionary] ([Id],[QuestionDataDictionaryGroupId],[LegacyName],[Name],[CreatedDateTime]) values (331,12,NULL,''VFM4_Investment_PV'',GetDate());
+Insert into [QuestionDataDictionary] ([Id],[QuestionDataDictionaryGroupId],[LegacyName],[Name],[CreatedDateTime]) values (332,12,NULL,''VFM4_NPSV'',GetDate());
