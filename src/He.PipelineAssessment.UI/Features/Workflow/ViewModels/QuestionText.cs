da554935e122cc19b2ac1c5aabf25a7e93b26320
Lorna Birchall <200~93874097+lornacbirchall@users.noreply.github.com>
2023-10-04 08:10:14 +0100

    Fix hints and question text labels.

--- src/He.PipelineAssessment.UI/Features/Workflow/ViewModels/QuestionText.cs
+++ src/He.PipelineAssessment.UI/Features/Workflow/ViewModels/QuestionText.cs
@@ -5,4 +5,5 @@ public class QuestionText
     public string? Text { get; set; }
     public bool ReevaluatePrepopulatedAnswers { get; set; }
     public bool IsReadOnly { get; set; }
+    public string? Label { get; set; }
 }
