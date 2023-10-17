9e95ecde6c54d45c71f713bd446e0db1b10a89e7
Jerry Zajac <32240008+jarekzaj@users.noreply.github.com>
2023-10-17 14:01:26 +0100

    fixed for incorrect statuses being returned on summary page

---
+++ src/He.PipelineAssessment.Infrastructure/Migrations/20231017124829_FifForIncorrectStatusOnAssessmentScreen.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace He.PipelineAssessment.Infrastructure.Migrations
+{
+    public partial class FifForIncorrectStatusOnAssessmentScreen : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
