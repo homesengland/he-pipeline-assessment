7f1a20d58234629d8c370a29e7b27a5a784c5450
Jerry Zajac <32240008+jarekzaj@users.noreply.github.com>
2023-10-17 15:57:43 +0100

    updated the Up method

--- src/He.PipelineAssessment.Infrastructure/Migrations/20231017124829_FifForIncorrectStatusOnAssessmentScreen.cs
+++ src/He.PipelineAssessment.Infrastructure/Migrations/20231017124829_FifForIncorrectStatusOnAssessmentScreen.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System.Reflection;
 
 #nullable disable
 
@@ -8,7 +9,20 @@ namespace He.PipelineAssessment.Infrastructure.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            var assembly = Assembly.GetExecutingAssembly();
+            var sqlFiles = assembly.GetManifestResourceNames().Where(file =>
 
+                file.Contains(
+                    "20231017124829_FifForIncorrectStatusOnAssessmentScreen.GetAssessments.sql"));
+            foreach (var sqlFile in sqlFiles)
+            {
+                using (Stream stream = assembly.GetManifestResourceStream(sqlFile))
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    var sqlScript = reader.ReadToEnd();
+                    migrationBuilder.Sql($"EXEC(N'{sqlScript}')");
+                }
+            }
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
