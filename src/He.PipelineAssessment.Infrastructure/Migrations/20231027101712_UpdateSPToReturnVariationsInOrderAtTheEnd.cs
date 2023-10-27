844f14bdd3b98f139372cf9c5d80e4898a3e6772
Jerry Zajac <32240008+jarekzaj@users.noreply.github.com>
2023-10-27 11:51:31 +0100

    started using IsVariation field for businness logic and updated stored procedure to influence the order of assessment stages

---
+++ src/He.PipelineAssessment.Infrastructure/Migrations/20231027101712_UpdateSPToReturnVariationsInOrderAtTheEnd.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace He.PipelineAssessment.Infrastructure.Migrations
+{
+    public partial class UpdateSPToReturnVariationsInOrderAtTheEnd : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var sqlFiles = assembly.GetManifestResourceNames().Where(file =>
+
+                file.Contains(
+                    "20231027101712_UpdateSPToReturnVariationsInOrderAtTheEnd.GetAssessmentStagesByAssessmentId.sql") ||
+            );
+            foreach (var sqlFile in sqlFiles)
+            {
+                using (Stream stream = assembly.GetManifestResourceStream(sqlFile))
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    var sqlScript = reader.ReadToEnd();
+                    migrationBuilder.Sql($"EXEC(N'{sqlScript}')");
+                }
+            }
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
