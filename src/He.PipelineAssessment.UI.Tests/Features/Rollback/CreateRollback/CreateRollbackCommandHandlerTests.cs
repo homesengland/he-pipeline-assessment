daa33f366d1f879c2b421f17368914d7458de81b
Jerry Zajac <32240008+jarekzaj@users.noreply.github.com>
2023-11-02 11:50:35 +0000

    fixed CreateRollback tests

--- src/He.PipelineAssessment.UI.Tests/Features/Rollback/CreateRollback/CreateRollbackCommandHandlerTests.cs
+++ src/He.PipelineAssessment.UI.Tests/Features/Rollback/CreateRollback/CreateRollbackCommandHandlerTests.cs
@@ -1,10 +1,7 @@
 ﻿using AutoFixture.Xunit2;
-using He.PipelineAssessment.Infrastructure.Repository;
-using He.PipelineAssessment.Models;
 using He.PipelineAssessment.Tests.Common;
-using He.PipelineAssessment.UI.Authorization;
-using He.PipelineAssessment.UI.Common.Utility;
 using He.PipelineAssessment.UI.Features.Rollback.CreateRollback;
+using He.PipelineAssessment.UI.Services;
 using Moq;
 using Xunit;
 
@@ -14,87 +11,38 @@ namespace He.PipelineAssessment.UI.Tests.Features.Rollback.CreateRollback
     {
         [Theory]
         [AutoMoqData]
-        public async Task Handle_ShouldError_GivenAssessmentToolWorkflowInstanceCannotBeFound(
+        public async Task Handle_ShouldError_GivenInterventionServiceErrors(
+            [Frozen] Mock<IInterventionService> interventionService,
+            Exception e,
             CreateRollbackCommand command,
             CreateRollbackCommandHandler sut)
         {
+            //Arrange
+            interventionService.Setup(x => x.CreateAssessmentIntervention(command)).Throws(e);
+
             //Act
-            var ex = await Assert.ThrowsAsync<ApplicationException>(() => sut.Handle(command, CancellationToken.None));
+            var ex = await Assert.ThrowsAsync<Exception>(() => sut.Handle(command, CancellationToken.None));
 
             //Assert
-            Assert.Equal($"Unable to create rollback. WorkflowInstanceId: {command.WorkflowInstanceId}", ex.Message);
+            Assert.Equal(e.Message, ex.Message);
         }
 
         [Theory]
         [AutoMoqData]
-        public async Task Handle_ShouldError_GivenAssessmentNotAuthorised(
-            [Frozen]Mock<IAssessmentRepository> repository,
-            [Frozen]Mock<IRoleValidation> roleValidation,
+        public async Task Handle_ShouldReturnInt_GivenSuccessfulCallToInterventionService(
+            [Frozen] Mock<IInterventionService> interventionService,
             CreateRollbackCommand command,
-            AssessmentToolWorkflowInstance instance,
             CreateRollbackCommandHandler sut)
         {
             //Arrange
-            repository.Setup(x => x.GetAssessmentToolWorkflowInstance(command.WorkflowInstanceId))
-                .ReturnsAsync(instance);
-
-            roleValidation.Setup(x => x.ValidateRole(It.IsAny<int>(), It.IsAny<string>())).ReturnsAsync(false);
-
-            //Act
-            var ex = await Assert.ThrowsAsync<UnauthorizedAccessException>(() => sut.Handle(command, CancellationToken.None));
-
-            //Assert
-            Assert.Equal($"You do not have permission to access this resource.", ex.Message);
-        }
-
-        [Theory]
-        [AutoMoqData]
-        public async Task Handle_ShouldError_GivenAssessmentToolWorkflowInstanceIsNotLatestSubmitted(
-            [Frozen] Mock<IAssessmentRepository> repository,
-            [Frozen] Mock<IRoleValidation> roleValidation,
-            [Frozen] Mock<IAssessmentToolWorkflowInstanceHelpers> assessmentToolWorkflowInstanceHelpers,
-            CreateRollbackCommand command,
-            AssessmentToolWorkflowInstance instance,
-            CreateRollbackCommandHandler sut)
-        {
-            //Arrange
-            repository.Setup(x => x.GetAssessmentToolWorkflowInstance(command.WorkflowInstanceId))
-                .ReturnsAsync(instance);
-
-            roleValidation.Setup(x => x.ValidateRole(It.IsAny<int>(), It.IsAny<string>())).ReturnsAsync(true);
-
-            assessmentToolWorkflowInstanceHelpers.Setup(x => x.IsOrderEqualToLatestSubmittedWorkflowOrder(instance)).ReturnsAsync(false);
-
-            //Act
-            var ex = await Assert.ThrowsAsync<ApplicationException>(() => sut.Handle(command, CancellationToken.None));
-
-            //Assert
-            Assert.Equal($"Unable to create rollback. WorkflowInstanceId: { command.WorkflowInstanceId}", ex.Message);
-        }
-
-        [Theory]
-        [AutoMoqData]
-        public async Task Handle_Should_GivenAssessmentToolWorkflowInstanceIsNotLatestSubmitted(
-            [Frozen] Mock<IAssessmentRepository> repository,
-            [Frozen] Mock<IRoleValidation> roleValidation,
-            [Frozen] Mock<IAssessmentToolWorkflowInstanceHelpers> assessmentToolWorkflowInstanceHelpers,
-            CreateRollbackCommand command,
-            AssessmentToolWorkflowInstance instance,
-            CreateRollbackCommandHandler sut)
-        {
-            //Arrange
-            repository.Setup(x => x.GetAssessmentToolWorkflowInstance(command.WorkflowInstanceId))
-                .ReturnsAsync(instance);
-
-            roleValidation.Setup(x => x.ValidateRole(It.IsAny<int>(), It.IsAny<string>())).ReturnsAsync(true);
-
-            assessmentToolWorkflowInstanceHelpers.Setup(x => x.IsOrderEqualToLatestSubmittedWorkflowOrder(instance)).ReturnsAsync(true);
+            interventionService.Setup(x => x.CreateAssessmentIntervention(command))
+                .ReturnsAsync(123);
 
             //Act
             var result = await sut.Handle(command, CancellationToken.None);
 
             //Assert
-            repository.Verify(x=>x.CreateAssessmentIntervention(It.IsAny<AssessmentIntervention>()),Times.Once);
+            Assert.Equal(123, result);
         }
     }
 }
