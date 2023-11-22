9778e098b531350ddddd16ee8346e591c1113572
Rob Ryan <Rob.Ryan@homesengland.gov.uk>
2023-11-22 09:16:54 +0000

    Removed a now redundant method and updated tests.

--- src/He.PipelineAssessment.Data/ExtendedSinglePipeline/EsriSinglePipelineExtendedClient.cs
+++ src/He.PipelineAssessment.Data/ExtendedSinglePipeline/EsriSinglePipelineExtendedClient.cs
@@ -5,7 +5,6 @@ namespace He.PipelineAssessment.Data.ExtendedSinglePipeline
     public interface IEsriSinglePipelineExtendedClient
     {
         Task<string?> GetSinglePipelineExtendedData(string spid);
-        Task<SinglePipelineExtendedDataList?> GetSinglePipelineExtendedDataList(int offset);
     }
     public class EsriSinglePipelineExtendedClient : IEsriSinglePipelineExtendedClient
     {
@@ -46,56 +45,5 @@ namespace He.PipelineAssessment.Data.ExtendedSinglePipeline
 
             return data;
         }      
-
-        public async Task<SinglePipelineExtendedDataList?> GetSinglePipelineExtendedDataList(int offset)
-        {
-            string? data = null;
-            string whereClause =
-                "sp_status in ('Active', 'Approved','In Programme') AND sp_stage in ('Lead', 'Opportunity', 'Contracted','Holding') AND sp_business_area in ('Investment', 'Development', 'MPP') " +
-                "AND (project_owner is NOT NULL AND project_owner <> '') " +
-                "AND (units_or_homes is NOT NULL) " +
-                "AND (local_authority is NOT NULL AND local_authority <> '') " +
-                "AND (funding_ask is NOT NULL) " +
-                "AND (pipeline_opportunity_site_name is NOT NULL AND pipeline_opportunity_site_name <> '') " +
-                "AND (applicant_1 is NOT NULL AND applicant_1 <> '') " +
-                "AND land_type in ('Greenfield', 'Brownfield', 'Mixed')";
-            string orderBy = "esri_id";
-            string outFields =
-                "sp_id,internal_reference,pipeline_opportunity_site_name," +
-                "applicant_1," +
-                "project_owner," +
-                "project_owner_email,local_authority,funding_ask," +
-                "units_or_homes," +
-                "sp_business_area," +
-                "land_type," +
-                "sensitive_status";
-
-            var relativeUri =
-                $"query?where={whereClause}&outFields={outFields}&orderByFields={orderBy}&resultOffset={offset}&f=json"; //potentially needs a token on the end also
-            using (var response = await _httpClientFactory.CreateClient("SinglePipelineClient").GetAsync(relativeUri)
-                       .ConfigureAwait(false))
-            {
-                data = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
-
-                if (!response.IsSuccessStatusCode)
-                {
-                    _logger.LogError($"StatusCode='{response.StatusCode}'," +
-                                     $"\n Message= '{data}'," +
-                                     $"\n Url='{relativeUri}'");
-
-                    return null;
-                }
-
-                var dataResult = _jsonHelper.JsonToSinglePipelineExtendedDataList(data);
-                if (dataResult != null)
-                {
-                    return dataResult;
-                }
-                else
-                {
-                    return null;
-                }              
-            }
-        }
     }
 }
