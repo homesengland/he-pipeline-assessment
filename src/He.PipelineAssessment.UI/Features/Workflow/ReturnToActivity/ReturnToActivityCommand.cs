b8938bdbf69e01ffc101c86a8bbe1bd54b7c4308
Lorna Birchall <200~93874097+lornacbirchall@users.noreply.github.com>
2023-11-28 11:55:33 +0000

    Fix nullable properties.

--- src/He.PipelineAssessment.UI/Features/Workflow/ReturnToActivity/ReturnToActivityCommand.cs
+++ src/He.PipelineAssessment.UI/Features/Workflow/ReturnToActivity/ReturnToActivityCommand.cs
@@ -6,8 +6,8 @@ namespace He.PipelineAssessment.UI.Features.Workflow.ReturnToActivity
 {
     public class ReturnToActivityCommand : IRequest<ReturnToActivityCommandResponse>
     {
-            public string WorkflowInstanceId { get; set; }
-            public string ActivityId { get; set; }
+            public string? WorkflowInstanceId { get; set; }
+            public string? ActivityId { get; set; }
 
     }
 }
