862ac2bbacca901b6f441a7477a7618f70e8957f
Lorna Birchall <200~93874097+lornacbirchall@users.noreply.github.com>
2023-12-05 15:27:50 +0000

    Add additional data sources.

--- src/Elsa.Server/Extensions/CustomTypeDefinitionProvider.cs
+++ src/Elsa.Server/Extensions/CustomTypeDefinitionProvider.cs
@@ -1,9 +1,16 @@
 ﻿using Elsa.CustomModels;
 using Elsa.Scripting.JavaScript.Services;
+using He.PipelineAssessment.Data.ExtendedSinglePipeline;
 using He.PipelineAssessment.Data.LaHouseNeed;
 using He.PipelineAssessment.Data.PCSProfile;
+using He.PipelineAssessment.Data.RegionalFigs;
+using He.PipelineAssessment.Data.RegionalIPU;
 using He.PipelineAssessment.Data.SinglePipeline;
 using He.PipelineAssessment.Data.VFM;
+using He.PipelineAssessment.Data.VoaLandValues.Models.Agriculture;
+using He.PipelineAssessment.Data.VoaLandValues.Models.Land;
+using He.PipelineAssessment.Data.VoaLandValues.Models.Office;
+using Npgsql.TypeMapping;
 
 namespace Elsa.Server.Extensions
 {
@@ -16,7 +23,13 @@ namespace Elsa.Server.Extensions
             typeof(LaHouseNeedData),
             typeof(PCSProfileData),
             typeof(VFMCalculationData),
-            typeof(Question)
+            typeof(Question),
+            typeof(SinglePipelineExtendedData),
+            typeof(RegionalFigsData),
+            typeof(RegionalIPUData),
+            typeof(LandValues),
+            typeof(AgricultureLandValues),
+            typeof(OfficeLandValues)
         };
     }
 }
