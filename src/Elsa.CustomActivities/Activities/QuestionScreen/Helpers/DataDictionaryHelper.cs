9b0141e94e939b2fc171a7f08bf1d0c4bc30cad3
Jerry Zajac <32240008+jarekzaj@users.noreply.github.com>
2023-12-14 17:32:15 +0000

    attempt to add intellisense with data dictionary items

---
+++ src/Elsa.CustomActivities/Activities/QuestionScreen/Helpers/DataDictionaryHelper.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+using Elsa.CustomInfrastructure.Data.Repository;
+using Elsa.Scripting.JavaScript.Events;
+using Elsa.Scripting.JavaScript.Messages;
+using MediatR;
+
+namespace Elsa.CustomActivities.Activities.QuestionScreen.Helpers
+{
+    public class DataDictionaryHelper : INotificationHandler<EvaluatingJavaScriptExpression>, INotificationHandler<RenderingTypeScriptDefinitions>
+    {
+        private readonly IElsaCustomRepository _elsaCustomRepository;
+
+        public DataDictionaryHelper(IElsaCustomRepository elsaCustomRepository)
+        {
+            _elsaCustomRepository = elsaCustomRepository;
+        }
+
+        public Task Handle(EvaluatingJavaScriptExpression notification, CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+
+        public async Task Handle(RenderingTypeScriptDefinitions notification, CancellationToken cancellationToken)
+        {
+            var output = notification.Output;
+            var dataDictionaryItems = await _elsaCustomRepository.GetQuestionDataDictionaryListAsync(cancellationToken);
+
+            var stringBuilder = new StringBuilder();
+
+            stringBuilder.Append("declare const DataDictionary = {");
+
+            foreach (var dataDictionary in dataDictionaryItems)
+            {
+                var group = dataDictionary.Group.Name.Replace(" ", "_");
+                stringBuilder.Append(group + "_" +
+                                     dataDictionary.Name + ": '" + dataDictionary.Id + "',");
+            }
+
+            stringBuilder.Remove(stringBuilder.Length - 1, 1);
+            stringBuilder.Append("};");
+
+            var dataDictionaryObject = stringBuilder.ToString();
+            output.AppendLine(dataDictionaryObject);
+        }
+    }
+}
