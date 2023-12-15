bb7a93df6271d9f177afdeed341b036db32597e8
Jerry Zajac <32240008+jarekzaj@users.noreply.github.com>
2023-12-15 15:50:45 +0000

    Bugfix/allow office values return multiple values (#310)
    * deserlialising response from office values to a list of objects
    
    Co-authored-by: Jerry Zajac 32240008+jarekzaj@users.noreply.github.com
    Co-authored-by: Lorna B 93874097+lornacbirchall@users.noreply.github.com
    Co-authored-by: Rob Ryan 112186767+rryan0088@users.noreply.github.com

--- src/He.PipelineAssessment.Data/VoaLandValues/Office/OfficeLandValuesDataJsonHelper.cs
+++ src/He.PipelineAssessment.Data/VoaLandValues/Office/OfficeLandValuesDataJsonHelper.cs
@@ -1,5 +1,4 @@
-﻿
-using He.PipelineAssessment.Data.VoaLandValues.Models.Office;
+﻿using He.PipelineAssessment.Data.VoaLandValues.Models.Office;
 using System.Text.Json;
 
 namespace He.PipelineAssessment.Data.VoaLandValues.Office
@@ -7,6 +6,7 @@ namespace He.PipelineAssessment.Data.VoaLandValues.Office
     public interface IOfficeLandValuesDataJsonHelper
     {
         OfficeLandValues? JsonToOfficeLandValuesData(string data);
+        List<OfficeLandValues>? JsonToOfficeLandValuesDataList(string data);
     }
 
     public class OfficeLandValuesDataJsonHelper : IOfficeLandValuesDataJsonHelper
@@ -20,6 +20,26 @@ namespace He.PipelineAssessment.Data.VoaLandValues.Office
                 if (result != null)
                 {
                     var dataResult = result.features.FirstOrDefault()!.attributes;
+                    return dataResult;
+                }
+            }
+            catch (Exception)
+            {
+
+                return null;
+            }
+
+            return null;
+        }
+
+        public List<OfficeLandValues>? JsonToOfficeLandValuesDataList(string data)
+        {
+            try
+            {
+                var result = JsonSerializer.Deserialize<OfficeLandValuesResponse>(data);
+                if (result != null && result.features.Any())
+                {
+                    List<OfficeLandValues> dataResult = result.features.Select(x => x.attributes).ToList();
                     return dataResult;
                 }
             }
