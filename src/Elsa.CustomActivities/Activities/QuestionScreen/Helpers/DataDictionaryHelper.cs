dd87358a0158013eb42f1267424d06bc74d152cd
Jerry Zajac <32240008+jarekzaj@users.noreply.github.com>
2023-12-15 12:14:38 +0000

    added a way of setting const values in jint engine, which resolve to data dictionary IDs

--- src/Elsa.CustomActivities/Activities/QuestionScreen/Helpers/DataDictionaryHelper.cs
+++ src/Elsa.CustomActivities/Activities/QuestionScreen/Helpers/DataDictionaryHelper.cs
@@ -1,7 +1,11 @@
 ﻿using System.Text;
 using Elsa.CustomInfrastructure.Data.Repository;
+using Elsa.CustomModels;
 using Elsa.Scripting.JavaScript.Events;
 using Elsa.Scripting.JavaScript.Messages;
+using Elsa.Services.Models;
+using Jint.Native;
+using Jint.Runtime;
 using MediatR;
 
 namespace Elsa.CustomActivities.Activities.QuestionScreen.Helpers
@@ -9,35 +13,45 @@ namespace Elsa.CustomActivities.Activities.QuestionScreen.Helpers
     public class DataDictionaryHelper : INotificationHandler<EvaluatingJavaScriptExpression>, INotificationHandler<RenderingTypeScriptDefinitions>
     {
         private readonly IElsaCustomRepository _elsaCustomRepository;
+        private List<QuestionDataDictionary> _dataDictionaryItems;
 
         public DataDictionaryHelper(IElsaCustomRepository elsaCustomRepository)
         {
             _elsaCustomRepository = elsaCustomRepository;
         }
 
-        public Task Handle(EvaluatingJavaScriptExpression notification, CancellationToken cancellationToken)
+        public async Task Handle(EvaluatingJavaScriptExpression notification, CancellationToken cancellationToken)
         {
-            return Task.CompletedTask;
+            var engine = notification.Engine;
+            _dataDictionaryItems = await _elsaCustomRepository.GetQuestionDataDictionaryListAsync(cancellationToken);
+            foreach (var dataDictionary in _dataDictionaryItems)
+            {
+                var group = dataDictionary.Group.Name.Replace(" ", "_");
+
+                engine.SetValue(group + "_" + dataDictionary.Name, dataDictionary.Id);
+            }
+            //engine.SetValue("DataDictionary", _dataDictionaryItems);
         }
 
         public async Task Handle(RenderingTypeScriptDefinitions notification, CancellationToken cancellationToken)
         {
             var output = notification.Output;
-            var dataDictionaryItems = await _elsaCustomRepository.GetQuestionDataDictionaryListAsync(cancellationToken);
+            _dataDictionaryItems = await _elsaCustomRepository.GetQuestionDataDictionaryListAsync(cancellationToken);
 
             var stringBuilder = new StringBuilder();
 
-            stringBuilder.Append("declare const DataDictionary = {");
+            //stringBuilder.Append("declare const DataDictionary = {");
 
-            foreach (var dataDictionary in dataDictionaryItems)
+            foreach (var dataDictionary in _dataDictionaryItems)
             {
                 var group = dataDictionary.Group.Name.Replace(" ", "_");
-                stringBuilder.Append(group + "_" +
-                                     dataDictionary.Name + ": '" + dataDictionary.Id + "',");
+                stringBuilder.Append("declare const " + group + "_" + dataDictionary.Name + " = null;");
+                //stringBuilder.Append(group + "_" +
+                //                     dataDictionary.Name + ": '" + dataDictionary.Id + "',");
             }
 
-            stringBuilder.Remove(stringBuilder.Length - 1, 1);
-            stringBuilder.Append("};");
+            //stringBuilder.Remove(stringBuilder.Length - 1, 1);
+            //stringBuilder.Append("};");
 
             var dataDictionaryObject = stringBuilder.ToString();
             output.AppendLine(dataDictionaryObject);
