f20131da6d6f34700aa0b197aa44a294433c56ad
avinashbobba95 <avinash.bobba@homesengland.gov.uk>
2024-03-13 11:09:08 +0000

    Revert "92919 Failed to load confirmation screen activity"
    This reverts commit 727bdd242e58bca1a730f3ecd30b756ccd96932d.

--- src/He.PipelineAssessment.UI/Common/Utility/AssessmentToolWorkflowInstanceHelpers.cs
+++ src/He.PipelineAssessment.UI/Common/Utility/AssessmentToolWorkflowInstanceHelpers.cs
@@ -21,7 +21,6 @@ namespace He.PipelineAssessment.UI.Common.Utility
         public async Task<bool> IsOrderEqualToLatestSubmittedWorkflowOrder(AssessmentToolWorkflowInstance currentAssessmentToolWorkflowInstance)
         {
             var latestSubmittedWorkflowInstance = currentAssessmentToolWorkflowInstance.Assessment!.AssessmentToolWorkflowInstances!
-                .Where(x => x.AssessmentToolWorkflowId != null)
                 .OrderByDescending(x => x.SubmittedDateTime)
                 .FirstOrDefault(x => x.Status == AssessmentToolWorkflowInstanceConstants.Submitted);
             if (latestSubmittedWorkflowInstance != null)
