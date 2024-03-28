89ef8318f66d87066de0676aab87ad1ec122cb5d
Pavlo Nikulin <Pavlo.Nikulin@homesengland.gov.uk>
2024-03-28 18:02:13 +0000

    Bug 94314: Fix PAS outcome name in Dynamics

--- src/He.PipelineAssessment.UI/Integration/ServiceBusSend/GetAssessmentDTOCommandHandler.cs
+++ src/He.PipelineAssessment.UI/Integration/ServiceBusSend/GetAssessmentDTOCommandHandler.cs
@@ -35,7 +35,7 @@ namespace He.PipelineAssessment.UI.Features.Workflow.QuestionScreenSaveAndContin
                     result.Assessment.Result = assessmentToolWorkflowInstance.Result;
                     result.Assessment.ProjectId = assessmentToolWorkflowInstance.Assessment.SpId;
                     result.Assessment.SubmittedBy = assessmentToolWorkflowInstance.SubmittedBy;
-                    result.Assessment.Name = string.Compare(result.Assessment.Status, "Suspended - RB")==0
+                    result.Assessment.Name = string.Compare(result.Assessment.Status, "Suspended - RB", true)==0
                         ? $"{assessmentToolWorkflowInstance.AssessmentToolWorkflow.Name} - {assessmentToolWorkflowInstance.WorkflowName}"
                         : result.Assessment.Name = assessmentToolWorkflowInstance.AssessmentToolWorkflow.Name;
                 }
