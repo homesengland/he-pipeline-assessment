9096e04f265c7b5b7f5f60ea644e12f88399c15e
avinashbobba95 <avinash.bobba@homesengland.gov.uk>
2024-07-23 12:21:29 +0100

    Vulnerability fix

--- src/He.PipelineAssessment.Data/Dataverse/DataverseClient.cs
+++ src/He.PipelineAssessment.Data/Dataverse/DataverseClient.cs
@@ -3,13 +3,7 @@ using Azure.Identity;
 using He.PipelineAssessment.Data.Auth;
 using Microsoft.Extensions.Options;
 using Microsoft.PowerPlatform.Dataverse.Client;
-using Microsoft.Xrm.Sdk;
 using Microsoft.Xrm.Sdk.Query;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace He.PipelineAssessment.Data.Dataverse
 {
--- src/He.PipelineAssessment.Data/Dataverse/IDataverseClient.cs
+++ src/He.PipelineAssessment.Data/Dataverse/IDataverseClient.cs
@@ -1,17 +1,4 @@
-﻿using Azure.Core;
-using Azure.Identity;
-using He.PipelineAssessment.Data.Auth;
-using Microsoft.Extensions.Options;
-using Microsoft.PowerPlatform.Dataverse.Client;
-using Microsoft.Xrm.Sdk;
-using Microsoft.Xrm.Sdk.Query;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace He.PipelineAssessment.Data.Dataverse
+﻿namespace He.PipelineAssessment.Data.Dataverse
 {
     public interface IDataverseClient
     {
