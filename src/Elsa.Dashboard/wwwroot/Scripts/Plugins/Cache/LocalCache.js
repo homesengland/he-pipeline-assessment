abc232e538b8c481b78e2daca0c06d1a42b6b668
Rob Ryan <Rob.Ryan@homesengland.gov.uk>
2024-08-12 10:33:32 +0100

    Initial setup of Cache

---
+++ src/Elsa.Dashboard/wwwroot/Scripts/Plugins/Cache/LocalCache.js
@@ -0,0 +1,24 @@
+export class LocalStorageCache {
+
+  constructor(cachePrefix, salt) {
+    this.prefix = cachePrefix;
+    this.salt = salt;
+  }
+
+  prefix;
+  salt;
+
+  async allKeys() {
+
+  }
+  async get(key) {
+
+  };
+  async remove(key) {
+
+  }
+  async set(key, entry) {
+
+  }
+
+}
