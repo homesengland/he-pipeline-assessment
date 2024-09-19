d33f16f502a2fd65d123ea79ebc6efbaae4ae5ed
Aaron Walsh <Aaron.Walsh@homesengland.gov.uk>
2024-09-19 17:08:44 +0100

    fixing styling issue

--- src/Elsa.Dashboard.Vue/src/main.ts
+++ src/Elsa.Dashboard.Vue/src/main.ts
@@ -1,30 +1,21 @@
-import './assets/main.css'
-
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
-import App from './App.vue'
-// import router from './router'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import App from './App.vue';
 
 import { createVuetify } from 'vuetify';
-import 'vuetify/styles';
-import '@mdi/font/css/materialdesignicons.css';
-import { aliases, mdi } from 'vuetify/iconsets/mdi';
+import 'vuetify/styles'; // Required Vuetify styles
+import '@mdi/font/css/materialdesignicons.css'; // Material Design Icons
+import * as components from 'vuetify/components'; // Import all Vuetify components
+import * as directives from 'vuetify/directives'; // Import Vuetify directives
 
 const vuetify = createVuetify({
-    icons: {
-        defaultSet: 'mdi',
-        aliases,
-        sets: {
-            mdi,
-        },
-    },
+  components, // Register all Vuetify components
+  directives, // Register Vuetify directives
 });
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(createPinia())
-// app.use(router)
-app.use(vuetify)
+app.use(createPinia());
+app.use(vuetify);
 
-app.mount('#app')
+app.mount('#app');
