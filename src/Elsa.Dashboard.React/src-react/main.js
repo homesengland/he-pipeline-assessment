9246e18e546cb1a1b4c46e67273dd88f2303bd70
Lorna Birchall <200~93874097+lornacbirchall@users.noreply.github.com>
2024-11-27 15:18:36 +0000

    Fix npm packages, and re include www folder.

--- src/Elsa.Dashboard.React/src-react/main.js
+++ src/Elsa.Dashboard.React/src-react/main.js
@@ -2,4 +2,4 @@ import { jsx as _jsx } from "react/jsx-runtime";
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import ElsaStudioRoot from './pages/elsa-studio-root';
-createRoot(document.getElementById('dashboard-root')).render(_jsx(StrictMode, { children: _jsx(ElsaStudioRoot, {}) }));
+createRoot(document.getElementById('dashboard-root')).render(_jsx(StrictMode, { children: _jsx(ElsaStudioRoot, {}, void 0) }, void 0));
--- src/Elsa.Dashboard.React/src-react/pages/elsa-studio-root.js
+++ src/Elsa.Dashboard.React/src-react/pages/elsa-studio-root.js
@@ -3,7 +3,7 @@ import { Component } from "react";
 import ElsaStudioDashboard from "./elsa-studio-dashboard";
 class ElsaStudioRoot extends Component {
     render() {
-        return (_jsx(ElsaStudioDashboard, {}));
+        return (_jsx(ElsaStudioDashboard, {}, void 0));
     }
 }
 export default ElsaStudioRoot;
