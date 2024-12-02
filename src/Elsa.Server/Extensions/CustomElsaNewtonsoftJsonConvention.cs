baff30d8a42beea49d136e68f07fdafee03f3c4a
Lorna Birchall <93874097+lornacbirchall@users.noreply.github.com>
2024-12-02 14:47:40 +0000

    Add new custom convention to set newtonsoft json serlializer when usi… (#362)
    * Add new custom convention to set newtonsoft json serlializer when using custom controllers.
    
    * Update minor build version
    
    ---------
    
    Co-authored-by: Lorna Birchall <200~93874097+lornacbirchall@users.noreply.github.com>

---
+++ src/Elsa.Server/Extensions/CustomElsaNewtonsoftJsonConvention.cs
@@ -0,0 +1,31 @@
+﻿using Elsa.Server.Api.Attributes;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+
+namespace Elsa.Server.Extensions
+{
+    public class CustomElsaNewtonsoftJsonConvention : IControllerModelConvention
+    {
+        public void Apply(ControllerModel controller)
+        {
+            if (ShouldApplyConvention(controller))
+            {
+                NewtonsoftJsonFormatterAttribute newtonsoftJsonFormatterAttribute = new NewtonsoftJsonFormatterAttribute();
+                newtonsoftJsonFormatterAttribute.Apply(controller);
+                controller.Filters.Add(newtonsoftJsonFormatterAttribute);
+            }
+        }
+
+        private bool ShouldApplyConvention(ControllerModel controller)
+        {
+            if (controller != null && 
+                (controller.ControllerType?.FullName?.StartsWith("Elsa.Server.Features.Dashboard.CustomList") == true ||
+                (controller.ControllerType?.FullName?.StartsWith("Elsa.Server.Features.Dashboard.CustomHistory") == true)))
+
+            {
+                return !controller.Attributes.Any((object x) => x.GetType() == typeof(NewtonsoftJsonFormatterAttribute));
+            }
+
+            return false;
+        }
+    }
+}
