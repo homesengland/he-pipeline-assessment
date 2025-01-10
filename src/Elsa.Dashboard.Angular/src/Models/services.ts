c319d84f26b4e2299063bd0c2c19aeb1ab428510
Rob Ryan <112186767+rryan0088@users.noreply.github.com>
2025-01-10 09:55:25 +0000

    Feature/angular/auth0 (#371)
    * Adding Models and Utils folder, Adding inital typescript files for models
    
    * Adding in file structure up to components/screens
    
    * Added last of folders to reflect elsa-workflows-studio as found in the 2.14.1 elsa core github
    
    * WIP - implimenting models
    
    * Refactoring and resolving errors on GetOrCreateProperty method.
    
    * Refactoring utils.ts and defining types
    
    * adding check to ensure versionOptions.version is not undefined
    
    * undefined check in domain.ts and  temporarily commenting out services.ts
    
    * Removing empty directories
    
    * Initial work setting up auth0
    
    * Setting up Auth0 plugin
    
    * Additional changes following work on Auth0 plugin
    
    ---------
    
    Co-authored-by: Cameron Skinner <Cameron.Skinner@homesengland.gov.uk>
    Co-authored-by: Aaron Walsh <Aaron.Walsh@homesengland.gov.uk>

---
+++ src/Elsa.Dashboard.Angular/src/Models/services.ts
@@ -0,0 +1,30 @@
+// TODO:
+
+export interface Sudio {
+
+}
+
+//import { PluginManager, ActivityIconProvider, ConfirmDialogService, PropertyDisplayManager } from "../services";
+//import { ElsaClient, ToastNotificationService } from "../services";
+//import EventBus from "../services/custom-event-bus";
+//import { AxiosInstance } from "axios";
+//import { ActivityDefinitionProperty } from "./domain";
+//import { ActivityModel } from "./view";
+
+//export interface Studio {
+//  serverUrl: string;
+//  basePath: string;
+//  features: any;
+//  serverFeatures: Array<string>;
+//  serverVersion: string;
+//  pluginManager: PluginManager;
+//  propertyDisplayManager: PropertyDisplayManager;
+//  elsaClientFactory: () => Promise<ElsaClient>;
+//  httpClientFactory: () => Promise<AxiosInstance>;
+//  eventBus: EventBus;
+//  activityIconProvider: ActivityIconProvider;
+//  confirmDialogService: ConfirmDialogService;
+//  toastNotificationService: ToastNotificationService;
+//  getOrCreateProperty: (activity: ActivityModel, name: string, defaultExpression?: () => string, defaultSyntax?: () => string) => ActivityDefinitionProperty;
+//  htmlToElement: (html: string) => Element;
+//}
