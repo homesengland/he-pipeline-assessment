3351b18ab9e19afa67189db8cfce5c7a6b59f62f
Aaron Walsh <Aaron.Walsh@homesengland.gov.uk>
2025-02-18 14:07:07 +0000

    Experimenting with dynamically rendering components using Angular

--- src/Elsa.Dashboard.Angular/src/components/dashboard/pages/workflow-definitions-list/workflow-definitions-list.ts
+++ src/Elsa.Dashboard.Angular/src/components/dashboard/pages/workflow-definitions-list/workflow-definitions-list.ts
@@ -1,7 +1,10 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewContainerRef, ViewChild } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { selectMonacoLibPath, selectServerUrl } from '../../../state/selectors/app.state.selectors';
+import { HelloWorldComponent } from '../../../hello-world-component';
+import { activityIconProvider } from '../../../../services/activity-icon-provider';
+import { WorkflowDashboard } from '../workflow-dashboard/workflow-dashboard';
 
 @Component({
   selector: 'workflow-definitions-list',
@@ -11,16 +14,37 @@ import { selectMonacoLibPath, selectServerUrl } from '../../../state/selectors/a
 export class WorkflowDefinitionsList implements OnInit {
   serverUrl: string;
   monacoLibPath: string;
+  userInput: string = '';
 
-  constructor(private http: HttpClient, private store: Store) {}
-    ngOnInit(): void {
+  @ViewChild('helloWorldContainer', { read: ViewContainerRef }) helloWorldContainer: ViewContainerRef;
+  @ViewChild('workflowDashboardContainer', { read: ViewContainerRef }) workflowDashboardContainer: ViewContainerRef;
+
+  constructor(private http: HttpClient, private store: Store) { }
+  ngOnInit(): void {
     this.store.select(selectServerUrl).subscribe(data => {
       this.serverUrl = data;
     });
     this.store.select(selectMonacoLibPath).subscribe(data => {
       this.monacoLibPath = data
     });
-    }
+    //this.viewContainer.createComponent(HelloWorldComponent);
+    //this.viewContainer.createComponent(WorkflowDashboard);
+  }
 
+  onRadioChange(value: string): void {
+    this.helloWorldContainer.clear();
+    if (value === 'yes') {
+      this.helloWorldContainer.createComponent(HelloWorldComponent);
+    }
+  }
+
+  onInputChange(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    const value = input.value;
+    this.workflowDashboardContainer.clear();
+    if (value === 'TEST') {
+      this.workflowDashboardContainer.createComponent(WorkflowDashboard);
+    }
+  }
 }
 
---
+++ src/Elsa.Dashboard.Angular/src/components/hello-world-component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hello-world-component',
+  template: `<h1>Hello World</h1>`,
+  styles: []
+})
+export class HelloWorldComponent { }
