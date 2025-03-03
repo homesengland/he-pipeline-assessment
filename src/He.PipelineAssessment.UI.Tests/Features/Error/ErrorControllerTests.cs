0817399825e63ae4488622937d3bb0fb3a05452c
Lorna Birchall <200~93874097+lornacbirchall@users.noreply.github.com>
2025-03-03 13:25:58 +0000

    Add logging and add test.

--- src/He.PipelineAssessment.UI.Tests/Features/Error/ErrorControllerTests.cs
+++ src/He.PipelineAssessment.UI.Tests/Features/Error/ErrorControllerTests.cs
@@ -1,11 +1,15 @@
 ﻿using System.Net;
+using System.Text.RegularExpressions;
 using AutoFixture.Xunit2;
+using Castle.Core.Logging;
 using He.PipelineAssessment.Tests.Common;
 using He.PipelineAssessment.UI.Features.Error;
+using HibernatingRhinos.Profiler.Appender;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Moq;
 using Xunit;
 
@@ -16,7 +20,8 @@ namespace He.PipelineAssessment.UI.Tests.Features.Error
         [Theory]
         [AutoMoqData]
         public void Index_ShouldReturn_ErrorView_GivenErrorOccurs(
-            [Frozen]Mock<IErrorHelper> errorHelper,
+            [Frozen] Mock<IErrorHelper> errorHelper,
+            [Frozen] Mock<ILogger<ErrorController>> logger,
             Exception exception,
             ErrorController sut
             )
@@ -24,12 +29,25 @@ namespace He.PipelineAssessment.UI.Tests.Features.Error
             //Arrange
             errorHelper.Setup(x => x.ExceptionHandlerFeatureGetException(It.IsAny<HttpContext>())).Returns(exception);
 
-             //Act
-             var result = sut.Index();
+            //Act
+            var result = sut.Index();
 
             //Assert
             Assert.IsAssignableFrom<IActionResult>(result);
             Assert.IsType<ViewResult>(result);
+            VerifyLog(logger, LogLevel.Error, Times.Once, $"An error occurred while processing your request {exception.Message}");
+
+        }
+
+        private void VerifyLog(Mock<ILogger<ErrorController>> logger, LogLevel level, Func<Times> times, string regex)
+        {
+            logger.Verify(m => m.Log(
+                level,
+                It.IsAny<EventId>(),
+                It.Is<It.IsAnyType>((x, y) => regex == null || Regex.IsMatch(x.ToString(), regex)),
+                It.IsAny<Exception?>(),
+                It.IsAny<Func<It.IsAnyType, Exception?, string>>()),
+                times);
         }
 
         [Theory]
@@ -49,5 +67,6 @@ namespace He.PipelineAssessment.UI.Tests.Features.Error
             Assert.IsAssignableFrom<IActionResult>(result);
             Assert.IsType<NoContentResult>(result);
         }
+
     }
 }
