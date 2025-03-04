8d9ed7b2c199ff6534b054b9706befb2f6057ee7
Lorna Birchall <200~93874097+lornacbirchall@users.noreply.github.com>
2025-03-04 10:15:39 +0000

    Revert changes to DataDictionaryCommandHandlerTests

--- src/Elsa.Server.Tests/Features/Activities/DataDictionary/DataDictionaryCommandHandlerTests.cs
+++ src/Elsa.Server.Tests/Features/Activities/DataDictionary/DataDictionaryCommandHandlerTests.cs
@@ -63,18 +63,9 @@ namespace Elsa.Server.Tests.Features.Activities.DataDictionaryProvider
 
             //Act
             var result = await sut.Handle(command, CancellationToken.None);
-            var resultObj = JsonConvert.DeserializeObject<Dictionary<string, string>>(result);
-
-            if (resultObj != null)
-            {
-                var dataDictionaryObj = JsonConvert.DeserializeObject<List<DataDictionary>>(resultObj["Dictionary"]);
-                if (dataDictionaryObj != null)
-                {
-                    //Assert
-                    Assert.Empty(dataDictionaryObj);
-                    Assert.Empty(resultObj["Intellisense"]);
-                }
-            }
+            
+            //Assert
+            Assert.Empty(result);
         }
     }
 }
