d562480242a04835f9197dc039a121d9807ee371
Lorna Birchall <200~93874097+lornacbirchall@users.noreply.github.com>
2025-03-04 10:17:59 +0000

    Remove unneccessary package references

--- src/He.PipelineAssessment.UI.Tests/Features/Error/ErrorControllerTests.cs
+++ src/He.PipelineAssessment.UI.Tests/Features/Error/ErrorControllerTests.cs
@@ -1,10 +1,7 @@
-﻿using System.Net;
-using AutoFixture.Xunit2;
+﻿using AutoFixture.Xunit2;
 using He.PipelineAssessment.Tests.Common;
 using He.PipelineAssessment.UI.Features.Error;
-using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using Xunit;
