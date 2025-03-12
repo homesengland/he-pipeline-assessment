5ebfaf74dfadb78e0a7af1e8c481af05a266a977
Lorna Birchall <200~93874097+lornacbirchall@users.noreply.github.com>
2025-03-12 14:00:28 +0000

    Correctly import moment.js.

--- src/Elsa.Dashboard.Angular/src/components/shared/workflow-fault-information/workflow-fault-information.ts
+++ src/Elsa.Dashboard.Angular/src/components/shared/workflow-fault-information/workflow-fault-information.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import moment from 'moment';
+import * as moment from 'moment';
 import { clip, durationToString } from '../../../utils/utils';
 import { WorkflowFault } from 'src/models';
 
