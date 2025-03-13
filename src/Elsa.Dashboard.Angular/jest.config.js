74b9935d9b9758b5d230844ad9d2d396d103adc5
Maruf Ahmed <Maruf.Ahmed@homesengland.gov.uk>
2025-03-13 10:57:27 +0000

    Using Jest Preset Angular to get mock tests working

--- src/Elsa.Dashboard.Angular/jest.config.js
+++ src/Elsa.Dashboard.Angular/jest.config.js
@@ -1,8 +1,62 @@
+//// TEST 1 - MY NEW FILE
+// module.exports = {
+//   preset: 'ts-jest',
+//   testEnvironment: 'jsdom',
+//   transform: {
+//     '^.+\\.ts$': 'ts-jest', // Only transform .ts files
+//   },
+//   transformIgnorePatterns: ['<rootDir>/node_modules/'],
+// };
+
+// // TEST 2 - THIS WORKED WITH NORMAL FUNCTION TEST but NOT with
+// // the Mocking Test for HttpClient
+// module.exports = {
+//   roots: ['<rootDir>/src'],
+//   preset: 'ts-jest',
+//   testEnvironment: 'jsdom',
+//   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+//   transform: {
+//     '^.+\\.ts?$': 'ts-jest',
+//     '^.+\\.(js|jsx)$': 'babel-jest',
+//   },
+//   transformIgnorePatterns: ['<rootDir>/node_modules/'],
+//   globals: {
+//     window: {},
+//   },
+//   moduleFileExtensions: ['ts', 'tsx', 'js'],
+//   reporters: ['default', ['jest-junit', { suiteName: 'jest tests' }]],
+// };
+
+// // TEST 3 - DIDN'T WORK AT ALL, function tests prev. worked started failing,
+// // mocks never worked. TypeError: configSet.processWithEsbuild is not a function
+// module.exports = {
+//   roots: ['<rootDir>/src'],
+//   preset: 'jest-preset-angular',
+//   setupFilesAfterEnv: ['<rootDir>/src/setup-jest.ts'], // init. jest-preset-angular
+//   testEnvironment: 'jsdom',
+//   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+//   transform: {
+//     '^.+\\.(ts|js|html)$': 'ts-jest',
+//   },
+//   transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+//   moduleFileExtensions: ['ts', 'tsx', 'js', 'html'],
+//   reporters: ['default', ['jest-junit', { suiteName: 'jest tests' }]],
+// };
+
+// TEST 4 - WORKS, Mock and function tests worked using Jest Preset Angular Setup
 module.exports = {
-  preset: 'ts-jest',
+  roots: ['<rootDir>/src'],
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/src/setup-jest.ts'], // init. jest-preset-angular
   testEnvironment: 'jsdom',
-  transform: {
-    '^.+\\.ts$': 'ts-jest', // Only transform .ts files
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+    'window': {},
   },
-  transformIgnorePatterns: ['<rootDir>/node_modules/'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'html'],
+  reporters: ['default', ['jest-junit', { suiteName: 'jest tests' }]],
 };
---
+++ src/Elsa.Dashboard.Angular/src/components/dashboard/pages/workflow-instances-view/__tests__/workflow-instances-view.spec.ts
@@ -0,0 +1,23 @@
+﻿import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { WorkflowInstancesView } from '../workflow-instances-view';
+
+describe('WorkflowInstancesView', () => {
+  let component: WorkflowInstancesView;
+  let fixture: ComponentFixture<WorkflowInstancesView>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [WorkflowInstancesView],
+      imports: [HttpClientTestingModule],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(WorkflowInstancesView);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
