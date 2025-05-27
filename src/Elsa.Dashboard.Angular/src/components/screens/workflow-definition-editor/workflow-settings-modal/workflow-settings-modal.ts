722a947dabf57727d3d02c85a0542fd55e60bb15
Aaron Walsh <Aaron.Walsh@homesengland.gov.uk>
2025-05-27 15:30:17 +0100

    WIP - converting workflow-settings-modal

--- src/Elsa.Dashboard.Angular/src/components/screens/workflow-definition-editor/workflow-settings-modal/workflow-settings-modal.ts
+++ src/Elsa.Dashboard.Angular/src/components/screens/workflow-definition-editor/workflow-settings-modal/workflow-settings-modal.ts
@@ -1,22 +1,70 @@
-// import { Component, OnInit } from '@angular/core';
-// import { WorkflowDefinition } from 'src/models';
-// import { HTMLElsaMonacoElement } from 'src/models/elsa-interfaces';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { EventTypes, WorkflowDefinition } from 'src/models';
+import { HTMLElsaMonacoElement, MonacoValueChangedArgs } from 'src/models/elsa-interfaces';
+import { eventBus } from 'src/services/event-bus';
+import { ModalDialog } from 'src/components/shared/modal-dialog/modal-dialog';
 
-// @Component({
-//   selector: 'workflow-settings-modal',
-//   templateUrl: './workflow-settings-modal.html',
-//   standalone: false,
-// })
-// export class WorkflowSettingsModal implements OnInit {
-//   isModalVisible: boolean;
+interface VariableDefinition {
+  name?: string;
+  value?: string;
+}
+@Component({
+  selector: 'workflow-settings-modal',
+  templateUrl: './workflow-settings-modal.html',
+  standalone: false,
+})
+export class WorkflowSettingsModal implements OnInit {
+  @ViewChild(ModalDialog) dialog;
+  serverUrl: string;
+  workflowDefinition: WorkflowDefinition;
+  workflowDefinitionInternal: WorkflowDefinition;
+  selectedTab: string = 'Settings';
+  newVariable: VariableDefinition = {};
+  monacoEditor: HTMLElsaMonacoElement; // ?? Not sure if this is correct
+  // formContext: FormContext; // To be converted.
+  workflowChannels: Array<string>;
 
-//   serverUrl: string;
-//   workflowDefinition: WorkflowDefinition;
-//   workflowDefinitionInternal: WorkflowDefinition;
-//   selectedTab: string = 'Settings';
-//   newVariable: VariableDefinition = {};
-//   dialog: HTMLDialogElement;
-//   monacoEditor: HTMLElsaMonacoElement; // ?? Not sure if this is correct
-//   // formContext: FormContext; // Look into FormContext to see if converted or conversion is needed.
-//   workflowChannels: Array<string>;
-// }
+  ngOnInit(): void {}
+
+  // @Watch('workflowDefinition')
+  // handleWorkflowDefinitionChanged(newValue: WorkflowDefinition) {
+  //   this.workflowDefinitionInternal = {...newValue};
+  //   this.formContext = new FormContext(this.workflowDefinitionInternal, newValue => this.workflowDefinitionInternal = newValue);
+  // }
+
+  // async componentWillLoad() {
+  //   this.handleWorkflowDefinitionChanged(this.workflowDefinition);
+
+  //   const client = await createElsaClient(this.serverUrl);
+  //   this.workflowChannels = await client.workflowChannelsApi.list();
+  // }
+
+  componentDidLoad() {
+    eventBus.on(EventTypes.ShowWorkflowSettings, async () => await this.dialog.show());
+  }
+
+  onTabClick(e: Event, tab: string) {
+    e.preventDefault();
+    this.selectedTab = tab;
+  }
+
+  async onCancelClick() {
+    await this.dialog.close();
+  }
+
+  async onSubmit(e: Event) {
+    e.preventDefault();
+    await this.dialog.close();
+    setTimeout(() => eventBus.emit(EventTypes.UpdateWorkflowSettings, this, this.workflowDefinitionInternal), 250);
+  }
+
+  // onMonacoValueChanged(e: MonacoValueChangedArgs) {
+  //   // Don't try and parse JSON if it contains errors.
+  //   const errorCount = e.markers.filter(x => x.severity == MarkerSeverity.Error).length;
+
+  //   if (errorCount > 0)
+  //     return;
+
+  //   this.workflowDefinitionInternal.variables = e.value;
+  // }
+}
