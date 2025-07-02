54c5334bf7251ea5d3b151d37ba688a6dfe93433
Cameron Skinner <Cameron.Skinner@homesengland.gov.uk>
2025-07-02 17:07:58 +0100

    Implementing onchange for radio list. Updating default value on load and selecting/unselecting radio buttons appropriately

--- src/Elsa.Dashboard.Angular/src/components/editors/properties/radio-list-property/radio-list-property.ts
+++ src/Elsa.Dashboard.Angular/src/components/editors/properties/radio-list-property/radio-list-property.ts
@@ -3,6 +3,8 @@ import { ActivityModel, SyntaxNames } from '../../../../models';
 import { ActivityDefinitionProperty, ActivityPropertyDescriptor, SelectList } from '../../../../models/domain';
 import { PropertyEditor } from '../../property-editor/property-editor';
 import { getSelectListItems } from 'src/utils/selected-list-items';
+import { ElsaClientService } from 'src/services/elsa-client';
+import { parseJson } from 'src/utils/utils';
 
 @Component({
   selector: 'radio-list-property',
@@ -13,31 +15,55 @@ export class RadioListProperty implements OnInit {
   activityModel = model<ActivityModel>();
   propertyDescriptor = model<ActivityPropertyDescriptor>();
   propertyModel = model<ActivityDefinitionProperty>();
+  serverUrl: string
+  currentValue?: string
 
   fieldLabel = computed(() => this.propertyDescriptor()?.label ?? 'default');
+  fieldId = computed(() => this.propertyDescriptor()?.name ?? 'default');
 
   selectList: SelectList = {
     items: [],
     isFlagsEnum: false,
   };
+$event: CustomEvent<any>;
+$defaultSyntaxEvent: CustomEvent;
 
-  constructor() {
+  constructor(private elsaClientService: ElsaClientService,) {
     console.log('Setting property model', this.propertyModel());
+  }
+
+  async ngOnInit(): Promise<void> {
+    const defaultValue = this.propertyDescriptor().defaultValue?.toString() || '';
+    this.currentValue = defaultValue;
+    this.selectList = await getSelectListItems(this.elsaClientService, this.serverUrl, this.propertyDescriptor());
+  }
+
+  onDefaultSyntaxValueChanged(e: CustomEvent) {
+    this.currentValue = e.detail;
+  }
+
+  onCheckChanged(e: Event) {
+    const radio = (e.target as HTMLInputElement);
+    const checked = radio.checked;
+
+    if (checked)
+      this.currentValue = radio.value;
+
+    const defaultSyntax = this.propertyDescriptor().defaultSyntax || SyntaxNames.Literal;
+    this.propertyModel().expressions[defaultSyntax] = this.currentValue;
   }
 
   getSelectItems(): any {
     console.log('Select list items:', this.selectList.items);
-    return this.selectList.items.map(item => {
+    return this.selectList.items.map((item, index) => {
       return {
         text: item.text,
         value: item.value,
-        isSelected: true,
-        inputId: '${this.fieldId}_${index}',
+        isSelected: this.currentValue == item.value,
+        inputId: `${this.fieldId}_${index}`,
       };
     });
   }
-
-  ngOnInit(): void {}
-
-  onCheckChanged(e: Event) {}
 }
+
+
