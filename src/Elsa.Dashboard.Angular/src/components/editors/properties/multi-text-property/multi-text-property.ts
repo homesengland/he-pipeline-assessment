73eddc0a9faf0b05caf848f3286a94b9673f81e8
Aaron Walsh <Aaron.Walsh@homesengland.gov.uk>
2025-07-14 16:03:31 +0100

    WIP - Converting multi-text property.

--- src/Elsa.Dashboard.Angular/src/components/editors/properties/multi-text-property/multi-text-property.ts
+++ src/Elsa.Dashboard.Angular/src/components/editors/properties/multi-text-property/multi-text-property.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, model } from '@angular/core';
+import { Component, EventEmitter, model, OnInit } from '@angular/core';
 import { ActivityDefinitionProperty, ActivityModel, ActivityPropertyDescriptor, SelectList, SelectListItem, SyntaxNames } from 'src/models';
 import { PropertyEditor } from '../../property-editor/property-editor';
 
@@ -7,20 +7,18 @@ import { PropertyEditor } from '../../property-editor/property-editor';
   templateUrl: './multi-text-property.html',
   standalone: false,
 })
-export class MultiTextProperty {
+export class MultiTextProperty implements OnInit {
   activityModel = model<ActivityModel>();
   propertyDescriptor = model<ActivityPropertyDescriptor>();
   propertyModel = model<ActivityDefinitionProperty>();
   serverUrl: string;
   currentValue?: string;
-  valueChange: EventEmitter<Array<string | number | boolean | SelectListItem>>;
+  valueChange = new EventEmitter<Array<string | number | boolean | SelectListItem>>();
 
   selectList: SelectList = {
     items: [],
     isFlagsEnum: false,
   };
-  $event: CustomEvent<any>;
-  $defaultSyntaxEvent: CustomEvent;
 
   constructor() {
     console.log('MultiTextProperty component initialized');
@@ -30,39 +28,30 @@ export class MultiTextProperty {
     this.currentValue = this.propertyModel().expressions['Json'] || '[]';
   }
 
-  onValueChanged(newValue: Array<string | number | boolean | SelectListItem>) {
-    const newValues = newValue.map(item => {
+  onValueChanged(values: Array<string | number | boolean | SelectListItem>) {
+    const newValues = values.map(item => {
       if (typeof item === 'string') return item;
       if (typeof item === 'number') return item.toString();
       if (typeof item === 'boolean') return item.toString();
-
       return item.value;
     });
 
-    this.valueChange.emit(newValue);
+    this.valueChange.emit(values);
     this.currentValue = JSON.stringify(newValues);
     this.propertyModel().expressions[SyntaxNames.Json] = this.currentValue;
   }
 
-  onDefaultSyntaxValueChanged(e: CustomEvent) {
-    this.currentValue = e.detail;
+  onDefaultSyntaxValueChanged(event: CustomEvent<string>) {
+    this.currentValue = event.detail;
   }
 
   createKeyValueOptions(options: Array<SelectListItem>) {
     if (options === null) return options;
-
     return options.map(option => (typeof option === 'string' ? { text: option, value: option } : option));
   }
 
   getSelectItems(): any {
-    console.log('Select list items:', this.selectList.items);
-    return this.selectList.items.map((item, index) => {
-      return {
-        text: item.text,
-        value: item.value,
-        isSelected: this.currentValue == item.value,
-      };
-    });
+    return JSON.parse(this.currentValue || '[]');
   }
 
   get useDropdown() {
