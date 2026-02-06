197089af7a8153d079c3a344e3271cf276db9928
Rob Ryan <Rob.Ryan@homesengland.gov.uk>
2026-02-06 17:00:09 +0000

    Fixes for the many failing unit tests.

--- src/He.PipelineAssessment.UI/Features/Admin/AssessmentToolManagement/Validators/UpdateAssessmentToolWorkflowCommandValidator.cs
+++ src/He.PipelineAssessment.UI/Features/Admin/AssessmentToolManagement/Validators/UpdateAssessmentToolWorkflowCommandValidator.cs
@@ -24,13 +24,13 @@ namespace He.PipelineAssessment.UI.Features.Admin.AssessmentToolManagement.Valid
                 .Must((c) => BeUnique(c.WorkflowDefinitionId, c.Id))
                 .WithMessage("The Workflow Definition Id must be unique and not used in another Assessment Tool Workflow");
 
-            RuleFor(c => c.IsEarlyStage)
-                .Must(FollowIsEarlyStageRule)
-                .WithMessage("Funds can only be assigned to assessment tool workflows that are not marked as Early Stage. " +
+            RuleFor(c => c.AssessmentFundId)
+            .Must((command, assessmentFundId) => command.IsEarlyStage && assessmentFundId.HasValue)
+            .WithMessage("Funds cannot be assigned to assessment tool workflows that are marked as Early Stage. " +
                 "Please ensure that no fund is selected when creating an Early Stage workflow.");
 
             RuleFor(c => c.AssessmentFundId)
-                .Must((command, assessmentFundId) => FollowFundRule(command.IsEarlyStage, assessmentFundId))
+                .Must((command, assessmentFundId) => !command.IsEarlyStage && (assessmentFundId.HasValue && assessmentFundId.Value > 0))
                 .WithMessage("A fund must be assigned to assessment tool workflows that are not marked as Early Stage.");
         }
 
@@ -53,7 +53,12 @@ namespace He.PipelineAssessment.UI.Features.Admin.AssessmentToolManagement.Valid
 
         private bool FollowFundRule(bool isEarlyStage, int? assessmentFundId)
         {
-            if (!isEarlyStage && (!assessmentFundId.HasValue || assessmentFundId == 0))
+            bool hasAssessmentFund = assessmentFundId.HasValue && assessmentFundId > 0;
+            if (isEarlyStage && hasAssessmentFund)
+            {
+                return false;
+            }
+            else if(!isEarlyStage && !hasAssessmentFund)
             {
                 return false;
             }
