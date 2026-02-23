1031a3af824910df82f9ca9fa7e78f3b2b6f8fea
Aaron Walsh <Aaron.Walsh@homesengland.gov.uk>
2026-02-23 13:33:00 +0000

    updating model and adding hidden table collumns for search

--- src/He.PipelineAssessment.Models/ViewModels/AssessmentDataViewModel.cs
+++ src/He.PipelineAssessment.Models/ViewModels/AssessmentDataViewModel.cs
@@ -19,6 +19,8 @@ namespace He.PipelineAssessment.Models.ViewModels
         public string? Status { get; set; }
         public string? SensitiveStatus { get; set; }
         public bool ValidData { get; set; }
+        public string? ProjectIdentifier { get; set; }
+        public string? InternalReference { get; set; }
         public string StatusDisplayTag()
         {
             switch (Status)
