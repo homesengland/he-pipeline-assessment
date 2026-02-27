afdd44291f23052d7774a92d5709f1c4eb872da0
Rob Ryan <Rob.Ryan@homesengland.gov.uk>
2026-02-27 11:45:22 +0000

    Additional Fixes to allow other fields to be searchable

---
+++ src/He.PipelineAssessment.Infrastructure/Migrations/20260227112015_AddingSearchFieldsToInterventionList.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System.Reflection;
+
+#nullable disable
+
+namespace He.PipelineAssessment.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingSearchFieldsToInterventionList : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var sqlFiles = assembly.GetManifestResourceNames().Where(file =>
+                file.Contains(
+                    "20260227112015_AddingSearchFieldsToInterventionList.GetInterventionList.sql"));
+            foreach (var sqlFile in sqlFiles)
+            {
+                using (Stream stream = assembly.GetManifestResourceStream(sqlFile))
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    var sqlScript = reader.ReadToEnd();
+                    migrationBuilder.Sql($"EXEC(N'{sqlScript}')");
+                }
+            }
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            var queryToDropInterventionListProc = "DROP PROC GetInterventionList";
+            migrationBuilder.Sql(queryToDropInterventionListProc);
+        }
+    }
+}
