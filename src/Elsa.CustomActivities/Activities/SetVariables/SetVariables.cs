45fb612f1e17d4abf6d723c0e71ad61040e1a8de
Rob Ryan <Rob.Ryan@homesengland.gov.uk>
2026-03-05 16:49:22 +0000

    Initial work on the Elsa Components

---
+++ src/Elsa.CustomActivities/Activities/SetVariables/SetVariables.cs
@@ -0,0 +1,33 @@
+﻿using Elsa.Activities.ControlFlow;
+using Elsa.ActivityResults;
+using Elsa.Attributes;
+using Elsa.CustomActivities.Constants;
+using Elsa.Expressions;
+using Elsa.Services;
+using Elsa.Services.Models;
+
+namespace Elsa.CustomActivities.Activities.SetVariables
+{
+    [Action(
+Category = "Utilities",
+Description = "Set Variables in the Database for future use",
+Outcomes = new[] { OutcomeNames.Done }
+)]
+    public class SetVariables : Activity
+    {
+
+        protected override async ValueTask<IActivityExecutionResult> OnExecuteAsync(ActivityExecutionContext context)
+        {
+            return await Task.FromResult(Suspend());
+        }
+
+        protected override async ValueTask<IActivityExecutionResult> OnResumeAsync(ActivityExecutionContext context)
+        {
+            return await Task.FromResult(new CombinedResult(new List<IActivityExecutionResult>
+            {
+                Outcomes("Done"),
+                new SuspendResult()
+            }));
+        }
+    }
+}
